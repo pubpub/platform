649bfee278c0f880dadc169556843d44e948282c
Travis Rich <travis.s.rich@gmail.com>
2023-06-22 14:54:56 -0400

    update: Adjust schema approach to metadata fields and values

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,6 @@
 import { PrismaClient } from "@prisma/client";
+import { v4 as uuidv4 } from "uuid";
+
 const prisma = new PrismaClient();
 async function main() {
 	const mainUserId = "a9a09993-8eb1-4122-abbf-b999d5c8afe3";
@@ -10,8 +12,9 @@ async function main() {
 			name: "Atta Test",
 		},
 	});
-	const communityUUID = "33222293-8eb1-4122-abbf-b999d5c8afe3";
-	const frankensteinUUID = "a9a09993-8eb1-4122-abbf-b999d5c8afe3";
+	const communityUUID = uuidv4();
+	const frankensteinUUID = uuidv4();
+	const fieldIDs = [...Array(6)].map((x) => uuidv4());
 	const community = await prisma.community.create({
 		data: {
 			id: communityUUID,
@@ -20,99 +23,112 @@ async function main() {
 				create: [
 					{
 						name: "Book",
-						fields: ["title", "isbn", "publicationDate"],
+						metadataFields: {
+							create: [
+								{ id: fieldIDs[0], name: "title" },
+								{ id: fieldIDs[1], name: "isbn" },
+								{ id: fieldIDs[2], name: "publicationDate" },
+							],
+						},
 						pubs: {
 							create: [
 								{
 									id: frankensteinUUID,
 									communityId: communityUUID,
-									metadata: {
+									metadataBlob: [
+										{ fieldId: fieldIDs[0], value: "Frankenstein" },
+										{ fieldId: fieldIDs[1], value: "10234-123414" },
+										{ fieldId: fieldIDs[2], value: "1959" },
+									],
+									metadataValues: {
 										create: [
-											{
-												type: "title",
-												value: "Frankenstein"
-											},
-											{
-												type: "ISBN",
-												value: "10234-123414"
-											},
-											{
-												type: "publicationDate",
-												value: "1959"
-											}
-										]
-									}
-								}
-							]
-						}
+											{ fieldId: fieldIDs[0], value: "Frankenstein" },
+											{ fieldId: fieldIDs[1], value: "10234-123414" },
+											{ fieldId: fieldIDs[2], value: "1959" },
+										],
+									},
+								},
+							],
+						},
 					},
 					{
 						name: "Chapter",
-						fields: ["title", "files"],
+						metadataFields: {
+							create: [
+								{ id: fieldIDs[3], name: "title" },
+								{ id: fieldIDs[4], name: "files" },
+							],
+						},
 						pubs: {
 							create: [
 								{
 									communityId: communityUUID,
 									parentId: frankensteinUUID,
-									metadata: {
+									metadataBlob: [
+										{ fieldId: fieldIDs[3], value: "Chapter 1" },
+										{ fieldId: fieldIDs[4], value: ["internet/chapter1.html"] },
+									],
+									metadataValues: {
 										create: [
+											{ fieldId: fieldIDs[3], value: "Chapter 1" },
 											{
-												type: "title",
-												value: "Chapter 1"
+												fieldId: fieldIDs[4],
+												value: ["internet/chapter1.html"],
 											},
-											{
-												type: "files",
-												value: ["internet/chapter1.html"]
-											},
-											
-										]
-									}
+										],
+									},
 								},
 								{
 									communityId: communityUUID,
 									parentId: frankensteinUUID,
-									metadata: {
+									metadataBlob: [
+										{ fieldId: fieldIDs[3], value: "Chapter 2" },
+										{ fieldId: fieldIDs[4], value: ["internet/chapter2.html"] },
+									],
+									metadataValues: {
 										create: [
+											{ fieldId: fieldIDs[3], value: "Chapter 2" },
 											{
-												type: "title",
-												value: "Chapter 2"
+												fieldId: fieldIDs[4],
+												value: ["internet/chapter2.html"],
 											},
-											{
-												type: "files",
-												value: ["internet/chapter2.html"]
-											},
-											
-										]
-									}
+										],
+									},
 								},
 								{
 									communityId: communityUUID,
 									parentId: frankensteinUUID,
-									metadata: {
+									metadataBlob: [
+										{ fieldId: fieldIDs[3], value: "Chapter 3" },
+										{ fieldId: fieldIDs[4], value: ["internet/chapter3.html"] },
+									],
+									metadataValues: {
 										create: [
+											{ fieldId: fieldIDs[3], value: "Chapter 3" },
 											{
-												type: "title",
-												value: "Chapter 3"
+												fieldId: fieldIDs[4],
+												value: ["internet/chapter3.html"],
 											},
-											{
-												type: "files",
-												value: ["internet/chapter3.html"]
-											},
-											
-										]
-									}
-								}
-							]
-						}
+										],
+									},
+								},
+							],
+						},
 					},
 					{
 						name: "Journal",
-						fields: ["title", "publisher", "publicationDate"],
-					}
-				]
-			}
-		}
-	})
+						metadataFields: {
+							create: [
+								{ name: "title" },
+								{ name: "publisher" },
+								{ name: "publicationDate" },
+							],
+						},
+					},
+				],
+			},
+		},
+	});
 }
 main()
 	.then(async () => {
