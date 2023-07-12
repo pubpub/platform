c490d30b3610027129a1ec213e11aa8f7ba5921d
Kalil Smith-Nuevelle <kalilsn@gmail.com>
2023-07-12 17:33:04 -0500

    Add empty routes for initial api structure

---
+++ app/api/v7/integration/[instance_id]/public_key/route.ts
@@ -0,0 +1,3 @@
+export async function GET(request: Request) {
+    // Return the public key that this integration can use to authenticate a PubPub signed token.
+}
---
+++ app/api/v7/integration/[instance_id]/pubs/[pub_id]/route.ts
@@ -0,0 +1,7 @@
+export async function GET(request: Request) {
+    // Return this single pub
+}
+export async function POST(request: Request) {
+    // Update pub
+    // Return completed pub
+}
