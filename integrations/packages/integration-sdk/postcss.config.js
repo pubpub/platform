53753318c1346e0f59e83a5a71762f994167d684
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2023-07-21 00:45:21 -0400

    integration styles

--- integrations/packages/integration-sdk/postcss.config.js
+++ integrations/packages/integration-sdk/postcss.config.js
@@ -2,7 +2,9 @@ import autoprefixer from "autoprefixer"
 import import_ from "postcss-import"
 import mixins from "postcss-mixins"
 import nested from "postcss-nested"
+import svg from "postcss-inline-svg"
+import vars from "postcss-simple-vars"
 
 export default {
-	plugins: [import_(), mixins(), nested(), autoprefixer()],
+	plugins: [import_(), mixins(), nested(), vars(), svg(), autoprefixer()],
 }
