303485713895ddc595227a4f15c4bfd1f328676c
qweliant <dmtorcode@gmail.com>
2023-07-24 15:23:31 -0400

    added route

--- core/app/api/v7/integration/[instance_id]/autocomplete/users/route.ts
+++ core/app/api/v7/integration/[instance_id]/autocomplete/users/route.ts
@@ -1,26 +1,27 @@
 import prisma from "prisma/db";
+import { NextResponse, NextRequest } from "next/server";
 
 export async function GET(request: Request) {
-    // return candidates for autocompleting a form with suggestions from the pubpub user database
-    // should search by name and email, but probably only return name and id
-    	// try {
-		// 	const namesStartingWithSt = await prisma.user.findMany({
-		// 		where: {
-		// 			name: {
-		// 				startsWith: { mode: "insensitive", value: "st" },
-		// 			},
-		// 		},
-		// 		take: 10,
-		// 		select: {
-		// 			name: true,
-		// 		},
-		// 	});
+	// return candidates for autocompleting a form with suggestions from the pubpub user database
+	// should search by name and email, but probably only return name and id
+	try {
+		const namesStartingWithSt = await prisma.user.findMany({
+			where: {
+				name: {
+					startsWith: "tes",
+				},
+			},
+			take: 10,
+			select: {
+				name: true,
+			},
+		});
 
-		// 	return namesStartingWithSt;
-		// } catch (error) {
-		// 	console.error(error);
-		// 	throw error;
-		// } finally {
-		// 	await prisma.$disconnect();
-		// }
+		return NextResponse.json({ suggestion: namesStartingWithSt });
+	} catch (error) {
+		console.error(error);
+		throw error;
+	} finally {
+		await prisma.$disconnect();
+	}
 }
