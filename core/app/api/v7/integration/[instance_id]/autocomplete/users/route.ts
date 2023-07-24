478062804472938b54262102a54be2a7205c869e
qweliant <dmtorcode@gmail.com>
2023-07-24 13:39:32 -0400

    making route

--- core/app/api/v7/integration/[instance_id]/autocomplete/users/route.ts
+++ core/app/api/v7/integration/[instance_id]/autocomplete/users/route.ts
@@ -1,4 +1,26 @@
+import prisma from "prisma/db";
+
 export async function GET(request: Request) {
     // return candidates for autocompleting a form with suggestions from the pubpub user database
     // should search by name and email, but probably only return name and id
+    	// try {
+		// 	const namesStartingWithSt = await prisma.user.findMany({
+		// 		where: {
+		// 			name: {
+		// 				startsWith: { mode: "insensitive", value: "st" },
+		// 			},
+		// 		},
+		// 		take: 10,
+		// 		select: {
+		// 			name: true,
+		// 		},
+		// 	});
+
+		// 	return namesStartingWithSt;
+		// } catch (error) {
+		// 	console.error(error);
+		// 	throw error;
+		// } finally {
+		// 	await prisma.$disconnect();
+		// }
 }
