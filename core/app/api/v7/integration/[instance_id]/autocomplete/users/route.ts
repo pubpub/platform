9a3b6c378c2bccc0164183cbaacd7a42ca95f6dd
qweliant <dmtorcode@gmail.com>
2023-07-24 15:40:25 -0400

    JUST BABBLING LIKE A CICADAAAAAAA

--- core/app/api/v7/integration/[instance_id]/autocomplete/users/route.ts
+++ core/app/api/v7/integration/[instance_id]/autocomplete/users/route.ts
@@ -1,14 +1,15 @@
 import prisma from "prisma/db";
 import { NextResponse, NextRequest } from "next/server";
 
-export async function GET(request: Request) {
+export async function GET(request: NextRequest) {
 	// return candidates for autocompleting a form with suggestions from the pubpub user database
 	// should search by name and email, but probably only return name and id
+	console.log(request);
 	try {
 		const namesStartingWithSt = await prisma.user.findMany({
 			where: {
 				name: {
-					startsWith: "tes",
+					startsWith: `${"request.body"}`,
 				},
 			},
 			take: 10,
