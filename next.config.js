2a7fc280dd08ac2a4e745099274e15d20428281a
Travis Rich <travis.s.rich@gmail.com>
2023-07-30 20:41:01 -0400

    Remove ts check skip

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-	typescript: {
-		// !! WARN !!
-		// Dangerously allow production builds to successfully complete even if
-		// your project has type errors.
-		// !! WARN !!
-		/* Remove this as soon as tr/deploy-spike is merged */
-		ignoreBuildErrors: true,
-	},
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
