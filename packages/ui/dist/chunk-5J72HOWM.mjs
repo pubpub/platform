337a926051e6f82355608ea27df01c1e7f725de3
Travis Rich <travis.s.rich@gmail.com>
2023-08-08 12:02:08 -0400

    Clean /dist

--- packages/ui/dist/chunk-5J72HOWM.mjs
+++
@@ -1,6 +0,0 @@
-import { clsx } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-
-function s(...r){return twMerge(clsx(r))}
-
-export { s as a };
--- packages/ui/dist/popover.mjs
+++
@@ -1,8 +0,0 @@
-import { a } from './chunk-5J72HOWM.mjs';
-import * as i from 'react';
-import * as e from '@radix-ui/react-popover';
-import { jsx } from 'react/jsx-runtime';
-
-var P=e.Root,l=e.Trigger,m=i.forwardRef(({className:a$1,align:r="center",sideOffset:d=4,...n},s)=>jsx(e.Portal,{children:jsx(e.Content,{ref:s,align:r,sideOffset:d,className:a("z-50 w-72 rounded-md border border-slate-200 bg-white p-4 text-slate-950 shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 dark:border-slate-800 dark:bg-slate-950 dark:text-slate-50",a$1),...n})}));m.displayName=e.Content.displayName;
-
-export { P as Popover, m as PopoverContent, l as PopoverTrigger };
