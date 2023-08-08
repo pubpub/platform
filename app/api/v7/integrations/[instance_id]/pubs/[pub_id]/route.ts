0b66efaa4d9c13a5181e7ab5260fc87bfee175a0
Kalil Smith-Nuevelle <kalilsn@gmail.com>
2023-08-08 15:03:48 -0500

    Make update response show latest values

--- app/api/v7/integrations/[instance_id]/pubs/[pub_id]/route.ts
+++ app/api/v7/integrations/[instance_id]/pubs/[pub_id]/route.ts
@@ -1,61 +1,58 @@
 import prisma from "prisma/db";
-import { NextResponse, NextRequest } from 'next/server';
+import { NextResponse, NextRequest } from "next/server";
 
-export async function GET(request: NextRequest, { params }: { params: { pub_id: string } }) {
+const getPubFields = async (pub_id: string) => {
 	const fields = await prisma.pubValue.findMany({
-		where: { pubId: params.pub_id },
-		distinct: ['fieldId'],
+		where: { pubId: pub_id },
+		distinct: ["fieldId"],
 		orderBy: {
-			createdAt: 'desc'
+			createdAt: "desc",
 		},
 		include: {
 			field: {
 				select: {
 					name: true,
-				}
-			}
-		}
-	})
+				},
+			},
+		},
+	});
 
-	// Raw query version that actually uses DISTINCT under the hood
-	//
-	// const fields: any[] = await prisma.$queryRaw`SELECT DISTINCT ON (field_id) pub_values.value, pub_fields.name, MAX(pub_values.created_at) FROM pub_values JOIN pub_fields ON pub_fields.id = pub_values.field_id WHERE pub_id = ${params.pub_id} GROUP BY field_id, pub_values.id, name`
+	return fields.reduce((prev: any, curr) => {
+		prev[curr.field.name] = curr.value;
+		return prev;
+	}, {});
+};
 
-	const pub = fields.reduce((prev: any, curr) => {
-		prev[curr.field.name] = curr.value
-		return prev
-	}, {})
+export async function GET(request: NextRequest, { params }: { params: { pub_id: string } }) {
+	const pub = await getPubFields(params.pub_id);
 	return NextResponse.json(pub);
 }
 
-
-// TODO: integrations will send field ids, not names
 export async function PUT(request: NextRequest, { params }: { params: { pub_id: string } }) {
 	const { fields } = await request.json();
 
-	const fieldNames = Object.keys(fields)
+	const fieldNames = Object.keys(fields);
 
 	const fieldIds = await prisma.pubField.findMany({
 		where: {
 			name: {
-				in: fieldNames
-			}
+				in: fieldNames,
+			},
 		},
 		select: {
 			id: true,
 			name: true,
-		}
+		},
 	});
 
 	const newValues = fieldIds.map((field) => {
 		return {
 			fieldId: field.id,
 			value: fields[field.name],
-		}
+		};
 	});
 
-	//TODO: only return the latest fields in this response too
-	const updatedPub = await prisma.pub.update({
+	await prisma.pub.update({
 		where: { id: params.pub_id },
 		include: {
 			values: true,
@@ -63,11 +60,14 @@ export async function PUT(request: NextRequest, { params }: { params: { pub_id: 
 		data: {
 			values: {
 				createMany: {
-					data: newValues
-				}
-			}
-		}
+					data: newValues,
+				},
+			},
+		},
 	});
 
-	return NextResponse.json(updatedPub);
+	//TODO: we shouldn't query the db twice for this
+	const updatedFields = await getPubFields(params.pub_id);
+
+	return NextResponse.json(updatedFields);
 }
