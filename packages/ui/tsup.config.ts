233f30165ae2471a10ee2f306b5e53348a0bba04
Travis Rich <travis.s.rich@gmail.com>
2023-08-09 14:41:13 -0400

    build simplifications

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -9,7 +9,6 @@ export default defineConfig((options: Options) => ({
 	minify: true,
 	clean: true,
 	external: ["react"],
-	sourcemap: true,
 	esbuildOptions(options) {
 		options.banner = {
 			js: '"use client"',
