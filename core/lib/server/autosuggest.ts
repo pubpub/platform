df035a3055eac96a74abe1ba371422ffea3be85c
qweliant <dmtorcode@gmail.com>
2023-08-16 17:48:27 -0400

    change autosuggestion to autosuggest

---
+++ core/lib/server/autosuggest.ts
@@ -0,0 +1,27 @@
+import prisma from "~/prisma/db";
+import { SuggestedMember } from "~/lib/contracts/resources/autosuggest";
+
+const getMembers = async (input: string): Promise<SuggestedMember[]> => {
+	const membersStartingWithName = await prisma.user.findMany({
+		where: {
+			name: {
+				startsWith: `${input}`,
+				mode: "insensitive",
+			},
+			email: {
+				startsWith: `${input}`,
+				mode: "insensitive",
+			},
+		},
+		take: 10,
+		select: {
+			id: true,
+			name: true,
+			email: true,
+		},
+	});
+
+	return membersStartingWithName;
+};
+
+export const autosuggestQueries = { getMembers };
