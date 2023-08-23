6defb2997960f4f4e9e6a306e5ecf8a1af87080a
qweliant <dmtorcode@gmail.com>
2023-08-22 21:40:35 -0400

    update import

--- core/lib/swagger/swaggerSpec.ts
+++ core/lib/swagger/swaggerSpec.ts
@@ -1,4 +1,4 @@
-import { api } from "../../contract";
+import { api } from "~/lib/contracts";
 import { generateOpenApi } from "@ts-rest/open-api";
 import { OpenAPIObject } from "openapi3-ts/oas30";
 
