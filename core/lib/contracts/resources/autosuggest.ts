f278b5a7a06aa35ea0ca8df05e2afbdc856034cc
Qwelian D Tanner <dmtorcode@tutanota.com>
2023-08-30 11:47:31 -0400

    Update autosuggest.ts

--- core/lib/contracts/resources/autosuggest.ts
+++ core/lib/contracts/resources/autosuggest.ts
@@ -13,7 +13,7 @@ export type SuggestedMember = z.infer<typeof SuggestedMembersSchema>;
 export const autosuggestApi = contract.router({
 	suggestMember: {
 		method: "GET",
-		path: ":instanceId/autosuggest/members/:memberCandidateString",
+		path: "integrations/:instanceId/autosuggest/members/:memberCandidateString",
 		summary: "autosuggest member",
 		description:
 			"A way to autosuggest members so that integrations users can find users or verify they exist. Will return a name for ",
