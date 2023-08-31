3b4bbcc902ca79db380da3deda2ca380ca8ca248
Gabriel Stein <g@gabestein.com>
2023-08-31 17:27:58 -0400

    add bad logout button

--- core/app/c/[communitySlug]/LoginSwitcher.tsx
+++ core/app/c/[communitySlug]/LoginSwitcher.tsx
@@ -1,16 +1,12 @@
 import { getLoginData } from "~/lib/auth/loginData";
 import { Avatar, AvatarFallback, AvatarImage } from "ui";
-import { supabase } from "~/lib/supabase";
+import LogoutButton from "./LogoutButton";
 
 export default async function LoginSwitcher() {
 	const loginData = await getLoginData();
 	if (!loginData) {
 		return null;
 	}
-	const handleSignout = async () => {
-		await supabase.auth.signOut();
-		window.location.href = "/";
-	};
 	return (
 		<div className="bg-white border border-gray-100 rounded-lg flex p-2">
 			<Avatar className="w-9 h-9 mr-2">
@@ -21,7 +17,7 @@ export default async function LoginSwitcher() {
 				<div className="text-xs">{loginData.name}</div>
 				<div className="text-xs text-gray-400">{loginData.email}</div>
 			</div>
-
+			<LogoutButton />
 			<div>
 				<img className="mt-2" src="/icons/ellipsis.svg" />
 			</div>
