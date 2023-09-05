fc03df7b8d6bcebcdee16fc31297ec646357f18b
Gabriel Stein <g@gabestein.com>
2023-09-05 14:24:24 -0400

    remove Head from reset

--- core/app/reset/page.tsx
+++ core/app/reset/page.tsx
@@ -1,10 +1,8 @@
-import Head from "components/Head";
 import ResetForm from "./ResetForm";
 
 export default async function Page() {
 	return (
 		<div className="max-w-lg m-auto">
-			<Head title="Reset Password · PubPub" triggers={[]} />
 			<ResetForm />
 		</div>
 	);
