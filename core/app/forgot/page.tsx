0d58a9631a834e326bbfffe09a400f9a66cfcfaa
Gabriel Stein <g@gabestein.com>
2023-09-06 16:42:48 -0400

    add user layout with basic styles

--- core/app/forgot/page.tsx
+++
@@ -1,9 +0,0 @@
-import ForgotForm from "./ForgotForm";
-
-export default async function Page() {
-	return (
-		<div className="max-w-lg m-auto">
-			<ForgotForm />
-		</div>
-	);
-}
--- core/app/reset/page.tsx
+++
@@ -1,9 +0,0 @@
-import ResetForm from "./ResetForm";
-
-export default async function Page() {
-	return (
-		<div className="max-w-lg m-auto">
-			<ResetForm />
-		</div>
-	);
-}
