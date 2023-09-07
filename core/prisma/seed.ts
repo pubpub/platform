2a20a6c643db3e92d43398f21b673fda252faa74
Kalil Smith-Nuevelle <kalilsn@gmail.com>
2023-09-06 19:32:12 -0500

    Make sure we always get the supabase user during seeding

--- core/prisma/seed.ts
+++ core/prisma/seed.ts
@@ -14,15 +14,18 @@ const prisma = new PrismaClient();
 const supabase = new SupabaseClient(supabaseUrl!, supabaseKey!);
 
 async function createUserMembers(email, password, slug, name, prismaCommunityIds) {
+	let user;
 	const { data, error } = await supabase.auth.admin.createUser({
 		email,
 		password,
 		email_confirm: true,
 	});
 	if (error) {
-		console.log(error);
+		const { data, error } = await supabase.auth.admin.getUserById('d86c5427-2600-4991-a675-7526ec4b9d2f')
+		user = data.user
+	} else {
+		user = data.user;
 	}
-	const { user } = data;
 	await prisma.user.create({
 		data: {
 			id: user ? user.id : undefined,
