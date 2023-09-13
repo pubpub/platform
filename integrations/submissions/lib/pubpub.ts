5f762fa0683c832a5558f8bd3f2109c0fc40e865
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2023-09-13 10:38:34 -0400

    Lazily evaluate environment variables for SDK client and instance db

--- integrations/submissions/lib/pubpub.ts
+++ integrations/submissions/lib/pubpub.ts
@@ -1,5 +1,4 @@
 import { makeClient, Parse } from "@pubpub/sdk";
 import manifest from "pubpub-integration.json";
-import { expect } from "utils";
 
-export const client = makeClient(<Parse<typeof manifest>>manifest, expect(process.env.API_KEY));
+export const client = makeClient(<Parse<typeof manifest>>manifest);
