48cb6efa1ef2cde792c43e45e116691b31ecd5c5
qweliant <dmtorcode@gmail.com>
2023-09-13 11:51:21 -0400

    ok one sec

--- core/app/c/[communitySlug]/members/page.tsx
+++ core/app/c/[communitySlug]/members/page.tsx
@@ -1,3 +1,7 @@
+import { Prisma } from "@prisma/client";
+import prisma
+ from "~/prisma/db";
+const getCommunityMember = Prisma.mem;
 export default function Page() {
 	return (
 		<>
--- core/lib/permissions.ts
+++ core/lib/permissions.ts
@@ -16,3 +16,5 @@ export const getPubUsers = (permissions: PermissionPayload[]) => {
 	}
 	return users;
 };
+
+const getStageConstraints = () => {}
