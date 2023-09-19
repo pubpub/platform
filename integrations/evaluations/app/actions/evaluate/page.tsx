1cb5eb5f493ef08684e627fec51a6d0e0f1c3e0a
Gabriel Stein <g@gabestein.com>
2023-09-19 10:41:02 -0400

    change pubdata to pub

--- integrations/evaluations/app/actions/evaluate/page.tsx
+++ integrations/evaluations/app/actions/evaluate/page.tsx
@@ -11,7 +11,7 @@ type Props = {
 
 export default async function Page(props: Props) {
 	const { instanceId, pubId } = props.searchParams;
-	const pubData = await client.read(instanceId, pubId);
+	const pub = await client.read(instanceId, pubId);
 
-	return <Evaluate instanceId={instanceId} pubData={pubData} />;
+	return <Evaluate instanceId={instanceId} pub={pub} />;
 }
