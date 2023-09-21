f106528604eea63aad19aec9516c93191957f60c
eric <eric.g.mcdaniel@gmail.com>
2023-09-21 19:20:30 -0400

    Fix prod urls

--- packages/sdk/src/manifest.ts
+++ packages/sdk/src/manifest.ts
@@ -1,12 +1,10 @@
 export type ManifestJson = {
-	url: string;
 	read?: { [key: string]: { id: string } } | string;
 	write?: { [key: string]: { id: string } } | string;
 	register?: { [key: string]: { id: string } };
 };
 
 export type Manifest = {
-	url: string;
 	read?: { [key: string]: { id: string } } | "*";
 	write?: { [key: string]: { id: string } } | "*";
 	register?: { [key: string]: { id: string } };
