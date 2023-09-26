77bcb20e61ff16a130c757fdb40d1671da6fafc5
Gabriel Stein <g@gabestein.com>
2023-09-26 16:40:32 -0600

    fix a type error

--- integrations/evaluations/app/actions/evaluate/evaluate.tsx
+++ integrations/evaluations/app/actions/evaluate/evaluate.tsx
@@ -1,6 +1,4 @@
 "use client";
-import Ajv2020 from "ajv/dist/2020";
-const ajv = new Ajv2020();
 import { ajvResolver } from "@hookform/resolvers/ajv";
 import { useEffect } from "react";
 import { useForm } from "react-hook-form";
@@ -29,6 +27,7 @@ import { cn } from "utils";
 import * as z from "zod";
 import { evaluate } from "./actions";
 import { GetPubResponseBody, GetPubTypeResponseBody } from "@pubpub/sdk";
+import { JSONSchemaType } from "ajv";
 
 type Props = {
 	instanceId: string;
@@ -40,12 +39,16 @@ const schema = z.object({
 	description: z.string().min(1, "Description is required"),
 });
 
+interface pubpubSchema {}
+
 const buildFormSchemaFromFields = (pubType) => {
 	const schema = {
 		$id: `urn:uuid:${pubType.id}`,
 		title: `${pubType.name}`,
 		type: "object",
 		properties: {},
+		// this is required due to an AJV bug, per: https://github.com/ajv-validator/ajv/issues/2317
+		oneOf: [],
 	};
 
 	pubType.fields.forEach((field) => {
@@ -63,7 +66,8 @@ const buildFormSchemaFromFields = (pubType) => {
 	return schema;
 };
 
-const getFormField = (schemaType: "string" | "array" | "number", field) => {
+// todo: array, and more complex types that we might want to handle
+const getFormField = (schemaType: "string" | "number", field) => {
 	switch (schemaType) {
 		case "number":
 			return (
@@ -111,7 +115,8 @@ const buildFormFromSchema = (schema, form, schemaIndex?: number, title?: string)
 export function Evaluate(props: Props) {
 	const { pub, pubType } = props;
 
-	const generatedSchema = buildFormSchemaFromFields(pubType);
+	const generatedSchema: JSONSchemaType<pubpubSchema> = buildFormSchemaFromFields(pubType);
+	console.log(JSON.stringify(generatedSchema));
 
 	const form = useForm({
 		mode: "onChange",
