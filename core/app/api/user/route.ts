60352087636ce25a174c461ecb08ad16afac1591
Kalil Smith-Nuevelle <kalilsn@gmail.com>
2023-10-11 10:06:32 -0500

    Allow users with db records to claim supabase accounts

--- core/app/api/user/route.ts
+++ core/app/api/user/route.ts
@@ -5,6 +5,7 @@ import { getServerSupabase } from "lib/supabaseServer";
 import { generateHash, getSlugSuffix, slugifyString } from "lib/string";
 import { getLoginId } from "lib/auth/loginId";
 import { BadRequestError, ForbiddenError, UnauthorizedError, handleErrors } from "~/lib/server";
+import { captureException } from "@sentry/nextjs";
 
 export type UserPostBody = {
 	firstName: string;
@@ -23,6 +24,17 @@ export async function POST(req: NextRequest) {
 		const submittedData: UserPostBody = await req.json();
 		const { firstName, lastName, email, password } = submittedData;
 		const supabase = getServerSupabase();
+
+		const existingUser = await prisma.user.findUnique({
+			where: {
+				email
+			}
+		})
+
+		if (existingUser?.supabaseId) {
+			throw new ForbiddenError("User already exists")
+		}
+
 		const { data, error } = await supabase.auth.signUp({
 			email,
 			password,
@@ -48,24 +60,39 @@ export async function POST(req: NextRequest) {
 				session: null
 			}
 		*/
+
+		console.log("Supabase user", data);
 		if (error || !data.user) {
-			console.error("Supabase createUser error:");
-			console.error(error);
+			console.error("Supabase createUser error: ", error);
+			captureException(error);
 			return NextResponse.json({ message: "Supabase createUser error" }, { status: 500 });
 		}
-		await prisma.user.create({
-			data: {
-				id: data.user.id,
-				slug: `${slugifyString(firstName)}${
-					lastName ? `-${slugifyString(lastName)}` : ""
-				}-${generateHash(4, "0123456789")}`,
-				firstName,
-				lastName: lastName || undefined,
-				email,
-			},
-		});
 
-		return NextResponse.json({}, { status: 201 });
+		if (existingUser) {
+			await prisma.user.update({
+				where: {
+					email
+				},
+				data: {
+					supabaseId: data.user.id
+				}
+			})
+			return NextResponse.json({ message: "Existing account claimed" }, { status: 200 })
+		} else {
+			const newUser = await prisma.user.create({
+				data: {
+					id: data.user.id,
+					supabaseId: data.user.id,
+					slug: `${slugifyString(firstName)}${
+						lastName ? `-${slugifyString(lastName)}` : ""
+					}-${generateHash(4, "0123456789")}`,
+					firstName,
+					lastName: lastName || undefined,
+					email,
+				},
+			});
+			return NextResponse.json({}, { status: 201 });
+		}
 	});
 }
 
