a258a9a6d2b2c8864a5c45539e7ff1899ae62e85
qweliant <dmtorcode@gmail.com>
2023-10-11 10:54:51 -0400

    rebase

--- core/app/docs/SwaggerUi.tsx
+++ core/app/docs/SwaggerUi.tsx
@@ -1,13 +1,19 @@
 "use client";
 import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
+import { API } from "@stoplight/elements";
+import "@stoplight/elements/styles.min.css";
 
 type Props = {
 	spec: Record<string, any>;
 };
 
 function ReactSwagger({ spec }: Props) {
-	return <SwaggerUI spec={spec} />;
+	return (
+		<>
+			<API apiDescriptionDocument={spec} />
+		</>
+	);
 }
 
 export default ReactSwagger;
