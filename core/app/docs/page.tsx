b977679168dc98f43e1b8fdc5c2966713a6982b0
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2023-10-16 12:58:53 -0400

    Force dynamic on docs page for prod build

--- core/app/docs/page.tsx
+++ core/app/docs/page.tsx
@@ -1,6 +1,8 @@
 import DocumentationUI from "./DocumentationUI";
 import { openApiDocument } from "lib/swagger/swaggerSpec";
 
+export const dynamic = "force-dynamic";
+
 export default async function IndexPage() {
 	return (
 		<section className="container">
