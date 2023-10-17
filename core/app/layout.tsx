73e885e62745458060016faaf4b205802bf479bb
qweliant <dmtorcode@gmail.com>
2023-10-17 17:49:22 -0400

    this is a start?

--- core/app/layout.tsx
+++ core/app/layout.tsx
@@ -2,19 +2,32 @@ import "ui/styles.css";
 import "./globals.css";
 import InitClient from "./InitClient";
 import { Toaster } from "ui";
+import Header from "./header";
+import { Inter } from "next/font/google";
 
 export const metadata = {
 	title: "PubPub v7 Mockup Demo",
 	description: "Just a demo to show the models and structure.",
 };
 
+const inter = Inter({
+	subsets: ["latin"],
+	variable: "--font-inter",
+	display: "swap",
+});
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
-			<body>
-				<InitClient />
-				{children}
-				<Toaster />
+			<body
+				className={`${inter.variable} font-inter antialiased bg-white text-gray-900 tracking-tight`}
+			>
+				<div className="flex flex-col min-h-screen overflow-hidden supports-[overflow:clip]:overflow-clip">
+					<Header />
+					<InitClient />
+					{children}
+					<Toaster />
+				</div>
 			</body>
 		</html>
 	);
