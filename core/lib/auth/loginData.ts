29ed4df5990e1664b35a4a0da367c68337e887ef
qweliant <dmtorcode@gmail.com>
2023-11-07 15:37:12 -0500

    rmv not needed func

--- core/lib/auth/loginData.ts
+++ core/lib/auth/loginData.ts
@@ -60,25 +60,3 @@ export const getLoginData = cache(async () => {
 	}
 	return user;
 });
-
-export const handleRedirect = async () => {
-	const loginData = await getLoginData();
-
-	if (loginData) {
-		/* If we have a logged in user navigating to `/`, check */
-		/* if they are a member of any community, and if so,    */
-		/* redirect them to that community by default. We could */
-		/* eventually have a query param override, but this     */
-		/* assumes that logged in users landing on pubpub.org   */
-		/* want to go to their dashboard a la github or twitter */
-		/* TODO: Does not select for member-group access yet */
-		const community = await prisma.community.findFirst({
-			where: { members: { some: { userId: loginData.id } } },
-			select: { slug: true },
-		});
-		// console.log("community", community);
-		// if (community) {
-		// 	redirect(`/c/${community.slug}`);
-		// }
-	}
-};
