94758ab462822b8ad83cea23e4d500b0423d433a
qweliant <dmtorcode@gmail.com>
2023-11-14 12:30:23 -0500

    rmv stuff

--- core/app/layout.tsx
+++ core/app/layout.tsx
@@ -1,4 +1,3 @@
-import { Inter } from "next/font/google";
 import { Toaster } from "ui";
 import "ui/styles.css";
 import InitClient from "./InitClient";
@@ -9,23 +8,13 @@ export const metadata = {
 	description: "Just a demo to show the models and structure.",
 };
 
-const inter = Inter({
-	subsets: ["latin"],
-	variable: "--font-inter",
-	display: "swap",
-});
-
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
-			<body
-				className={`${inter.variable} font-inter antialiased bg-white text-gray-900 tracking-tight`}
-			>
-				<div className="flex flex-col min-h-screen overflow-hidden supports-[overflow:clip]:overflow-clip">
-					<InitClient />
-					{children}
-					<Toaster />
-				</div>
+			<body>
+				<InitClient />
+				{children}
+				<Toaster />
 			</body>
 		</html>
 	);
