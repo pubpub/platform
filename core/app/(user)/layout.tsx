2fcdf60ab7b31c7c28456def7941c4f11bcaafc4
qweliant <dmtorcode@gmail.com>
2023-11-14 12:59:38 -0500

    Hark Triton, hark! Bellow, bid our father the Sea King rise from the depths full foul in his fury! Black waves teeming with salt foam to smother this young mouth with pungent slime, to choke ye, engorging your organs til’ ye turn blue and bloated with bilge and brine and can scream no more

--- core/app/(user)/layout.tsx
+++ core/app/(user)/layout.tsx
@@ -1,6 +1,9 @@
 export default function UserLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<div className="grow">
+		<div className="min-h-screen">
+			<div className="container mx-auto py-5">
+				<img src="/logos/icon.svg" className="w-6" />
+			</div>
 			<div className="container mx-auto">{children}</div>
 		</div>
 	);
