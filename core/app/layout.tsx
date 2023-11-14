81e67d72fb432415441ab387f4013462a325f383
qweliant <dmtorcode@gmail.com>
2023-11-14 16:20:08 -0500

    forgotten to any man, to any time, forgotten to any god or devil, forgotten even to the sea, for any stuff for part of Winslow, even any scantling of your soul is Winslow no more, but is now itself the sea

--- core/app/layout.tsx
+++ core/app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata = {
 	description: "Just a demo to show the models and structure.",
 };
 
-export default async function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
 			<body>
