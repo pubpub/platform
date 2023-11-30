3c12febc98b16ab840922c1410afb02dae498a5a
Kalil Smith-Nuevelle <kalilsn@gmail.com>
2023-11-30 11:39:13 -0600

    Prevent use of process.env

--- core/lib/env/env.ts
+++ core/lib/env/env.ts
@@ -1,4 +1,5 @@
 export const getEnvVar = (varName) => {
+	// eslint-disable-next-line no-process-env
 	const value = process.env[varName];
 	if (!value) {
 		throw new Error(`Environment variable ${varName} not found`);
