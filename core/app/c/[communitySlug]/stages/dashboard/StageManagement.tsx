75b11a5d62e6a59203b4aa5a9793fcbb4d206804
qweliant <dmtorcode@gmail.com>
2023-12-03 08:55:13 -0500

    add em req changes

--- core/app/c/[communitySlug]/stages/dashboard/StageManagement.tsx
+++ core/app/c/[communitySlug]/stages/dashboard/StageManagement.tsx
@@ -13,7 +13,7 @@ export default function StageManagement(props: Props) {
 	const [tab, setTab] = useState<number>(1);
 	return (
 		<Tabs defaultValue={tab.toString()} className="pt-12 md:pt-20">
-			<TabsList className="mb-6">
+			<TabsList className="mb-6 ">
 				<TabsTrigger
 					onClick={(e) => {
 						e.preventDefault();
@@ -22,8 +22,8 @@ export default function StageManagement(props: Props) {
 					value="1"
 				>
 					<div>
-						<p className="font-bold tracking-tight mb-1 text-left">Edit Stages</p>
-						<p className="text-gray-600">Edit your current stages</p>
+						<p className="font-bold text-left">Edit Stages</p>
+						<p>Edit your current stages</p>
 					</div>
 				</TabsTrigger>
 				<TabsTrigger
@@ -34,25 +34,19 @@ export default function StageManagement(props: Props) {
 					value="2"
 				>
 					<div>
-						<p className="font-bold tracking-tight mb-1 text-left">Create new Stages</p>
-						<p className="text-gray-600">create new stages</p>
+						<p className="font-bold text-left">Create new Stages</p>
+						<p>create new stages</p>
 					</div>
 				</TabsTrigger>
 			</TabsList>
-			<TabsContent
-				value="1"
-				className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-2 lg:col-span-2 mb-8 md:mb-0 md:order-1"
-			>
-				<div className="relative flex flex-col text-center lg:text-left">
+			<TabsContent value="1">
+				<div className="relative flex flex-col text-left lg:text-left">
 					<div className="relative inline-flex flex-col">
 						<StagesEditor stages={props.stages} />
 					</div>
 				</div>
 			</TabsContent>
-			<TabsContent
-				value="2"
-				className="max-w-xl md:max-w-none md:w-full mx-auto md:col-span-2 lg:col-span-2 mb-8 md:mb-0 md:order-1"
-			>
+			<TabsContent value="2">
 				<div className="relative inline-flex flex-col max-w-lg">
 					<Input placeholder="Stage Name" />
 					<Button> Create stage </Button>
