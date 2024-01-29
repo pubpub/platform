54b450fb347829068561d1656d1857edb1fa5afc
Kalil Smith-Nuevelle <kalilsn@gmail.com>
2024-01-29 10:26:04 -0600

    Add kysely types

---
+++ core/kysely/database.ts
@@ -0,0 +1,17 @@
+import Database from "./types/Database";
+import { Pool } from "pg";
+import { Kysely, PostgresDialect } from "kysely";
+
+const dialect = new PostgresDialect({
+	pool: new Pool({
+		connectionString: process.env["DATABASE_URL"],
+	}),
+});
+
+// Database interface is passed to Kysely's constructor, and from now on, Kysely
+// knows your database structure.
+// Dialect is passed to Kysely's constructor, and from now on, Kysely knows how
+// to communicate with your database.
+export const db = new Kysely<Database>({
+	dialect,
+});
