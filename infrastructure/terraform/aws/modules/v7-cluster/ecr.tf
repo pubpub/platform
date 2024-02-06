54cc9d8a3e588a6ee064c512db3aea4cfd9f1214
eve n.u <eve@eatbigger.fish>
2024-02-06 15:21:59 -0700

    add repositories for component-based containers

--- infrastructure/terraform/aws/modules/v7-cluster/ecr.tf
+++ infrastructure/terraform/aws/modules/v7-cluster/ecr.tf
@@ -7,3 +7,21 @@ resource "aws_ecr_repository" "pubpub_v7" {
     scan_on_push = false # can set this to true if we want
   }
 }
+
+resource "aws_ecr_repository" "pubpub_v7_core" {
+  name                 = "pubpub-v7-core"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false # can set this to true if we want
+  }
+}
+
+resource "aws_ecr_repository" "pubpub_v7_intg_submissions" {
+  name                 = "pubpub-v7-integration-submissions"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false # can set this to true if we want
+  }
+}
