aa4fccd40bd719ee9fa730b11d1e506303e24757
Kalil Smith-Nuevelle <kalilsn@gmail.com>
2024-02-08 13:13:52 -0600

    Add kysely query logging example

--- core/kysely/database.ts
+++ core/kysely/database.ts
@@ -14,4 +14,10 @@ const dialect = new PostgresDialect({
 // to communicate with your database.
 export const db = new Kysely<Database>({
 	dialect,
+	// log(event) {
+	// if (event.level === "query") {
+	// 	console.log(event.query.sql);
+	// 	console.log(event.query.parameters);
+	// }
+	// },
 });
