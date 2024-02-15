acdd6970f4cb11d1a033d4c65bc2b817b9ca1b57
gabestein <gabe@ghscommunications.com>
2024-02-15 17:27:03 -0500

    make language tweaks

--- integrations/evaluations/lib/components/Research.tsx
+++ integrations/evaluations/lib/components/Research.tsx
@@ -1,3 +1,5 @@
+import { Card, CardContent } from "ui";
+
 export type Props = {
 	title: string;
 	abstract: string;
@@ -9,7 +11,10 @@ export const Research = (props: Props) => {
 	return (
 		<>
 			{props.evaluating ? (
-				<h2>To evaluate</h2>
+				<>
+					<h2>The Unjournal: Evaluation Form</h2>
+					<h3>You have been asked to evaluate the following research:</h3>
+				</>
 			) : (
 				<>
 					<h2>About the research</h2>
@@ -18,23 +23,27 @@ export const Research = (props: Props) => {
 					</p>
 				</>
 			)}
-			<h3>{props.title}</h3>
-			{props.abstract ? (
-				<p>
-					<strong>Abstract:</strong> {props.abstract}
-				</p>
-			) : (
-				<p>
-					<em>No abstract provided.</em>
-				</p>
-			)}
-			{props.url && (
-				<p>
-					<a target="_blank" href={props.url}>
-						Link to research
-					</a>
-				</p>
-			)}
+			<Card>
+				<CardContent>
+					<h3>{props.title}</h3>
+					{props.abstract ? (
+						<p>
+							<strong>Abstract:</strong> {props.abstract}
+						</p>
+					) : (
+						<p>
+							<em>No abstract provided.</em>
+						</p>
+					)}
+					{props.url && (
+						<p>
+							<a target="_blank" href={props.url}>
+								View or download the original.
+							</a>
+						</p>
+					)}
+				</CardContent>
+			</Card>
 		</>
 	);
 };
