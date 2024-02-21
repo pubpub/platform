5687898fa69c748799c98164c6c242a428f0f1b3
Thomas F. K. Jorna <hello@tefkah.com>
2024-02-21 12:38:31 +0100

    fix: remove bad import and test ci var

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,5 +1,7 @@
 set -euxo pipefail
 
+echo "Checking CI: $CI"
+
 npm i -g pnpm@8.7.0
 pnpm install
 pnpm p:build
--- integrations/submissions/lib/env.mjs
+++ integrations/submissions/lib/env.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 
 import { createEnv } from "@t3-oss/env-nextjs";
-import { isBuilding } from "utils";
 import { z } from "zod";
 
 export const env = createEnv({
