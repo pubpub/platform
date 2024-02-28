7ce4f4a96af8c7b06dcfec5248cd864e8e6077c7
Thomas F. K. Jorna <hello@tefkah.com>
2024-02-28 13:41:04 +0100

    feat: add very basic list of existing members

--- core/app/c/[communitySlug]/members/page.tsx
+++ core/app/c/[communitySlug]/members/page.tsx
@@ -1,7 +1,48 @@
-export default function Page() {
+import { Card, CardContent } from "ui";
+import prisma from "~/prisma/db";
+import Image from "next/image";
+
+export default async function Page({
+	params: { communitySlug },
+}: {
+	params: {
+		communitySlug: string;
+	};
+}) {
+	const existingMembers = await prisma.member.findMany({
+		where: {
+			community: {
+				slug: communitySlug,
+			},
+		},
+		include: {
+			user: true,
+		},
+	});
+
 	return (
 		<>
-			<h1>Members</h1>
+			<div className="flex mb-16 justify-between items-center">
+				<h1 className="font-bold text-xl">Members</h1>
+				{/* <Link href="stages/dashboard">Manage Stages</Link> */}
+			</div>
+			<Card>
+				<CardContent className="flex flex-col gap-y-10 py-4">
+					{existingMembers.map(({ user, id, canAdmin, createdAt }) => (
+						<div key={id} className="flex gap-x-4 items-center">
+							<Image src={user.avatar} width="50" height="50" />
+							<div className="flex flex-col gap-2">
+								<span>
+									{user.firstName} {user.lastName}
+								</span>
+								<span className="text-sm">
+									Joined: {new Date(createdAt).toLocaleDateString()}
+								</span>
+							</div>
+						</div>
+					))}
+				</CardContent>
+			</Card>
 		</>
 	);
 }
