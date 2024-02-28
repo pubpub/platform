ab21a338621a5afee1c0175e4fbcc566f13f72d9
qweliant <dmtorcode@gmail.com>
2024-02-28 13:14:11 -0500

    simplify

--- packages/ui/src/circle.tsx
+++ packages/ui/src/circle.tsx
@@ -2,9 +2,8 @@ import React from "react";
 
 export function Circle({ text }: { text: string }) {
 	return (
-		<span className="relative w-20px h-20px flex justify-center items-center">
-			<span className="absolute bg-gray-500 rounded-full w-full h-full"></span>
-			<p className="relative z-10 text-white text-sm p-1">{text}</p>
+		<span className="w-20px h-20px bg-gray-500 rounded-full flex justify-center items-center">
+			<p className="text-white text-sm p-1">{text}</p>
 		</span>
 	);
 }
