3bfd03469112caf086bb5c8b5adebd039acf56e2
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2024-02-29 14:52:23 -0500

    Actions pattern draft

---
+++ core/actions/init.ts
@@ -0,0 +1,10 @@
+import { CoreField } from "./fields";
+import { Action } from "./types";
+
+export const registerCoreField = async (field: CoreField) => {
+	// Ensure the field exists in the database and is up to date
+};
+
+export const registerAction = async (action: Action) => {
+	// Ensure the action exists in the database and is up to date
+};
--- core/instrumentation.ts
+++ core/instrumentation.ts
@@ -1,8 +1,14 @@
-// Example filename: tracing.ts
+import { registerAction } from "./actions/init";
+import { actions } from "./actions";
 
 import { logger } from "logger";
 
 export async function register() {
+	logger.info("Registering actions");
+	for (const action of actions) {
+		logger.info(`Registering action ${action.name}`);
+		await registerAction(action);
+	}
 	logger.info(`Registering instrumentation hook for ${process.env.NEXT_RUNTIME}`);
 	if (process.env.NEXT_RUNTIME === "nodejs") {
 		await import("./instrumentation.node");
