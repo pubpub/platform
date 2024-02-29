fe89db8818a13c92b91851ec1e1a232fcb0fb623
qweliant <dmtorcode@gmail.com>
2024-02-29 13:32:30 -0500

    use face

--- core/app/c/[communitySlug]/stages/components/Assign.tsx
+++ core/app/c/[communitySlug]/stages/components/Assign.tsx
@@ -51,7 +51,10 @@ export default function Assign(props: Props) {
 			variant: "default",
 		});
 	};
-
+	const member = props.members.find((member) => member.user.id === props.loginData.id)!;
+	const memberIntials = member.user.lastName
+		? `${member.user.firstName[0]} ${member.user.lastName[0]}`
+		: `${member.user.firstName[0]}`;
 	return (
 		<Popover>
 			<PopoverTrigger asChild>
@@ -65,71 +68,80 @@ export default function Assign(props: Props) {
 					className="mb-5"
 					onClick={() => onAssign(props.pub.id, props.loginData.id, props.stage.id)}
 				>
-					Self assign
+					<div className="mr-4">
+						<Avatar className="rounded w-9 h-9 mr-2">
+							<AvatarFallback>{memberIntials}</AvatarFallback>
+						</Avatar>
+					</div>
+					<p>
+						{member.user.firstName} {member.user.lastName}
+					</p>
 				</Button>
-				{props.members.map((member) => {
-					const intials = member.user.lastName
-						? `${member.user.firstName[0]} ${member.user.lastName[0]}`
-						: `${member.user.firstName[0]}`;
-					return (
-						<Dialog
-							open={open && selectedUserId === member.user.id}
-							onOpenChange={setOpen}
-							key={member.user.id}
-						>
-							<DialogTrigger>
-								<Button
-									variant="ghost"
-									className="p-1"
-									onClick={() => setSelectedUserid(member.user.id)}
-								>
-									<div className="mr-4">
-										<Avatar className="rounded w-9 h-9 mr-2">
-											<AvatarFallback>{intials}</AvatarFallback>
-										</Avatar>
-									</div>
-									<p>
-										{member.user.firstName} {member.user.lastName}
-									</p>
-								</Button>
-							</DialogTrigger>
-							<DialogContent>
-								<Card>
-									<CardTitle className="space-y-1.5 p-6">
-										Assign <i>{getTitle(props.pub)}</i> to{" "}
-										{member.user.firstName} {member.user.lastName}?
-									</CardTitle>
-									<CardContent>
-										{member.user.firstName} {member.user.lastName} will be
-										notified that they have been assigned to this Pub.
-									</CardContent>
-									<CardFooter className="flex flex-row">
-										<Button
-											variant="default"
-											onClick={(event) => {
-												onAssign(
-													props.pub.id,
-													member.user.id,
-													props.stage.id
-												).then(() => setOpen(false));
-												event.preventDefault();
-											}}
-										>
-											Assign
-										</Button>
-										<Button
-											className="mx-3"
-											variant="secondary"
-											onClick={() => setOpen(false)}
-										>
-											Cancel
-										</Button>
-									</CardFooter>
-								</Card>
-							</DialogContent>
-						</Dialog>
-					);
-				})}
+				{props.members
+					.filter((member) => member.user.id !== props.loginData.id)
+					.map((member) => {
+						const intials = member.user.lastName
+							? `${member.user.firstName[0]} ${member.user.lastName[0]}`
+							: `${member.user.firstName[0]}`;
+						return (
+							<Dialog
+								open={open && selectedUserId === member.user.id}
+								onOpenChange={setOpen}
+								key={member.user.id}
+							>
+								<DialogTrigger>
+									<Button
+										variant="ghost"
+										className="p-1"
+										onClick={() => setSelectedUserid(member.user.id)}
+									>
+										<div className="mr-4">
+											<Avatar className="rounded w-9 h-9 mr-2">
+												<AvatarFallback>{intials}</AvatarFallback>
+											</Avatar>
+										</div>
+										<p>
+											{member.user.firstName} {member.user.lastName}
+										</p>
+									</Button>
+								</DialogTrigger>
+								<DialogContent>
+									<Card>
+										<CardTitle className="space-y-1.5 p-6">
+											Assign <i>{getTitle(props.pub)}</i> to{" "}
+											{member.user.firstName} {member.user.lastName}?
+										</CardTitle>
+										<CardContent>
+											{member.user.firstName} {member.user.lastName} will be
+											notified that they have been assigned to this Pub.
+										</CardContent>
+										<CardFooter className="flex flex-row">
+											<Button
+												variant="default"
+												onClick={(event) => {
+													onAssign(
+														props.pub.id,
+														member.user.id,
+														props.stage.id
+													).then(() => setOpen(false));
+													event.preventDefault();
+												}}
+											>
+												Assign
+											</Button>
+											<Button
+												className="mx-3"
+												variant="secondary"
+												onClick={() => setOpen(false)}
+											>
+												Cancel
+											</Button>
+										</CardFooter>
+									</Card>
+								</DialogContent>
+							</Dialog>
+						);
+					})}
 			</PopoverContent>
 		</Popover>
 	);
