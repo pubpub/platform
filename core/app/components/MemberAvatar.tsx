02ba211c79a64fdb41aeed0560586ef3ea715731
qweliant <dmtorcode@gmail.com>
2024-03-04 10:50:16 -0500

    update avatar styling

--- core/app/components/MemberAvatar.tsx
+++ core/app/components/MemberAvatar.tsx
@@ -4,7 +4,7 @@ import { PubPayload } from "~/lib/types";
 function MembersAvatars({ pub }: { pub: PubPayload }) {
 	return (
 		pub.claims.length > 0 && (
-			<div>
+			<>
 				<p className="font-bold pb-1">Assigned to:</p>
 				<div className="ml-4">
 					{pub.claims.map((claim) => {
@@ -12,12 +12,10 @@ function MembersAvatars({ pub }: { pub: PubPayload }) {
 							? `${claim.user.firstName[0]} ${claim.user.lastName[0]}`
 							: `${claim.user.firstName[0]}`;
 						return (
-							<div className="flex flex-row p-1">
-								<div className="mr-4">
-									<Avatar>
-										<AvatarFallback>{intials}</AvatarFallback>
-									</Avatar>
-								</div>
+							<div className="flex flex-row items-center space-x-3">
+								<Avatar>
+									<AvatarFallback>{intials}</AvatarFallback>
+								</Avatar>
 								<p className="font-medium" key={claim.id}>
 									{claim.user.firstName} {claim.user.lastName}{" "}
 								</p>
@@ -25,7 +23,7 @@ function MembersAvatars({ pub }: { pub: PubPayload }) {
 						);
 					})}
 				</div>
-			</div>
+			</>
 		)
 	);
 }
