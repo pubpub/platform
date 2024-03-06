a98337a6a531509f6a3a1ac514d3de6fd6d9334b
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2024-03-06 12:49:01 -0500

    Simplify example

--- core/actions/log/run.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-
-import { RunProps } from "../types";
-import type { RunConfig, RunPubType } from "./action";
-
-export async function run({ pub, config }: RunProps<RunPubType, RunConfig>) {
-	// Do something with `pub` and `config`
-}
--- core/instrumentation.ts
+++ core/instrumentation.ts
@@ -2,7 +2,7 @@ import { registerAction } from "./actions/init";
 import { actions } from "./actions";
 
 export async function register() {
-	for (const action of actions) {
+	for (const { action } of Object.values(actions)) {
 		console.log(`Registering action ${action.name}`);
 		await registerAction(action);
 	}
