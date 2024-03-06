063b798358fdca34903d4c4a307840287598008f
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2024-03-06 12:49:01 -0500

    Simplify example

--- core/actions/log/action.tsx
+++ core/actions/log/action.tsx
@@ -1,36 +1,14 @@
 import * as z from "zod";
 import * as fields from "../fields";
-import { ConfigProps, RunProps, defineAction } from "../types";
-import { run } from "./run";
+import { defineAction } from "../types";
 
-export type RunPubType = [typeof fields.title];
-export type RunConfig = { debounce?: number };
-
-const Run = (props: RunProps<RunPubType, RunConfig>) => {
-	const onClick = async () => {
-		try {
-			await run(props);
-		} catch (e) {
-			console.error(e);
-		}
-	};
-	return <button onClick={onClick}>Run</button>;
-};
-
-const Configure = (props: ConfigProps<RunConfig>) => {
-	// Render form using `props.config`
-	return <div>Configure</div>;
-};
-
-export const log = defineAction({
+export const action = defineAction({
 	name: "log",
 	fields: [fields.title],
 	config: z.object({
 		debounce: z.number().optional(),
 	}),
-	run,
-	ui: {
-		Configure,
-		Run,
-	},
+	pubConfig: z.object({}),
 });
+
+export { run } from "./run";
