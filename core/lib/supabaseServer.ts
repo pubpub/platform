f2302e245fcdb1a4daa2462ca83503d683a722ed
Thomas F. K. Jorna <hello@tefkah.com>
2024-03-12 10:40:34 +0100

    feat: add manual getUserByEmail method on supabase server client

--- core/lib/supabaseServer.ts
+++ core/lib/supabaseServer.ts
@@ -1,5 +1,5 @@
 import "server-only";
-import { createClient } from "@supabase/supabase-js";
+import { User, createClient } from "@supabase/supabase-js";
 import { env } from "./env/env.mjs";
 
 export const getServerSupabase = () => {
@@ -8,10 +8,27 @@ export const getServerSupabase = () => {
 	if (!url || !key) {
 		throw new Error("Missing Supabase parameters");
 	}
-	return createClient(url, key, {
+	const client = createClient(url, key, {
 		auth: {
 			autoRefreshToken: true,
 			persistSession: true /* Persisting session is necessary for autoRefresh to function */,
 		},
 	});
+
+	/**
+	 * This is a custom method to get users by email, which for some reason is not available in the official Supabase client
+	 */
+	// @ts-expect-error
+	client.auth.admin.getUserByEmail = (email: string) =>
+		fetch(`${env.NEXT_PUBLIC_SUPABASE_URL}/auth/v1/admin/users?filter=${email}`, {
+			headers: {
+				Authorization: `Bearer ${env.SUPABASE_SERVICE_ROLE_KEY}`,
+			},
+		})
+			.then((res) => res.json())
+			.then((res) => res?.users?.find((u: User) => u.email === email));
+
+	return client as typeof client & {
+		auth: { admin: { getUserByEmail: (email: string) => Promise<User | undefined> } };
+	};
 };
