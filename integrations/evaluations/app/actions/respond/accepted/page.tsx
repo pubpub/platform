bdef9e9144e47d0347a67dc105665a54dedc646c
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2024-03-13 16:02:44 -0400

    Show submission title on accepted page

--- integrations/evaluations/app/actions/respond/accepted/page.tsx
+++ integrations/evaluations/app/actions/respond/accepted/page.tsx
@@ -1,4 +1,7 @@
 import { notFound } from "next/navigation";
+import { expect } from "utils";
+import { getInstanceConfig } from "~/lib/instance";
+import { client } from "~/lib/pubpub";
 
 type Props = {
 	searchParams: {
@@ -7,17 +10,29 @@ type Props = {
 	};
 };
 
-export default function Page(props: Props) {
+export default async function Page(props: Props) {
 	const { instanceId, pubId } = props.searchParams;
 	if (!(instanceId && pubId)) {
 		notFound();
 	}
+	const instanceConfig = expect(await getInstanceConfig(instanceId), "Instance not configured");
+	const pub = await client.getPub(instanceId, pubId);
+	const submissionUrl = pub.values["unjournal:url"] as string;
+	const submissionTitle = pub.values[instanceConfig.titleFieldSlug] as string;
+
 	// TODO: Get copy from Jeff/Gabe
 	return (
 		<div className="prose max-w-none">
 			<p>
-				Thanks for accepting! We'll send you an email with instructions on how to submit
-				your evaluation.
+				Thanks for accepting the invite to evaluate{" "}
+				<a target="_blank" href={submissionUrl}>
+					"{submissionTitle}"
+				</a>{" "}
+				for{" "}
+				<a target="_blank" href="https://unjournal.org">
+					<em>The Unjournal</em>
+				</a>
+				! We'll send you an email with instructions on how to submit your evaluation.
 			</p>
 		</div>
 	);
