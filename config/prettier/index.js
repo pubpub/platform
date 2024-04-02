88d67ba2f833e9285127a89484e679223c3153f2
Thomas F. K. Jorna <hello@tefkah.com>
2024-04-02 18:35:19 +0200

    feat: add prettier config

---
+++ config/prettier/index.js
@@ -0,0 +1,44 @@
+import { fileURLToPath } from "url";
+
+/** @typedef {import("prettier").Config} PrettierConfig */
+/** @typedef {import("prettier-plugin-tailwindcss").PluginOptions} TailwindConfig */
+/** @typedef {import("@ianvs/prettier-plugin-sort-imports").PluginConfig} SortImportsConfig */
+
+/** @type { PrettierConfig | SortImportsConfig | TailwindConfig } */
+const config = {
+	arrowParens: "always",
+	printWidth: 100,
+	tabWidth: 4,
+	trailingComma: "es5",
+	useTabs: true,
+	singleQuote: false,
+	plugins: ["@ianvs/prettier-plugin-sort-imports", "prettier-plugin-tailwindcss"],
+	// tailwindConfig: fileURLToPath(
+	//   new URL("../../tooling/tailwind/web.ts", import.meta.url),
+	// ),
+	tailwindFunctions: ["cn", "cva"],
+	importOrder: [
+		"<BUILTIN_MODULES>",
+		"",
+		"<TYPES>",
+		"",
+		"^(react/(.*)$)|^(react$)|^(react-native(.*)$)",
+		"^(next/(.*)$)|^(next$)",
+		"<THIRD_PARTY_MODULES>",
+		"",
+		"<TYPES>^((lib|ui|@pubpub|utils|contracts)/(.*)$)|^((lib|ui|@pubpub/*|utils|contracts)$)",
+		"^((lib|ui|@pubpub|utils|contracts)/(.*)$)|^((lib|ui|@pubpub/*|utils|contracts)$)",
+		"",
+		"<TYPES>^[.|..|~]",
+		"^~/",
+		"^[../]",
+		"^[./]",
+		"",
+		"^(?!.*[.]css$)[./].*$",
+		".css$",
+	],
+	importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+	importOrderTypeScriptVersion: "4.5.0",
+};
+
+export default config;
