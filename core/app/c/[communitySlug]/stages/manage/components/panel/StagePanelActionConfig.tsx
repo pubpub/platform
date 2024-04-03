e021461d263daa2e4524b174d23b95e9688e394a
Kalil Smith-Nuevelle <kalilsn@gmail.com>
2024-04-03 17:54:03 -0500

    Add submit button to autogenerated form

--- core/app/c/[communitySlug]/stages/manage/components/panel/StagePanelActionConfig.tsx
+++ core/app/c/[communitySlug]/stages/manage/components/panel/StagePanelActionConfig.tsx
@@ -1,15 +1,34 @@
 "use client";
 
-import { ZodObject } from "zod";
+import type { ZodObject } from "zod";
 
-import AutoForm from "ui/auto-form";
+import AutoForm, { AutoFormSubmit } from "ui/auto-form";
 
-import { Action } from "~/actions/types";
+import type { Action } from "~/actions/types";
+import type { ActionInstancesId } from "~/kysely/types/public/ActionInstances";
+import type { StagePayloadActionInstance } from "~/lib/types";
+import { updateAction } from "../../actions";
 
 export type Props = {
 	action: Action;
+	instance: StagePayloadActionInstance;
+	communityId: string;
 };
 
 export const StagePanelActionConfig = (props: Props) => {
-	return <AutoForm formSchema={props.action.config as ZodObject<{}>} />;
+	return (
+		<AutoForm
+			values={props.instance.config ?? {}}
+			formSchema={props.action.config as ZodObject<{}>}
+			onSubmit={(instanceConfig) => {
+				updateAction(
+					props.communityId,
+					props.instance.id as ActionInstancesId,
+					instanceConfig
+				);
+			}}
+		>
+			<AutoFormSubmit>Update config</AutoFormSubmit>
+		</AutoForm>
+	);
 };
