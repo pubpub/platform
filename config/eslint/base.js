71aba24f8f0d036bf4fcacf3f4c92b8885922983
Thomas F. K. Jorna <hello@tefkah.com>
2024-04-03 19:25:55 +0200

    chore: reorder uncommented things

--- config/eslint/base.js
+++ config/eslint/base.js
@@ -16,18 +16,18 @@ const config = {
 	plugins: ["@typescript-eslint", "import", "n"],
 	rules: {
 		// "turbo/no-undeclared-env-vars": "error",
-		"n/no-process-env": "error",
 		// "@typescript-eslint/no-unused-vars": [
 		// 	"error",
 		// 	{ argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
 		// ],
+		// "@typescript-eslint/no-misused-promises": [2, { checksVoidReturn: { attributes: false } }],
+		"n/no-process-env": "error",
+		"no-console": "error",
 		"@typescript-eslint/consistent-type-imports": [
 			"warn",
 			{ prefer: "type-imports", fixStyle: "separate-type-imports" },
 		],
-		// "@typescript-eslint/no-misused-promises": [2, { checksVoidReturn: { attributes: false } }],
 		"import/consistent-type-specifier-style": ["error", "prefer-top-level"],
-		"no-console": "error",
 	},
 	ignorePatterns: [
 		"**/*.config.js",
