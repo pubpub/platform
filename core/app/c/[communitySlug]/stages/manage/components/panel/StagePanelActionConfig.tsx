ff1fe589bd0525a59c4e4c520be419a129ce8fb1
Kalil Smith-Nuevelle <kalilsn@gmail.com>
2024-04-10 15:17:39 -0500

    Cache submit handler function

--- core/app/c/[communitySlug]/stages/manage/components/panel/StagePanelActionConfig.tsx
+++ core/app/c/[communitySlug]/stages/manage/components/panel/StagePanelActionConfig.tsx
@@ -2,6 +2,8 @@
 
 import type { ZodObject } from "zod";
 
+import { useCallback } from "react";
+
 import AutoForm, { AutoFormSubmit } from "ui/auto-form";
 
 import type { Action } from "~/actions/types";
@@ -16,17 +18,18 @@ export type Props = {
 };
 
 export const StagePanelActionConfig = (props: Props) => {
+	const onSubmit = useCallback(
+		(instanceConfig) => {
+			updateAction(props.communityId, props.instance.id as ActionInstancesId, instanceConfig);
+		},
+		[props.communityId, props.instance.id]
+	);
+
 	return (
 		<AutoForm
 			values={props.instance.config ?? {}}
 			formSchema={props.action.config as ZodObject<{}>}
-			onSubmit={(instanceConfig) => {
-				updateAction(
-					props.communityId,
-					props.instance.id as ActionInstancesId,
-					instanceConfig
-				);
-			}}
+			onSubmit={onSubmit}
 		>
 			<AutoFormSubmit>Update config</AutoFormSubmit>
 		</AutoForm>
