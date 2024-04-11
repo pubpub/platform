f94075cd8aea6bbed1df4f5e4a6c61149c4e9e8e
Kalil Smith-Nuevelle <kalilsn@gmail.com>
2024-04-10 23:26:14 -0500

    Copy data before deleting old table

--- core/prisma/migrations/20240410233228_remove_implicit_pubs_to_stages_relationship/migration.sql
+++ core/prisma/migrations/20240410233228_remove_implicit_pubs_to_stages_relationship/migration.sql
@@ -4,6 +4,9 @@
   - You are about to drop the `_PubToStage` table. If the table is not empty, all the data it contains will be lost.
 
 */
+
+-- Copy Data
+INSERT INTO "PubsInStages" SELECT "A","B" FROM "_PubToStage";
 -- DropForeignKey
 ALTER TABLE "_PubToStage" DROP CONSTRAINT "_PubToStage_A_fkey";
 
