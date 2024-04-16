17b1edea48cc52b9d255546b26ee52aeabc997d5
qweliant <dmtorcode@gmail.com>
2024-04-16 14:19:38 -0400

    start action

--- core/actions/email/run.ts
+++ core/actions/email/run.ts
@@ -4,9 +4,20 @@ import { logger } from "logger";
 
 import type { action } from "./action";
 import { defineRun } from "../types";
+import { title } from '../corePubFields';
+import { smtpclient } from "~/lib/server/mailgun";
 
 export const run = defineRun<typeof action>(async ({ pub, config, pubConfig }) => {
 	logger.info({ msg: "email", pub, config, pubConfig });
+
+	// query for community?
+	const { accepted, rejected } = await smtpclient.sendMail({
+		from: `<hello@mg.pubpub.org>`,
+		to: config.email,
+		replyTo: "hello@pubpub.org",
+		html: config.body,
+		subject: config.subject,
+	}); 
 
 	return {
 		success: true,
