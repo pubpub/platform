6ed4419bba79d537a960b6797025cf78a564eac6
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2024-04-17 15:02:22 -0400

    Push to v6 action

---
+++ core/actions/pushToV6/action.ts
@@ -0,0 +1,18 @@
+import * as z from "zod";
+
+import { FileText } from "ui/icon";
+
+import * as corePubFields from "../corePubFields";
+import { defineAction } from "../types";
+
+export const action = defineAction({
+	name: "pushToV6",
+	config: z.object({
+		communitySlug: z.string().describe("Community slug"),
+		authToken: z.string().describe("PubPub v6 API auth token"),
+	}),
+	description: "Sync a PubPub v7 pub to v6",
+	pubConfig: z.object({}),
+	pubFields: [corePubFields.title, corePubFields.content],
+	icon: FileText,
+});
