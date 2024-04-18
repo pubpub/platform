774f436d599b4380d216e0b538c8941ca7c9b22b
qweliant <dmtorcode@gmail.com>
2024-04-18 16:41:56 -0400

    can send email via action

--- core/actions/email/run.ts
+++ core/actions/email/run.ts
@@ -7,22 +7,28 @@ import { smtpclient } from "~/lib/server/mailgun";
 import { defineRun } from "../types";
 
 export const run = defineRun<typeof action>(async ({ pub, config, pubConfig }) => {
+	try {
+		await smtpclient.sendMail({
+			from: "hello@pubpub.org",
+			to: config.email,
+			replyTo: "hello@pubpub.org",
+			html: config.body,
+			subject: config.subject,
+		});
+	} catch (error) {
+		logger.error({ msg: "email", error });
+		return {
+			title: "Failed to Send Email",
+			error: "An error occured while sending the email",
+			cause: error,
+		};
+	}
+
 	logger.info({ msg: "email", pub, config, pubConfig });
-
-	
-
-	// query for community?
-	const { accepted, rejected } = await smtpclient.sendMail({
-		from: "hello@pubpub.org",
-		to: config.email,
-		replyTo: "hello@pubpub.org",
-		html: config.body,
-		subject: config.subject,
-	});
 
 	return {
 		success: true,
 		report: "Email sent",
-		data: { accepted, rejected },
+		data: {},
 	};
 });
