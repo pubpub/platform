993a230aabae7a42f0c22e3fb413a23e4189c0e2
Qwelian D Tanner <dmtorcode@tutanota.com>
2024-04-18 17:12:05 -0400

    Email Action (#328)
    * start action
    
    * use data
    
    * change email
    
    * can send email via action
    
    * rmv dev env var dupe
    
    ---------
    
    Co-authored-by: qweliant <dmtorcode@gmail.com>

--- core/actions/email/run.ts
+++ core/actions/email/run.ts
@@ -3,9 +3,27 @@
 import { logger } from "logger";
 
 import type { action } from "./action";
+import { smtpclient } from "~/lib/server/mailgun";
 import { defineRun } from "../types";
 
 export const run = defineRun<typeof action>(async ({ pub, config, pubConfig }) => {
+	try {
+		await smtpclient.sendMail({
+			from: "hello@pubpub.org",
+			to: config.email,
+			replyTo: "hello@pubpub.org",
+			html: config.body,
+			subject: config.subject,
+		});
+	} catch (error) {
+		logger.error({ msg: "email", error });
+		return {
+			title: "Failed to Send Email",
+			error: "An error occured while sending the email",
+			cause: error,
+		};
+	}
+
 	logger.info({ msg: "email", pub, config, pubConfig });
 
 	return {
