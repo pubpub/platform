087409df92d2947ecabc16fcae727c6b5ddf56f0
qweliant <dmtorcode@gmail.com>
2024-04-30 23:07:28 -0400

    create login spec and logout spec

--- core/playwright/example.spec.ts
+++ core/playwright/example.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('http:/localhost:3000/login');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
