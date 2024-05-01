158f2bac92ddc94adfeaf81d5729dd67fb52fdb6
qweliant <dmtorcode@gmail.com>
2024-05-01 10:26:21 -0400

    working login test

--- core/playwright/login.spec.ts
+++ core/playwright/login.spec.ts
@@ -3,20 +3,19 @@ import { expect, test } from "@playwright/test";
 const authFile = "playwright/.auth/user.json";
 
 test("Login", async ({ page }) => {
-	// Perform authentication steps. Replace these actions with your own.
 	await page.goto("/login");
 	await page.getByLabel("email").fill("all@pubpub.org");
 	await page.getByRole("textbox", { name: "password" }).fill("pubpub-all");
 	await page.getByRole("button", { name: "Sign in" }).click();
+
 	// Wait until the page receives the cookies.
 	//
 	// Sometimes login flow sets cookies in the process of several redirects.
 	// Wait for the final URL to ensure that the cookies are actually set.
 	await page.waitForURL("/c/unjournal/stages");
-
-	// // End of authentication steps.
-
-	// await page.context().storageState({ path: authFile });
+	await expect(page.getByRole("link", { name: "Stages" })).toBeVisible();
+	// End of authentication steps.
+	await page.context().storageState({ path: authFile });
 });
 
 test("Logout", async ({ page }) => {
@@ -30,6 +29,7 @@ test("Logout", async ({ page }) => {
 	// Sometimes login flow sets cookies in the process of several redirects.
 	// Wait for the final URL to ensure that the cookies are actually set.
 	await page.waitForURL("/c/unjournal/stages");
-	await page.getByRole("button", { name: "Log out" }).click();
+	await page.getByRole("button", { name: "Logout" }).click();
 	await page.waitForURL("/login");
+	await page.context().storageState({ path: authFile });
 });
