cf796c0b3257dd5de51517765c5cd33a370e7aed
qweliant <dmtorcode@gmail.com>
2024-05-01 10:56:43 -0400

    run lint

--- core/playwright/assign.spec.ts
+++ core/playwright/assign.spec.ts
@@ -1,10 +1,13 @@
 import { expect, test } from "@playwright/test";
 
+const authFile = "playwright/.auth/user.json";
 
-test("Assigning members to a stage", async ({ page }) => {
+test("Assigning members to a pub", async ({ page }) => {
+	// move a pub to to evaluate or just add it there in seed???
 	await page.goto("/c/unjournal/stages");
 	await page.getByRole("button", { name: "Create stage" }).click();
 	await page.getByRole("textbox", { name: "title" }).fill("Test stage");
 	await page.getByRole("button", { name: "Create" }).click();
 	await page.waitForURL("/c/unjournal/stages/test-stage");
+	await page.context().storageState({ path: authFile });
 });
