865a9d7020f0b41b3d699a09a074348de822a7ba
qweliant <dmtorcode@gmail.com>
2024-05-07 08:41:37 -0400

    webkit  test do not run

--- core/playwright/assign.spec.ts
+++ core/playwright/assign.spec.ts
@@ -7,10 +7,7 @@ test("Assigning members to a pub", async ({ page }) => {
 	await page.getByLabel("email").fill("all@pubpub.org");
 	await page.getByRole("textbox", { name: "password" }).fill("pubpub-all");
 	await page.getByRole("button", { name: "Sign in" }).click();
-	// Wait until the page receives the cookies.
-	//
-	// Sometimes login flow sets cookies in the process of several redirects.
-	// Wait for the final URL to ensure that the cookies are actually set.
+
 	await page.waitForURL("/c/unjournal/stages");
 	await page.getByRole("combobox").click();
 	await page.getByRole("option", { name: "Jill Admin" }).click();
