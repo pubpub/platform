ee4c985ddce418c1f05a7fcb859df3c41c1e7d79
eric <eric.g.mcdaniel@gmail.com>
2024-05-07 10:06:48 -0400

    Use transaction

--- core/actions/move/run.ts
+++ core/actions/move/run.ts
@@ -10,37 +10,25 @@ import { defineRun } from "../types";
 
 export const run = defineRun<typeof action>(async ({ pub, config, stageId }) => {
 	try {
-		// Confirm that the stage still exists.
-		const destinationStage = await db
-			.selectFrom("stages")
-			.select("id")
-			.where("id", "=", config.stage as StagesId)
-			.executeTakeFirst();
+		await db.transaction().execute(async (trx) => {
+			// Remove the pub from the current stage.
+			await trx
+				.deleteFrom("PubsInStages")
+				.where("pubId", "=", pub.id as PubsId)
+				.where("stageId", "=", stageId)
+				.execute();
 
-		if (destinationStage === undefined) {
-			return {
-				title: "Failed to move pub",
-				error: "The destination stage no longer exists",
-			};
-		}
-
-		// Remove the pub from the current stage.
-		await db
-			.deleteFrom("PubsInStages")
-			.where("pubId", "=", pub.id as PubsId)
-			.where("stageId", "=", stageId)
-			.execute();
-
-		// Add the pub to the destination stage.
-		await db
-			.insertInto("PubsInStages")
-			.values({
-				pubId: pub.id as PubsId,
-				stageId: config.stage as StagesId,
-			})
-			.execute();
+			// Add the pub to the destination stage.
+			return await trx
+				.insertInto("PubsInStages")
+				.values({
+					pubId: pub.id as PubsId,
+					stageId: config.stage as StagesId,
+				})
+				.execute();
+		});
 	} catch (error) {
-		logger.error({ msg: "email", error });
+		logger.error({ msg: "move", error });
 		return {
 			title: "Failed to move pub",
 			error: "An error occured while moving the pub",
