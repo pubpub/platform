723335dcff1ca9068dc03c754bbe5b42b65aaebb
Qwelian Tanner <dmtorcode@tutanota.com>
2024-05-22 09:29:54 -0400

    add trhow back and 35

--- core/prisma/exampleCommunitySeeds/unjournal.ts
+++ core/prisma/exampleCommunitySeeds/unjournal.ts
@@ -803,7 +803,7 @@ export default async function main(prisma: PrismaClient, communityUUID: string) 
 				},
 				titleFieldSlug: "unjournal:title",
 				evaluatorFieldSlug: "unjournal:evaluator",
-				deadlineLength: 21,
+				deadlineLength: 35,
 				deadlineUnit: "days",
 			},
 		},
