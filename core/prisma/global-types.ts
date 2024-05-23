51d20e59c2dd47ab5ee6a783aa93856d57d6bedb
Thomas F. K. Jorna <hello@tefkah.com>
2024-05-23 15:06:37 +0200

    chore: get rid of prisma json type generation, just generate comments and have kanel handle it

--- core/prisma/global-types.ts
+++
@@ -1,9 +0,0 @@
-import { Interval } from "~/actions/_lib/rules";
-
-import "../actions/types";
-
-declare global {
-	namespace PrismaJson {
-		type RuleConfig = { duration: number; interval: Interval };
-	}
-}
