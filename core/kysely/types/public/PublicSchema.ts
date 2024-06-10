9ea43d6b058a67fc48bc70a5207cc2eb04775e91
qwelian <qwelian@qwelians-Laptop.local>
2024-06-10 00:38:12 -0400

    delete stuff

--- core/kysely/types/public/PublicSchema.ts
+++ core/kysely/types/public/PublicSchema.ts
@@ -33,19 +33,9 @@ import { type default as StagesTable } from "./Stages";
 import { type default as UsersTable } from "./Users";
 
 export default interface PublicSchema {
-	_prisma_migrations: PrismaMigrationsTable;
+	forms: FormsTable;
 
-	users: UsersTable;
-
-	pubs: PubsTable;
-
-	pub_types: PubTypesTable;
-
-	stages: StagesTable;
-
-	members: MembersTable;
-
-	member_groups: MemberGroupsTable;
+	form_inputs: FormInputsTable;
 
 	integrations: IntegrationsTable;
 
@@ -89,7 +79,23 @@ export default interface PublicSchema {
 
 	action_runs: ActionRunsTable;
 
+<<<<<<< HEAD
 	forms: FormsTable;
 
 	form_inputs: FormInputsTable;
+=======
+	_prisma_migrations: PrismaMigrationsTable;
+
+	users: UsersTable;
+
+	pubs: PubsTable;
+
+	pub_types: PubTypesTable;
+
+	stages: StagesTable;
+
+	members: MembersTable;
+
+	member_groups: MemberGroupsTable;
+>>>>>>> 08fd7fc (delete stuff)
 }
