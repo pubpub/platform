9d27909d2e0222a992703b771ef0a711d77a712f
Thomas F. K. Jorna <hello@tefkah.com>
2024-06-18 15:09:25 +0200

    chore: cache things

--- core/actions/http/params/outputMap.field.tsx
+++ core/actions/http/params/outputMap.field.tsx
@@ -1,4 +1,5 @@
 import { db } from "~/kysely/database";
+import { autoCache } from "~/lib/server/cache/autoCache";
 import { defineActionFormFieldServerComponent } from "../../_lib/custom-form-field/defineConfigServerComponent";
 import { action } from "../action";
 import { FieldOutputMap } from "../config/client-components/FieldOutputMap";
@@ -7,20 +8,20 @@ const component = defineActionFormFieldServerComponent(
 	action,
 	"params",
 	async ({ action, actionInstance, stageId, communityId, pubId }) => {
-		const pubFields = await db
-			.selectFrom("pub_fields")
-			.distinctOn("pub_fields.id")
-			.innerJoin("pub_values", "pub_values.fieldId", "pub_fields.id")
-			.innerJoin("pubs", "pubs.id", "pub_values.pubId")
-			.where("pubs.id", "=", pubId)
-			.select([
-				"pub_fields.id",
-				"pub_fields.name",
-				"pub_fields.pubFieldSchemaId",
-				"pub_fields.slug",
-			])
-
-			.execute();
+		const pubFields = await autoCache(
+			db
+				.selectFrom("pub_fields")
+				.distinctOn("pub_fields.id")
+				.innerJoin("pub_values", "pub_values.fieldId", "pub_fields.id")
+				.innerJoin("pubs", "pubs.id", "pub_values.pubId")
+				.where("pubs.id", "=", pubId)
+				.select([
+					"pub_fields.id",
+					"pub_fields.name",
+					"pub_fields.pubFieldSchemaId",
+					"pub_fields.slug",
+				])
+		).execute();
 
 		return <FieldOutputMap context={{ pubFields }} />;
 	}
