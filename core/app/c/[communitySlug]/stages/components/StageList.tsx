28ad81473f1d7530de038d13fb22691692031283
Thomas F. K. Jorna <hello@tefkah.com>
2024-06-18 18:42:20 +0200

    fix: restore throwing behavior for incorrect permissions, i guess

--- core/app/c/[communitySlug]/stages/components/StageList.tsx
+++ core/app/c/[communitySlug]/stages/components/StageList.tsx
@@ -26,7 +26,7 @@ function StageList(props: Props) {
 					return (
 						<div key={stages[0].id}>
 							{stages.map((stage) => {
-								// const users = getPubUsers(stage.permissions);
+								const users = getPubUsers(stage.permissions);
 								// users should be just member but these are users
 
 								const sources = moveConstraintSourcesForStage(
