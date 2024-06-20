868170ba8d71ec9600a2a455c7168383748b4391
Kalil Smith-Nuevelle <kalilsn@gmail.com>
2024-06-20 16:23:53 -0500

    Use automatic caching

--- core/app/c/[communitySlug]/types/page.tsx
+++ core/app/c/[communitySlug]/types/page.tsx
@@ -4,36 +4,33 @@ import { jsonArrayFrom } from "kysely/helpers/postgres";
 
 import { db } from "~/kysely/database";
 import { getLoginData } from "~/lib/auth/loginData";
+import { autoCache } from "~/lib/server/cache/autoCache";
 import { FieldsProvider } from "./FieldsProvider";
 import TypeList from "./TypeList";
 
-// TODO: this should probably filter and cache by the community, but fields aren't actually scoped to a community yet!
-const getFields = unstable_cache(
-	async () => {
-		return await db.selectFrom("pub_fields").select(["id", "name", "slug"]).execute();
-	},
-	undefined,
-	{ tags: ["fields"] }
-);
+// TODO: this should probably filter by the community, but fields aren't actually scoped to a community yet!
+const getFields = async () =>
+	await autoCache(db.selectFrom("pub_fields").select(["id", "name", "slug"])).execute();
 
 const getTypes = async (communitySlug: string) => {
-	return await db
-		.selectFrom("pub_types")
-		.innerJoin("communities", "communities.id", "pub_types.communityId")
-		.innerJoin("_PubFieldToPubType", "_PubFieldToPubType.B", "pub_types.id")
-		.where("communities.slug", "=", communitySlug)
-		.selectAll("pub_types")
-		.select((eb) =>
-			jsonArrayFrom(
-				eb
-					.selectFrom("_PubFieldToPubType")
-					.whereRef("B", "=", "pub_types.id")
-					.innerJoin("pub_fields", "pub_fields.id", "A")
-					.selectAll("pub_fields")
-			).as("fields")
-		)
-		.groupBy("pub_types.id")
-		.execute();
+	return await autoCache(
+		db
+			.selectFrom("pub_types")
+			.innerJoin("communities", "communities.id", "pub_types.communityId")
+			.innerJoin("_PubFieldToPubType", "_PubFieldToPubType.B", "pub_types.id")
+			.where("communities.slug", "=", communitySlug)
+			.selectAll("pub_types")
+			.select((eb) =>
+				jsonArrayFrom(
+					eb
+						.selectFrom("_PubFieldToPubType")
+						.whereRef("B", "=", "pub_types.id")
+						.innerJoin("pub_fields", "pub_fields.id", "A")
+						.selectAll("pub_fields")
+				).as("fields")
+			)
+			.groupBy("pub_types.id")
+	).execute();
 };
 
 type Props = { params: { communitySlug: string } };
