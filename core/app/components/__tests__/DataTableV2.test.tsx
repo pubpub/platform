e153081025b41700d649ed43b6d9d13fb844db75
allisonking <aking@knowledgefutures.org>
2024-07-09 10:18:07 -0400

    MORE TESTS 🤓

--- core/app/components/__tests__/DataTableV2.test.tsx
+++ core/app/components/__tests__/DataTableV2.test.tsx
@@ -1,9 +1,9 @@
-import { afterEach, describe } from "node:test";
+import { describe } from "node:test";
 
 import type { ColumnDef } from "@tanstack/react-table";
 
 import { createColumnHelper } from "@tanstack/react-table";
-import { cleanup, fireEvent, render, screen, within } from "@testing-library/react";
+import { fireEvent, render, screen, within } from "@testing-library/react";
 import { expect, it, vi } from "vitest";
 
 import { DataTable } from "../DataTable/v2/DataTable";
@@ -24,23 +24,49 @@ const columns = [
 ] as const satisfies ColumnDef<Animal, unknown>[];
 
 describe("DataTable", () => {
-	it("does not render paging elements when there is only one page of data", async () => {
-		render(<DataTable columns={columns} data={DATA} />);
-		expect(screen.queryByTestId("data-table-pagination")).toBeNull();
+	describe("paging", () => {
+		it("does not render paging elements when there is only one page of data", async () => {
+			render(<DataTable columns={columns} data={DATA} />);
+			expect(screen.queryByTestId("data-table-pagination")).toBeNull();
+		});
+
+		it("renders paging elements when there is paged data", async () => {
+			const pagedData = [...DATA, ...DATA, ...DATA, ...DATA, ...DATA, ...DATA, ...DATA];
+			render(<DataTable columns={columns} data={pagedData} />);
+			const { getByText } = within(screen.getByTestId("data-table-pagination"));
+			expect(getByText("Page 1 of 2")).toBeDefined();
+		});
 	});
 
-	it("renders paging elements when there is paged data", async () => {
-		const pagedData = [...DATA, ...DATA, ...DATA, ...DATA, ...DATA, ...DATA, ...DATA];
-		render(<DataTable columns={columns} data={pagedData} />);
-		const { getByText } = within(screen.getByTestId("data-table-pagination"));
-		expect(getByText("Page 1 of 2")).toBeDefined();
-	});
+	describe("row clicking", () => {
+		it("can use a row click handler", async () => {
+			const rowClick = vi.fn();
+			render(<DataTable columns={columns} data={DATA} onRowClick={rowClick} />);
+			const row = screen.getByText(DATA[0].name);
+			fireEvent.click(row);
+			expect(rowClick).toHaveBeenCalledOnce();
+		});
 
-	it("can use a row click handler", async () => {
-		const rowClick = vi.fn();
-		render(<DataTable columns={columns} data={DATA} onRowClick={rowClick} />);
-		const row = screen.getByText(DATA[0].name);
-		fireEvent.click(row);
-		expect(rowClick).toHaveBeenCalledOnce();
+		it("will not trigger row click on button click", async () => {
+			const rowClick = vi.fn();
+			const buttonClick = vi.fn();
+			const columnsWithButton = [
+				...columns,
+				{
+					id: "test-button",
+					header: () => "Test",
+					cell: ({ row }) => (
+						<button onClick={buttonClick} data-testid={`test-btn-${row.id}`}>
+							test button
+						</button>
+					),
+				},
+			];
+			render(<DataTable columns={columnsWithButton} data={DATA} onRowClick={rowClick} />);
+			const button = screen.getByTestId(`test-btn-0`);
+			fireEvent.click(button);
+			expect(buttonClick).toHaveBeenCalledOnce();
+			expect(rowClick).toHaveBeenCalledTimes(0);
+		});
 	});
 });
