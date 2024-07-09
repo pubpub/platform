e749e76f12405986acfc18b7aa70a3b0a317f84b
allisonking <aking@knowledgefutures.org>
2024-07-09 10:24:52 -0400

    Update comment to refer to testing library docs

--- core/vitest.config.ts
+++ core/vitest.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
 			"**/.{idea,git,cache,output,temp}/**",
 			"**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*",
 		],
-		// Clean up DOM between test runs https://github.com/testing-library/vue-testing-library/issues/296
+		// Clean up DOM between test runs https://testing-library.com/docs/vue-testing-library/setup/
 		globals: true,
 	},
 });
