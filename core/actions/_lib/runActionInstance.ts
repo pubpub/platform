dbde38308bc7bd860871027b89cc609d5ae7d72d
Thomas F. K. Jorna <hello@tefkah.com>
2024-07-09 15:17:42 +0200

    chore: cleanup actions a bit

--- core/actions/_lib/runActionInstance.ts
+++ core/actions/_lib/runActionInstance.ts
@@ -1,23 +1,20 @@
 "use server";
 
-import { revalidateTag } from "next/cache";
 import { captureException } from "@sentry/nextjs";
 import { sql } from "kysely";
 import { jsonObjectFrom } from "kysely/helpers/postgres";
 
-import type { GetPubResponseBody } from "contracts";
 import { logger } from "logger";
 
 import type { ActionSuccess } from "../types";
-import type Action from "~/kysely/types/public/Action";
 import type { ActionInstancesId } from "~/kysely/types/public/ActionInstances";
+import type { CommunitiesId } from "~/kysely/types/public/Communities";
 import type { PubsId } from "~/kysely/types/public/Pubs";
 import type { StagesId } from "~/kysely/types/public/Stages";
 import type { UsersId } from "~/kysely/types/public/Users";
 import type { ClientException, ClientExceptionOptions } from "~/lib/serverActions";
 import { db } from "~/kysely/database";
 import ActionRunStatus from "~/kysely/types/public/ActionRunStatus";
-import { CommunitiesId } from "~/kysely/types/public/Communities";
 import Event from "~/kysely/types/public/Event";
 import { getPub } from "~/lib/server";
 import { autoRevalidate } from "~/lib/server/cache/autoRevalidate";
