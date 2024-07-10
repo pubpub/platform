ebb425e18dc2ca7a2759c4f43db7324f137b235f
allisonking <aking@knowledgefutures.org>
2024-07-10 15:14:27 -0400

    Add logo as SVG to manipulate colors

--- core/app/c/(public)/[communitySlug]/forms/[formSlug]/layout.tsx
+++ core/app/c/(public)/[communitySlug]/forms/[formSlug]/layout.tsx
@@ -1,6 +1,7 @@
 import { Button } from "ui/button";
 import { Bookmark } from "ui/icon";
 
+import Logo from "~/app/components/Logo";
 import { findCommunityBySlug } from "~/lib/server/community";
 
 type Props = { children: React.ReactNode; params: { communitySlug: string; formSlug: string } };
@@ -12,12 +13,13 @@ export default async function Layout({ children, params }: Props) {
 	}
 	return (
 		<div className="min-h-screen">
-			<div className="border-color-gray-100 container mx-auto flex items-center border-b bg-gray-50 py-5">
-				<div className="flex-1">
-					<img src="/logos/icon.svg" className="w-8" alt="" />
+			<div className="border-color-gray-100 flex items-center gap-2 border-b bg-gray-50 py-5">
+				<div className="ml-4 flex-1">
+					<Logo className="text-[#C1C8CD]" width={32} height={32} />
 				</div>
+
 				<h1 className="text-xl font-bold">Evaluation for {community.name}</h1>
-				<div className="flex flex-1 justify-end">
+				<div className="mr-6 flex flex-1 justify-end">
 					<Button variant="outline" className="border-foreground">
 						<Bookmark size={16} className="mr-2" strokeWidth={1} /> Bookmark
 					</Button>
