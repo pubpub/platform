3b065e7d3491836d2719f3d6d7043729ecfc3a39
Thomas F. K. Jorna <hello@tefkah.com>
2024-07-11 16:14:06 +0200

    rough initial migration

--- core/.kanelrc.js
+++ core/.kanelrc.js
@@ -1,14 +1,31 @@
-const path = require("path");
+// @ts-check
 const { makeKyselyHook } = require("kanel-kysely");
+const {
+	generateZodSchemas,
+	makeGenerateZodSchemas,
+	defaultGetZodIdentifierMetadata,
+	defaultGetZodSchemaMetadata,
+} = require("kanel-zod");
+const {
+	kanelKyselyZodCompatibilityPreRenderHook,
+} = require("./prisma/kanel-kysely-zod-compatibility-hook.cjs");
 
 /** @type {import('kanel').Config} */
 module.exports = {
 	connection: process.env["DATABASE_URL"],
 	schemas: ["public"],
 
-	preDeleteOutputFolder: true,
-	preRenderHooks: [makeKyselyHook()],
-	outputPath: "./kysely/types",
+	/**
+	 * otherwise e.g. the `packages/db/src/types` folder will be deleted before the build
+	 */
+	preDeleteOutputFolder: false,
+	enumStyle: "enum",
+	preRenderHooks: [
+		makeKyselyHook(),
+		generateZodSchemas,
+		kanelKyselyZodCompatibilityPreRenderHook,
+	],
+	outputPath: "../packages/db/src",
 
 	customTypeMap: {
 		"pg_catalog.tsvector": "string",
