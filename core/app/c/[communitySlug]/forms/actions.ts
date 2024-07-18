360d9159004c2d43f6ab2af3ef7929e6cdb81120
Thomas F. K. Jorna <hello@tefkah.com>
2024-07-18 17:14:08 +0200

    feat: add 'addUserToForm' action, to be used later

--- core/app/c/[communitySlug]/forms/actions.ts
+++ core/app/c/[communitySlug]/forms/actions.ts
@@ -5,10 +5,16 @@ import { logger } from "logger";
 import type { CommunitiesId } from "~/kysely/types/public/Communities";
 import type { FormsId } from "~/kysely/types/public/Forms";
 import type { PubTypesId } from "~/kysely/types/public/PubTypes";
+import type { UsersId } from "~/kysely/types/public/Users";
 import { db, isUniqueConstraintError } from "~/kysely/database";
 import { autoRevalidate } from "~/lib/server/cache/autoRevalidate";
+import { getCommunitySlug } from "~/lib/server/cache/getCommunitySlug";
+import { findCommunityBySlug } from "~/lib/server/community";
 import { defineServerAction } from "~/lib/server/defineServerAction";
+import { getForm } from "~/lib/server/form";
+import { getUser } from "~/lib/server/user";
 import { slugifyString } from "~/lib/string";
+import { inviteUserToForm } from "../../(public)/[communitySlug]/public/forms/[formSlug]/actions";
 
 export const createForm = defineServerAction(async function createForm(
 	pubTypeId: PubTypesId,
@@ -46,5 +52,67 @@ export const archiveForm = defineServerAction(async function archiveForm(id: For
 	} catch (error) {
 		logger.error({ msg: "error archiving form", error });
 		return { error: "Unable to archive form" };
+	}
+});
+
+export const addUserToForm = defineServerAction(async function addUserToForm({
+	userId,
+	...formSlugOrId
+}: {
+	userId: UsersId;
+} & (
+	| {
+			slug: string;
+			id?: never;
+	  }
+	| {
+			id: FormsId;
+			slug?: never;
+	  }
+)) {
+	const communitySlug = getCommunitySlug();
+	const community = await findCommunityBySlug(communitySlug);
+	if (!community) {
+		return { error: "Community not found" };
+	}
+
+	try {
+		const form = await getForm(formSlugOrId).executeTakeFirstOrThrow();
+
+		await autoRevalidate(
+			db
+				.with("current_member", (db) =>
+					db
+						.selectFrom("members")
+						.selectAll()
+						.where("members.userId", "=", userId)
+						.where("members.communityId", "=", community.id)
+				)
+				.with("new_permission", (db) =>
+					db
+						.insertInto("permissions")
+						.values((eb) => ({
+							memberId: eb.selectFrom("current_member").select("current_member.id"),
+						}))
+						.returning("id")
+				)
+				.insertInto("_FormToPermission")
+				.values((eb) => ({
+					A: form.id,
+					B: eb.selectFrom("new_permission").select("new_permission.id"),
+				}))
+				.returning(["A as formId", "B as permissionId"])
+		).executeTakeFirstOrThrow();
+
+		const user = await getUser({ id: userId }).executeTakeFirstOrThrow();
+
+		await inviteUserToForm({
+			communitySlug,
+			email: user.email,
+			...formSlugOrId,
+		});
+	} catch (error) {
+		logger.error({ msg: "error adding user to form", error });
+		return { error: "Unable to add user to form" };
 	}
 });
