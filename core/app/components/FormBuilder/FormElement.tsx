2bd3c92198a1f9b320659cf4bf081143eb940c87
Kalil Smith-Nuevelle <kalilsn@gmail.com>
2024-07-24 18:07:42 -0500

    Improve readability of FormElement

--- core/app/components/FormBuilder/FormElement.tsx
+++ core/app/components/FormBuilder/FormElement.tsx
@@ -9,7 +9,7 @@ import { ArchiveRestore, GripVertical, Pencil, Trash, Type } from "ui/icon";
 import { usePubFieldContext } from "ui/pubFields";
 import { cn } from "utils";
 
-import type { FormBuilderSchema, FormElementData } from "./types";
+import type { FormBuilderSchema, InputElement } from "./types";
 import { useFormBuilder } from "./FormBuilderContext";
 import { isFieldInput } from "./types";
 
@@ -33,110 +33,109 @@ export const FormElement = ({ element, index, isEditing, isDisabled }: FormEleme
 
 	const { setEditingElement, openConfigPanel, removeElement, restoreElement } = useFormBuilder();
 
-	const pubFields = usePubFieldContext();
-	if (isFieldInput(element)) {
-		const field = pubFields[element.fieldId as PubFieldsId];
-		return (
-			<div
-				ref={setNodeRef}
-				style={style}
-				{...attributes}
-				className={cn(
-					"flex flex-1 flex-shrink-0 items-center justify-between gap-3 self-stretch rounded border border-l-[12px] border-solid border-gray-200 border-l-emerald-100 bg-white p-3 pr-4",
-					isEditing ? "border-sky-500 border-l-blue-500" : "",
-					isDisabled ? "cursor-auto opacity-50" : "",
-					element.deleted ? "border-l-red-200" : ""
-				)}
+	const restoreRemoveButton = element.deleted ? (
+		<>
+			<div className="my-auto text-slate-500">Deleted on save</div>
+			<Button
+				type="button"
+				disabled={isDisabled}
+				variant="ghost"
+				className="invisible p-2 hover:bg-white group-hover:visible"
+				aria-label="Restore element"
+				onClick={() => {
+					restoreElement(index);
+				}}
 			>
+				<ArchiveRestore size={24} className="text-neutral-400 hover:text-red-500" />
+			</Button>
+		</>
+	) : (
+		<Button
+			type="button"
+			disabled={isDisabled}
+			variant="ghost"
+			className="invisible p-2 hover:bg-white group-hover:visible"
+			aria-label="Delete element"
+			onClick={() => {
+				removeElement(index);
+			}}
+		>
+			<Trash size={24} className="text-neutral-400 hover:text-red-500" />
+		</Button>
+	);
+	return (
+		<div
+			ref={setNodeRef}
+			style={style}
+			{...attributes}
+			className={cn(
+				"flex flex-1 flex-shrink-0 items-center justify-between gap-3 self-stretch rounded border border-l-[12px] border-solid border-gray-200 border-l-emerald-100 bg-white p-3 pr-4",
+				isEditing && "border-sky-500 border-l-blue-500",
+				isDisabled && "cursor-auto opacity-50",
+				element.deleted && "border-l-red-200"
+			)}
+		>
+			<div className="group flex flex-1 flex-shrink-0 flex-wrap justify-start gap-0.5">
 				{isFieldInput(element) && (
-					<div className="group flex flex-1 flex-shrink-0 flex-wrap justify-start gap-0.5">
-						<Type
-							size={20}
-							className={cn(
-								"my-auto mr-4",
-								isEditing ? "text-blue-500" : "text-emerald-500",
-								element.deleted ? "text-slate-500" : ""
-							)}
-						/>
-						<div>
-							<div className="text-slate-500">{field.slug}</div>
-							<div
-								className={cn(
-									"font-semibold",
-									element.deleted ? "text-slate-500" : ""
-								)}
-							>
-								{element.label ?? field.name}
-							</div>
-						</div>
-						{isEditing ? (
-							<div className="my-auto ml-auto text-xs text-blue-500">EDITING</div>
-						) : (
-							<div className="my-auto ml-auto flex gap-1">
-								{element.deleted ? (
-									<>
-										<div className="my-auto text-slate-500">
-											Deleted on save
-										</div>
-										<Button
-											type="button"
-											variant="ghost"
-											className="invisible p-2 hover:bg-white group-hover:visible"
-											aria-label="Restore element"
-											onClick={() => {
-												restoreElement(index);
-											}}
-										>
-											<ArchiveRestore
-												size={24}
-												className="text-neutral-400 hover:text-red-500"
-											/>
-										</Button>
-									</>
-								) : (
-									<Button
-										type="button"
-										disabled={isDisabled}
-										variant="ghost"
-										className="invisible p-2 hover:bg-white group-hover:visible"
-										aria-label="Delete element"
-										onClick={() => {
-											removeElement(index);
-										}}
-									>
-										<Trash
-											size={24}
-											className="text-neutral-400 hover:text-red-500"
-										/>
-									</Button>
-								)}
-								<Button
-									type="button"
-									disabled={isDisabled || element.deleted}
-									variant="ghost"
-									className="invisible p-2 group-hover:visible"
-									onClick={() => {
-										setEditingElement(index);
-										openConfigPanel();
-									}}
-								>
-									<Pencil size={24} className="text-neutral-400" />
-								</Button>
-								<Button
-									type="button"
-									disabled={isDisabled || element.deleted}
-									variant="ghost"
-									className="invisible p-1.5 group-hover:visible"
-									{...listeners}
-								>
-									<GripVertical size={24} className="text-neutral-400" />
-								</Button>
-							</div>
-						)}
+					<FieldInputElement element={element} isEditing={isEditing} />
+				)}
+				{isEditing ? (
+					<div className="my-auto ml-auto text-xs text-blue-500">EDITING</div>
+				) : (
+					<div className="my-auto ml-auto flex gap-1">
+						{restoreRemoveButton}
+						<Button
+							type="button"
+							disabled={isDisabled || element.deleted}
+							variant="ghost"
+							className="invisible p-2 group-hover:visible"
+							onClick={() => {
+								setEditingElement(index);
+								openConfigPanel();
+							}}
+						>
+							<Pencil size={24} className="text-neutral-400" />
+						</Button>
+						<Button
+							type="button"
+							disabled={isDisabled || element.deleted}
+							variant="ghost"
+							className="invisible p-1.5 group-hover:visible"
+							{...listeners}
+						>
+							<GripVertical size={24} className="text-neutral-400" />
+						</Button>
 					</div>
 				)}
 			</div>
-		);
-	}
-	return <></>;
+		</div>
+	);
+};
+
+type FieldInputElementProps = {
+	element: InputElement;
+	isEditing: boolean;
+};
+const FieldInputElement = ({ element, isEditing }: FieldInputElementProps) => {
+	const pubFields = usePubFieldContext();
+	const field = pubFields[element.fieldId as PubFieldsId];
+
+	return (
+		<>
+			<Type
+				size={20}
+				className={cn(
+					"my-auto mr-4",
+					isEditing ? "text-blue-500" : "text-emerald-500",
+					element.deleted && "text-slate-500"
+				)}
+			/>
+			<div>
+				<div className="text-slate-500">{field.slug}</div>
+				<div className={cn("font-semibold", element.deleted ? "text-slate-500" : "")}>
+					{element.label ?? field.name}
+				</div>
+			</div>
+		</>
+	);
 };
