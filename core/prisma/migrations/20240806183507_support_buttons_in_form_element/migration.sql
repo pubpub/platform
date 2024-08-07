f0f404cdcbf954867a2c894b8e2e10d8401dcf4e
allisonking <aking@knowledgefutures.org>
2024-08-07 13:50:10 -0400

    Add unique constraint

--- core/prisma/migrations/20240806183507_support_buttons_in_form_element/migration.sql
+++ core/prisma/migrations/20240806183507_support_buttons_in_form_element/migration.sql
@@ -2,6 +2,7 @@
   Warnings:
 
   - You are about to drop the column `isSubmit` on the `form_elements` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[type,label]` on the table `form_elements` will be added. If there are existing duplicate values, this will fail.
 
 */
 
@@ -12,6 +13,9 @@ ALTER TYPE "ElementType" ADD VALUE 'button';
 ALTER TABLE "form_elements" ADD COLUMN     "stageId" TEXT,
 ALTER COLUMN "order" DROP NOT NULL;
 ALTER TABLE "form_elements" DROP COLUMN "isSubmit";
+
+-- CreateIndex
+CREATE UNIQUE INDEX "form_elements_type_label_key" ON "form_elements"("type", "label");
 
 -- AddForeignKey
 ALTER TABLE "form_elements" ADD CONSTRAINT "form_elements_stageId_fkey" FOREIGN KEY ("stageId") REFERENCES "stages"("id") ON DELETE SET NULL ON UPDATE CASCADE;
