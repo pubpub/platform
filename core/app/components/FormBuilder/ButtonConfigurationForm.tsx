69bf9b94a74b0e732503e07691c687fc7e448d21
allisonking <aking@knowledgefutures.org>
2024-08-07 13:02:07 -0400

    Add stageId dropdown

--- core/app/components/FormBuilder/ButtonConfigurationForm.tsx
+++ core/app/components/FormBuilder/ButtonConfigurationForm.tsx
@@ -4,9 +4,26 @@ import { z } from "zod";
 
 import { MarkdownEditor, zodToHtmlInputProps } from "ui/auto-form";
 import { Button } from "ui/button";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "ui/form";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from "ui/dropdown-menu";
+import {
+	Form,
+	FormControl,
+	FormDescription,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "ui/form";
+import { ChevronDown } from "ui/icon";
 import { Input } from "ui/input";
+import { cn } from "utils";
 
+import { useCommunity } from "../providers/CommunityProvider";
 import { useFormBuilder } from "./FormBuilderContext";
 
 export const DEFAULT_BUTTON = {
@@ -24,16 +41,24 @@ const SCHEMA = z.object({
 export const ButtonConfigurationForm = ({ id }: { id: string | null }) => {
 	const { elements, dispatch } = useFormBuilder();
 	const button = elements.find((e) => e.id === id);
+	const community = useCommunity();
+	const { stages } = community;
+
+	const defaultValues = button
+		? {
+				label: button.label ?? "",
+				content: button.content ?? "",
+				stageId: button.stageId ?? undefined,
+			}
+		: {
+				label: DEFAULT_BUTTON.label,
+				content: DEFAULT_BUTTON.content,
+				stageId: undefined,
+			};
 
 	const form = useForm<z.infer<typeof SCHEMA>>({
 		resolver: zodResolver(SCHEMA),
-		defaultValues: button
-			? {
-					label: button.label ?? "",
-					content: button.content ?? "",
-					stageId: button.stageId ?? undefined,
-				}
-			: undefined,
+		defaultValues,
 	});
 
 	const onSubmit = (values: z.infer<typeof SCHEMA>) => {
@@ -90,6 +115,57 @@ export const ButtonConfigurationForm = ({ id }: { id: string | null }) => {
 							descriptionPlacement="bottom"
 						/>
 					)}
+				/>
+				<FormField
+					name="stageId"
+					control={form.control}
+					render={({ field }) => {
+						const currentValue = field.value
+							? stages.find((s) => s.id === field.value)?.name
+							: undefined;
+						return (
+							<FormItem
+								aria-label="Stage"
+								className="flex flex-col items-start gap-2"
+							>
+								<FormLabel>Post-submission stage</FormLabel>
+								<DropdownMenu>
+									<DropdownMenuTrigger asChild>
+										<Button
+											size="sm"
+											variant="outline"
+											className={cn(
+												"relative w-full justify-between bg-white",
+												{
+													"text-muted-foreground": !currentValue,
+												}
+											)}
+										>
+											{currentValue || "Select a stage"}
+											<ChevronDown size="16" />
+										</Button>
+									</DropdownMenuTrigger>
+									<DropdownMenuContent>
+										{stages.map((stage) => (
+											<DropdownMenuItem
+												key={stage.id}
+												onClick={() => {
+													field.onChange(stage.id);
+												}}
+											>
+												{stage.name}
+											</DropdownMenuItem>
+										))}
+									</DropdownMenuContent>
+								</DropdownMenu>
+								<FormDescription>
+									The stage the pub should be in after submission
+								</FormDescription>
+
+								<FormMessage />
+							</FormItem>
+						);
+					}}
 				/>
 				<div className="grid grid-cols-2 gap-2">
 					<Button
