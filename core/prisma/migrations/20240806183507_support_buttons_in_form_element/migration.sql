6e786daa9873f3b0fd39c2e33ddbfac3122d895a
allisonking <aking@knowledgefutures.org>
2024-08-08 12:21:58 -0400

    Fix unique constraint to be form specific

--- core/prisma/migrations/20240806183507_support_buttons_in_form_element/migration.sql
+++ core/prisma/migrations/20240806183507_support_buttons_in_form_element/migration.sql
@@ -2,7 +2,7 @@
   Warnings:
 
   - You are about to drop the column `isSubmit` on the `form_elements` table. All the data in the column will be lost.
-  - A unique constraint covering the columns `[type,label]` on the table `form_elements` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[type,label,formId]` on the table `form_elements` will be added. If there are existing duplicate values, this will fail.
 
 */
 
@@ -15,7 +15,7 @@ ALTER COLUMN "order" DROP NOT NULL;
 ALTER TABLE "form_elements" DROP COLUMN "isSubmit";
 
 -- CreateIndex
-CREATE UNIQUE INDEX "form_elements_type_label_key" ON "form_elements"("type", "label");
+CREATE UNIQUE INDEX "form_elements_type_label_formId_key" ON "form_elements"("type", "label", "formId");
 
 -- AddForeignKey
 ALTER TABLE "form_elements" ADD CONSTRAINT "form_elements_stageId_fkey" FOREIGN KEY ("stageId") REFERENCES "stages"("id") ON DELETE SET NULL ON UPDATE CASCADE;
