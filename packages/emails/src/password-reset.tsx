0cea2baf8d6ea5302720bbb3263b18c7124f4245
Thomas F. K. Jorna <hello@tefkah.com>
2024-08-08 18:53:14 +0200

    chore: add logo to password reset email

--- packages/emails/src/password-reset.tsx
+++ packages/emails/src/password-reset.tsx
@@ -17,32 +17,20 @@ import {
 	Text,
 } from "@react-email/components";
 
-interface VercelInviteUserEmailProps {
-	username?: string;
-	userImage?: string;
-	invitedByUsername?: string;
-	invitedByEmail?: string;
-	teamName?: string;
-	teamImage?: string;
-	inviteLink?: string;
-	inviteFromIp?: string;
-	inviteFromLocation?: string;
+interface ForgotPasswordProps {
+	firstName: string;
+	lastName?: string;
+	resetPasswordLink: string;
 }
 
-const baseUrl = process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : "";
+const baseUrl = process.env.PUBPUB_URL ? process.env.PUBPUB_URL : "";
 
 export const VercelInviteUserEmail = ({
-	username = "zenorocha",
-	userImage = `${baseUrl}/static/vercel-user.png`,
-	invitedByUsername = "bukinoshita",
-	invitedByEmail = "bukinoshita@example.com",
-	teamName = "My Project",
-	teamImage = `${baseUrl}/static/vercel-team.png`,
-	inviteLink = "https://vercel.com/teams/invite/foo",
-	inviteFromIp = "204.13.186.218",
-	inviteFromLocation = "São Paulo, Brazil",
-}: VercelInviteUserEmailProps) => {
-	const previewText = `Join ${invitedByUsername} on Vercel`;
+	firstName = "ANDY",
+	lastName = "Rocha",
+	resetPasswordLink,
+}: ForgotPasswordProps) => {
+	const previewText = `Reset your PubPub password`;
 
 	return (
 		<Html>
@@ -53,81 +41,44 @@ export const VercelInviteUserEmail = ({
 					<Container className="mx-auto my-[40px] w-[465px] rounded border border-solid border-[#eaeaea] p-[20px]">
 						<Section className="mt-[32px]">
 							<Img
-								src={`${baseUrl}/static/vercel-logo.png`}
+								src={`${baseUrl}/static/logo.svg`}
 								width="40"
-								height="37"
-								alt="Vercel"
+								height="40"
+								alt="PubPub"
 								className="mx-auto my-0"
 							/>
 						</Section>
 						<Heading className="mx-0 my-[30px] p-0 text-center text-[24px] font-normal text-black">
-							Join <strong>{teamName}</strong> on <strong>Vercel</strong>
+							Reset your PubPub password
 						</Heading>
 						<Text className="text-[14px] leading-[24px] text-black">
-							Hello {username},
+							Hello {firstName},
 						</Text>
 						<Text className="text-[14px] leading-[24px] text-black">
-							<strong>bukinoshita</strong> (
-							<Link
-								href={`mailto:${invitedByEmail}`}
-								className="text-blue-600 no-underline"
-							>
-								{invitedByEmail}
-							</Link>
-							) has invited you to the <strong>{teamName}</strong> team on{" "}
-							<strong>Vercel</strong>.
+							You can use the button below to reset your password. If you were not
+							expecting this invitation, you can ignore this email. If you are
+							concerned about your account's safety, please reply to this email to get
+							in touch with us.
 						</Text>
-						<Section>
-							<Row>
-								<Column align="right">
-									<Img
-										className="rounded-full"
-										src={userImage}
-										width="64"
-										height="64"
-									/>
-								</Column>
-								<Column align="center">
-									<Img
-										src={`${baseUrl}/static/vercel-arrow.png`}
-										width="12"
-										height="9"
-										alt="invited you to"
-									/>
-								</Column>
-								<Column align="left">
-									<Img
-										className="rounded-full"
-										src={teamImage}
-										width="64"
-										height="64"
-									/>
-								</Column>
-							</Row>
-						</Section>
 						<Section className="mb-[32px] mt-[32px] text-center">
 							<Button
 								className="rounded bg-[#000000] px-4 py-3 text-center text-[12px] font-semibold text-white no-underline"
-								href={inviteLink}
+								href={resetPasswordLink}
 							>
-								Join the team
+								Reset your password
 							</Button>
 						</Section>
 						<Text className="text-[14px] leading-[24px] text-black">
 							or copy and paste this URL into your browser:{" "}
-							<Link href={inviteLink} className="text-blue-600 no-underline">
-								{inviteLink}
+							<Link href={resetPasswordLink} className="text-blue-600 no-underline">
+								{resetPasswordLink}
 							</Link>
 						</Text>
 						<Hr className="mx-0 my-[26px] w-full border border-solid border-[#eaeaea]" />
 						<Text className="text-[12px] leading-[24px] text-[#666666]">
-							This invitation was intended for{" "}
-							<span className="text-black">{username} </span>.This invite was sent
-							from <span className="text-black">{inviteFromIp}</span> located in{" "}
-							<span className="text-black">{inviteFromLocation}</span>. If you were
-							not expecting this invitation, you can ignore this email. If you are
-							concerned about your account's safety, please reply to this email to get
-							in touch with us.
+							If you were not expecting this invitation, you can ignore this email. If
+							you are concerned about your account's safety, please reply to this
+							email to get in touch with us.
 						</Text>
 					</Container>
 				</Body>
