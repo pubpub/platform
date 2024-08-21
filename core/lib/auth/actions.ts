58d98dcb7d4040caf977f23ad8dcb8fff0340f71
Thomas F. K. Jorna <hello@tefkah.com>
2024-08-21 16:04:51 +0200

    refactor: remove react-email ot make pr simpler

--- core/lib/auth/actions.ts
+++ core/lib/auth/actions.ts
@@ -2,8 +2,6 @@
 
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
-import { renderAsync } from "@react-email/render";
-import { PasswordReset } from "emails";
 import { z } from "zod";
 
 import type { Communities, Members, Users } from "db/public";
@@ -224,19 +222,20 @@ async function luciaSendForgotPasswordMail(props: {
 		userId: props.user.id,
 	});
 
-	const email = await renderAsync(
-		PasswordReset({
-			firstName: props.user.firstName,
-			lastName: props.user.lastName ?? undefined,
-			resetPasswordLink: magicLink,
-		})
-	);
+	const email = `Hello ${props.user.firstName} ${props.user.lastName ?? ""},
+
+		You have requested a password reset. Please click the link below to reset your password:
+
+		${magicLink}
+
+		If you did not request a password reset, please ignore this email.
+		`;
 
 	const send = await smtpclient.sendMail({
 		from: "PubPub <noreply@pubpub.com>",
 		to: props.user.email,
 		subject: "Reset your password",
-		html: email,
+		text: email,
 	});
 
 	return {
