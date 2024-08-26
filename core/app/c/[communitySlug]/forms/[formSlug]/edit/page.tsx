cbff21b7acc469fe255861e5c2b0feb5880d5d34
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2024-08-26 11:37:21 -0400

    fix caching issue in post-submit stage dropdown; render stage name in… (#535)
    * fix caching issue in post-submit stage dropdown; render stage name in pub child table
    
    * remove autocache for now

--- core/app/c/[communitySlug]/forms/[formSlug]/edit/page.tsx
+++ core/app/c/[communitySlug]/forms/[formSlug]/edit/page.tsx
@@ -1,21 +1,37 @@
+import { notFound } from "next/navigation";
+
+import { CommunitiesId } from "db/public";
 import { ClipboardPenLine } from "ui/icon";
 import { PubFieldProvider } from "ui/pubFields";
 
-import { ArchiveFormButton } from "~/app/components/FormBuilder/ArchiveFormButton";
 import { FormBuilder } from "~/app/components/FormBuilder/FormBuilder";
 import { SaveFormButton } from "~/app/components/FormBuilder/SaveFormButton";
+import { db } from "~/kysely/database";
 import { getLoginData } from "~/lib/auth/loginData";
 import { isCommunityAdmin } from "~/lib/auth/roles";
+import { autoCache } from "~/lib/server/cache/autoCache";
+import { findCommunityBySlug } from "~/lib/server/community";
 import { getForm } from "~/lib/server/form";
 import { getPubFields } from "~/lib/server/pubFields";
 import { ContentLayout } from "../../../ContentLayout";
 
+const getCommunityStages = (communityId: CommunitiesId) =>
+	db.selectFrom("stages").where("stages.communityId", "=", communityId).selectAll();
+
 export default async function Page({ params: { formSlug, communitySlug } }) {
 	const loginData = await getLoginData();
+	const community = await findCommunityBySlug();
+
+	if (!community) {
+		notFound();
+	}
+
+	const communityStages = await getCommunityStages(community.id).execute();
 
 	if (!loginData) {
 		return null;
 	}
+
 	if (!isCommunityAdmin(loginData, { slug: communitySlug })) {
 		return null;
 	}
@@ -43,7 +59,7 @@ export default async function Page({ params: { formSlug, communitySlug } }) {
 			}
 		>
 			<PubFieldProvider pubFields={fields}>
-				<FormBuilder pubForm={form} id={formBuilderId} />
+				<FormBuilder pubForm={form} id={formBuilderId} stages={communityStages} />
 			</PubFieldProvider>
 		</ContentLayout>
 	);
