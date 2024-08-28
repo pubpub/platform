3f7a56d469e00dd167cac284c84111150f66e853
Thomas F. K. Jorna <hello@tefkah.com>
2024-08-28 16:52:14 +0200

    fix: improve lucia user types

--- core/lib/auth/lucia.ts
+++ core/lib/auth/lucia.ts
@@ -20,6 +20,7 @@ type UserWithMembersShips = Omit<Users, "passwordHash"> & {
 };
 declare module "lucia" {
 	interface Register {
+		UserId: UsersId;
 		Lucia: typeof lucia;
 		DatabaseUserAttributes: Omit<UserWithMembersShips, "id">;
 		DatabaseSessionAttributes: Omit<
