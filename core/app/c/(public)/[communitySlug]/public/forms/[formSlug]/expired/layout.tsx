9198d569fadc14000be61e65be30b338601cb463
Thomas F. K. Jorna <hello@tefkah.com>
2024-09-02 16:48:24 +0200

    fix: show pubtype name rather than evaluation in expired layout

--- core/app/c/(public)/[communitySlug]/public/forms/[formSlug]/expired/layout.tsx
+++ core/app/c/(public)/[communitySlug]/public/forms/[formSlug]/expired/layout.tsx
@@ -1,5 +1,7 @@
 import { Header } from "~/app/c/(public)/[communitySlug]/public/Header";
+import { getPubTypeForForm } from "~/lib/server";
 import { findCommunityBySlug } from "~/lib/server/community";
+import { capitalize } from "~/lib/string";
 
 type Props = { children: React.ReactNode; params: { communitySlug: string; formSlug: string } };
 
@@ -8,10 +10,19 @@ export default async function Layout({ children, params }: Props) {
 	if (!community) {
 		return null;
 	}
+
+	const pubType = await getPubTypeForForm({ slug: params.formSlug }).executeTakeFirstOrThrow();
+
+	if (!pubType) {
+		return null;
+	}
+
 	return (
 		<div>
 			<Header>
-				<h1 className="text-xl font-bold">Evaluation for {community.name}</h1>
+				<h1 className="text-xl font-bold">
+					{capitalize(pubType.name)} for {community.name}
+				</h1>
 			</Header>
 			<div className="container mx-auto">{children}</div>
 		</div>
