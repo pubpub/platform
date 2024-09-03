3712cb3104e2d35011e2f06f2c2ec50140a147bc
Kalil Smith-Nuevelle <kalilsn@gmail.com>
2024-09-03 16:39:12 -0500

    Rename instead of dropping

--- core/prisma/migrations/20240903150607_add_placeholder/migration.sql
+++ core/prisma/migrations/20240903150607_add_placeholder/migration.sql
@@ -1,9 +1,2 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `description` on the `form_elements` table. All the data in the column will be lost.
-
-*/
 -- AlterTable
-ALTER TABLE "form_elements" DROP COLUMN "description",
-ADD COLUMN     "placeholder" TEXT;
+ALTER TABLE "form_elements" RENAME COLUMN "description" TO "placeholder";
