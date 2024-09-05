1dc8e0ee90ae8b41cd50f05844f42d1171f8d4fd
Kalil Smith-Nuevelle <kalilsn@gmail.com>
2024-09-05 15:18:32 -0500

    Set default components

--- core/prisma/migrations/20240905145651_component_configuration/migration.sql
+++ core/prisma/migrations/20240905145651_component_configuration/migration.sql
@@ -11,3 +11,13 @@ CREATE TYPE "InputComponent" AS ENUM ('textArea', 'textInput', 'datePicker', 'ch
 ALTER TABLE "form_elements" DROP COLUMN "description",
 ADD COLUMN     "component" "InputComponent",
 ADD COLUMN     "config" JSONB;
+
+-- Set default form components
+UPDATE "form_elements" SET "component" = 'checkbox'::"InputComponent" FROM "pub_fields" WHERE "form_elements"."fieldId" = "pub_fields"."id" AND "pub_fields"."schemaName" = 'Boolean'::"CoreSchemaType";
+UPDATE "form_elements" SET "component" = 'textInput'::"InputComponent" FROM "pub_fields" WHERE "form_elements"."fieldId" = "pub_fields"."id" AND "pub_fields"."schemaName" = 'String'::"CoreSchemaType";
+UPDATE "form_elements" SET "component" = 'datePicker'::"InputComponent" FROM "pub_fields" WHERE "form_elements"."fieldId" = "pub_fields"."id" AND "pub_fields"."schemaName" = 'DateTime'::"CoreSchemaType";
+UPDATE "form_elements" SET "component" = 'textInput'::"InputComponent" FROM "pub_fields" WHERE "form_elements"."fieldId" = "pub_fields"."id" AND "pub_fields"."schemaName" = 'Email'::"CoreSchemaType";
+UPDATE "form_elements" SET "component" = 'fileUpload'::"InputComponent" FROM "pub_fields" WHERE "form_elements"."fieldId" = "pub_fields"."id" AND "pub_fields"."schemaName" = 'FileUpload'::"CoreSchemaType";
+UPDATE "form_elements" SET "component" = 'textInput'::"InputComponent" FROM "pub_fields" WHERE "form_elements"."fieldId" = "pub_fields"."id" AND "pub_fields"."schemaName" = 'URL'::"CoreSchemaType";
+UPDATE "form_elements" SET "component" = 'memberSelect'::"InputComponent" FROM "pub_fields" WHERE "form_elements"."fieldId" = "pub_fields"."id" AND "pub_fields"."schemaName" = 'MemberId'::"CoreSchemaType";
+UPDATE "form_elements" SET "component" = 'confidenceInterval'::"InputComponent" FROM "pub_fields" WHERE "form_elements"."fieldId" = "pub_fields"."id" AND "pub_fields"."schemaName" = 'Vector3'::"CoreSchemaType";
