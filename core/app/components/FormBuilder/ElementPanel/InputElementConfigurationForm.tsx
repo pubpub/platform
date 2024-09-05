a175f28311a59df822c6e74d44da7f31d9a3da78
Kalil Smith-Nuevelle <kalilsn@gmail.com>
2024-09-05 14:01:59 -0500

    Add basic component selection and label-only config

--- core/app/components/FormBuilder/ElementPanel/InputElementConfigurationForm.tsx
+++ core/app/components/FormBuilder/ElementPanel/InputElementConfigurationForm.tsx
@@ -12,15 +12,8 @@ import { Checkbox } from "ui/checkbox";
 import { Confidence } from "ui/customRenderers/confidence/confidence";
 import { FileUpload } from "ui/customRenderers/fileUpload/fileUpload";
 import { DatePicker } from "ui/date-picker";
-import {
-	Form,
-	FormControl,
-	FormDescription,
-	FormField,
-	FormItem,
-	FormLabel,
-	FormMessage,
-} from "ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "ui/form";
+import { ImagePlus } from "ui/icon";
 import { Input } from "ui/input";
 import { RadioGroup, RadioGroupCard } from "ui/radio-group";
 import { Switch } from "ui/switch";
@@ -33,54 +26,59 @@ import { isFieldInput } from "../types";
 type SchemaComponentData = {
 	name?: string;
 	placeholder?: string;
-	Component?: JSX.Element;
+	demoComponent?: JSX.Element;
 };
 
 const componentInfo: Record<InputComponent, SchemaComponentData> = {
 	[InputComponent.textArea]: {
 		name: "Textarea",
-		Component: <Textarea disabled placeholder="For long text" />,
+		demoComponent: <Textarea placeholder="For long text" />,
 	},
 	[InputComponent.textInput]: {
 		name: "Input",
 		placeholder: "For short text",
-		Component: <Input disabled placeholder="For short text" />,
+		demoComponent: <Input placeholder="For short text" />,
 	},
-	[InputComponent.checkbox]: { name: "Checkbox", Component: <Checkbox checked /> },
+	[InputComponent.checkbox]: { name: "Checkbox", demoComponent: <Checkbox checked /> },
 	[InputComponent.datePicker]: {
 		name: "Date picker",
-		Component: <DatePicker setDate={() => {}} />,
+		demoComponent: <DatePicker setDate={() => {}} />,
 	},
 	[InputComponent.fileUpload]: {
 		name: "File Upload",
-		Component: <FileUpload upload={() => {}} onUpdateFiles={() => {}} disabled />,
+		demoComponent: <ImagePlus size={30} />,
 	},
 	[InputComponent.memberSelect]: { name: "Member select" },
-	[InputComponent.confidenceInterval]: { name: "Combo Slider", Component: <Confidence /> },
+	[InputComponent.confidenceInterval]: {
+		name: "Combo Slider",
+		demoComponent: <Confidence value={[0, 50, 100]} min={0} max={100} />,
+	},
 } as const;
 
 const ComponentSelect = ({
 	components,
 	onChange,
+	value,
 }: {
 	components: InputComponent[];
+	value: InputComponent;
 	onChange: (component: InputComponent) => void;
 }) => {
 	return (
 		<RadioGroup
 			className="grid grid-cols-2 gap-3"
-			defaultValue={components[0]}
+			defaultValue={value}
 			onValueChange={onChange}
 		>
 			{components.map((c) => {
-				const { name, Component } = componentInfo[c];
+				const { name, demoComponent: Component } = componentInfo[c];
 				return (
-					<RadioGroupCard key={c} value={c} className="w-full max-w-sm">
-						<CardContent className="grid gap-4">{Component}</CardContent>
-						<hr />
-						<CardFooter>
-							<CardDescription>{name}</CardDescription>
-						</CardFooter>
+					<RadioGroupCard key={c} value={c} className="flex h-[124px] w-full flex-col">
+						<div className="flex h-[88px] w-full items-center justify-center p-3">
+							{Component}
+						</div>
+						<hr className="w-full" />
+						<div className="w-full text-center text-sm text-foreground">{name}</div>
 					</RadioGroupCard>
 				);
 			})}
@@ -97,14 +95,14 @@ export const InputElementConfigurationForm = ({ index }: Props) => {
 	if (!selectedElement || !isFieldInput(selectedElement)) {
 		return null;
 	}
-	console.log("selected element schema", selectedElement.schemaName);
+
 	const components = componentsBySchema[selectedElement.schemaName];
 	// const configSchema = componentConfigSchemas[selectedElement.schemaName]
 
 	const schema = z.object({
 		label: z.string().nullable().default(""),
 		required: z.boolean().nullable(),
-		component: z.nativeEnum(InputComponent).refine((c) => components.includes(c)),
+		component: z.nativeEnum(InputComponent),
 		//config: configSchema,
 	});
 
@@ -132,9 +130,16 @@ export const InputElementConfigurationForm = ({ index }: Props) => {
 				</div>
 				<div className="text-sm uppercase text-muted-foreground">Appearance</div>
 				<hr />
-				<ComponentSelect
-					onChange={(component) => form.setValue("component", component)}
-					components={components}
+				<FormField
+					control={form.control}
+					name="component"
+					render={({ field }) => (
+						<ComponentSelect
+							onChange={field.onChange}
+							value={field.value}
+							components={components}
+						/>
+					)}
 				/>
 				<FormField
 					control={form.control}
@@ -149,7 +154,7 @@ export const InputElementConfigurationForm = ({ index }: Props) => {
 						</FormItem>
 					)}
 				/>
-				{/* TODO: Autoform style rendering of componentConfig  */}
+				{/* TODO: Autoform style rendering of config  */}
 				<hr className="mt-4" />
 				<FormField
 					control={form.control}
