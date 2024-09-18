dabea40ba52a1e1a69792871ad37dd70637218ba
Allison King <aking@knowledgefutures.org>
2024-09-18 07:52:28 -0400

    Add move button to pub page (#622)
    * Replace mb's with gaps
    
    * Refactor move button so it can be reused more easily
    
    * Close popover when move is done
    
    * Do not render move if there are no sources or destinations
    
    * Add tests

---
+++ core/playwright/pub.spec.ts
@@ -0,0 +1,63 @@
+import type { Page } from "@playwright/test";
+
+import { expect, test } from "@playwright/test";
+
+import { CoreSchemaType } from "db/public";
+
+import { PubsPage } from "./fixtures/pubs-page";
+import { createCommunity, login } from "./helpers";
+
+const now = new Date().getTime();
+const COMMUNITY_SLUG = `playwright-test-community-${now}`;
+
+test.describe.configure({ mode: "serial" });
+
+let page: Page;
+
+test.beforeAll(async ({ browser }) => {
+	page = await browser.newPage();
+	await login({ page });
+	await createCommunity({
+		page,
+		community: { name: `test community ${now}`, slug: COMMUNITY_SLUG },
+	});
+});
+
+test.afterAll(async () => {
+	await page.close();
+});
+
+test.describe("Moving a pub", () => {
+	test("Can move a pub across linked stages", async () => {
+		const pubsPage = new PubsPage(page, COMMUNITY_SLUG);
+		await pubsPage.goToSeededPub();
+		await expect(page.getByTestId("current-stage")).toHaveText("Submitted");
+		// For this initial stage, there are only destinations ,no sources
+		await page.getByRole("button", { name: "Move" }).click();
+		const sources = page.getByTestId("sources");
+		const destinations = page.getByTestId("destinations");
+		await expect(sources).toHaveCount(0);
+		await destinations.getByRole("button", { name: "Ask Author For Consent" }).click();
+		await expect(page.getByTestId("current-stage")).toHaveText("Ask Author for Consent");
+
+		// Open the move modal again and expect to be able to move to sources and destinations
+		await page.getByRole("button", { name: "Move" }).click();
+		await expect(sources.getByRole("button", { name: "Submitted" })).toHaveCount(1);
+		await expect(destinations.getByRole("button", { name: "To Evaluate" })).toHaveCount(1);
+	});
+
+	test("No move button if pub is not in a linked stage", async () => {
+		const pubsPage = new PubsPage(page, COMMUNITY_SLUG);
+		await pubsPage.goTo();
+		await page.getByTestId("pub-dropdown-button").click();
+		await page.getByRole("button", { name: "Edit Pub" }).click();
+		await page.getByTestId("stage-selector").click();
+		// Shelved is its own node in stages
+		await page.getByRole("menuitem", { name: "Shelved" }).click();
+		await page.getByRole("button", { name: "Update Pub" }).click();
+
+		await pubsPage.goToSeededPub();
+		await expect(page.getByTestId("current-stage")).toHaveText("Shelved");
+		await expect(page.getByRole("button", { name: "Move" })).toHaveCount(0);
+	});
+});
