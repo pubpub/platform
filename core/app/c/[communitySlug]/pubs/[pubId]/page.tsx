1a9b9312d06fdce1372a1aa37e413cfa001e256d
Thomas F. K. Jorna <hello@tefkah.com>
2024-09-23 13:52:51 +0200

    fix: add action run modal to pub page

--- core/app/c/[communitySlug]/pubs/[pubId]/page.tsx
+++ core/app/c/[communitySlug]/pubs/[pubId]/page.tsx
@@ -7,6 +7,7 @@ import { AuthTokenType } from "db/public";
 import { Avatar, AvatarFallback, AvatarImage } from "ui/avatar";
 
 import Assign from "~/app/c/[communitySlug]/stages/components/Assign";
+import { ActionRunModal } from "~/app/components/ActionUI/ActionRunModal";
 import { PubsRunActionDropDownMenu } from "~/app/components/ActionUI/PubsRunActionDropDownMenu";
 import IntegrationActions from "~/app/components/IntegrationActions";
 import MembersAvatars from "~/app/components/MemberAvatar";
@@ -105,110 +106,108 @@ export default async function Page({
 	modalSearchParamsCache.parse(searchParams);
 
 	return (
-		<div className="flex flex-col space-y-4">
-			<div className="mb-8">
-				<h3 className="mb-2 text-xl font-bold">{pub.pubType.name}</h3>
-				<PubTitle pub={pub} />
-			</div>
-			<div className="flex flex-wrap space-x-4">
-				<div className="flex-1">
-					{pub.values
-						.filter((value) => {
-							return value.field.name !== "Title";
-						})
-						.map((value) => {
-							return (
-								<div className="mb-4" key={value.id}>
-									<div>{renderField(value)}</div>
-								</div>
-							);
-						})}
+		<>
+			<div className="flex flex-col space-y-4">
+				<div className="mb-8">
+					<h3 className="mb-2 text-xl font-bold">{pub.pubType.name}</h3>
+					<PubTitle pub={pub} />
 				</div>
-				<div className="w-64 rounded-lg bg-gray-50 p-4 font-semibold shadow-inner">
-					<div className="mb-4">
-						<div className="mb-1 text-lg font-bold">Current Stage</div>
-						<div className="ml-4 font-medium">
-							{pub.stages.map(({ stage }) => {
-								return <div key={stage.id}>{stage.name}</div>;
-							})}
-						</div>
-					</div>
-					<div className="mb-4">
-						<MembersAvatars pub={pub} />
-					</div>
-					<div className="mb-4">
-						<div className="mb-1 text-lg font-bold">Integrations</div>
-						<div>
-							<IntegrationActions pub={pub} token={token} />
-						</div>
-					</div>
-					<div className="mb-4">
-						<div className="mb-1 text-lg font-bold">Actions</div>
-						{actions && actions.length > 0 && stage ? (
-							<div>
-								<PubsRunActionDropDownMenu
-									actionInstances={actions}
-									pubId={pub.id as PubsId}
-									stage={stage!}
-									pageContext={{
-										params: params,
-										searchParams,
-									}}
-								/>
-							</div>
-						) : (
-							<div className="ml-4 font-medium">
-								Configure actions to run for this Pub in the stage management
-								settings
-							</div>
-						)}
-					</div>
-
-					<div className="mb-4">
-						<div className="mb-1 text-lg font-bold">Members</div>
-						<div className="flex flex-row flex-wrap">
-							{users.map((user) => {
+				<div className="flex flex-wrap space-x-4">
+					<div className="flex-1">
+						{pub.values
+							.filter((value) => {
+								return value.field.name !== "Title";
+							})
+							.map((value) => {
 								return (
-									<div key={user.id}>
-										<Avatar className="mr-2 h-8 w-8">
-											<AvatarImage src={user.avatar || undefined} />
-											<AvatarFallback>{user.firstName[0]}</AvatarFallback>
-										</Avatar>
+									<div className="mb-4" key={value.id}>
+										<div>{renderField(value)}</div>
 									</div>
 								);
 							})}
-						</div>
 					</div>
-					<div className="mb-4">
-						<div className="mb-1 text-lg font-bold">Assignee</div>
-						<div className="ml-4">
-							<Assign members={community.members} pub={pub} />
+					<div className="w-64 rounded-lg bg-gray-50 p-4 font-semibold shadow-inner">
+						<div className="mb-4">
+							<div className="mb-1 text-lg font-bold">Current Stage</div>
+							<div className="ml-4 font-medium">
+								{pub.stages.map(({ stage }) => {
+									return <div key={stage.id}>{stage.name}</div>;
+								})}
+							</div>
+						</div>
+						<div className="mb-4">
+							<MembersAvatars pub={pub} />
+						</div>
+						<div className="mb-4">
+							<div className="mb-1 text-lg font-bold">Integrations</div>
+							<div>
+								<IntegrationActions pub={pub} token={token} />
+							</div>
+						</div>
+						<div className="mb-4">
+							<div className="mb-1 text-lg font-bold">Actions</div>
+							{actions && actions.length > 0 && stage ? (
+								<div>
+									<PubsRunActionDropDownMenu
+										actionInstances={actions}
+										pubId={pub.id as PubsId}
+									/>
+								</div>
+							) : (
+								<div className="ml-4 font-medium">
+									Configure actions to run for this Pub in the stage management
+									settings
+								</div>
+							)}
+						</div>
+
+						<div className="mb-4">
+							<div className="mb-1 text-lg font-bold">Members</div>
+							<div className="flex flex-row flex-wrap">
+								{users.map((user) => {
+									return (
+										<div key={user.id}>
+											<Avatar className="mr-2 h-8 w-8">
+												<AvatarImage src={user.avatar || undefined} />
+												<AvatarFallback>{user.firstName[0]}</AvatarFallback>
+											</Avatar>
+										</div>
+									);
+								})}
+							</div>
+						</div>
+						<div className="mb-4">
+							<div className="mb-1 text-lg font-bold">Assignee</div>
+							<div className="ml-4">
+								<Assign members={community.members} pub={pub} />
+							</div>
 						</div>
 					</div>
 				</div>
+				<div>
+					<h2 className="text-xl font-bold">Pub Contents</h2>
+					<p className="text-muted-foreground">
+						Use the "Add New Pub" button below to create a new pub and add it to this
+						pub's contents.
+					</p>
+				</div>
+				<div className="mb-2">
+					<PubCreateButton
+						label="Add New Pub"
+						communityId={community.id as CommunitiesId}
+						parentId={pub.id as PubsId}
+						searchParams={searchParams}
+					/>
+				</div>
+				<Suspense fallback={<SkeletonTable /> /* does not exist yet */}>
+					<PubChildrenTableWrapper
+						communitySlug={params.communitySlug}
+						pageContext={{ params, searchParams }}
+						parentPubId={pub.id as PubsId}
+					/>
+				</Suspense>
 			</div>
-			<div>
-				<h2 className="text-xl font-bold">Pub Contents</h2>
-				<p className="text-muted-foreground">
-					Use the "Add New Pub" button below to create a new pub and add it to this pub's
-					contents.
-				</p>
-			</div>
-			<div className="mb-2">
-				<PubCreateButton
-					label="Add New Pub"
-					communityId={community.id as CommunitiesId}
-					parentId={pub.id as PubsId}
-					searchParams={searchParams}
-				/>
-			</div>
-			<Suspense fallback={<SkeletonTable /> /* does not exist yet */}>
-				<PubChildrenTableWrapper
-					communitySlug={params.communitySlug}
-					pageContext={{ params, searchParams }}
-					parentPubId={pub.id as PubsId}
-				/>
-			</Suspense>
-		</div>
+			<ActionRunModal pageContext={{ params, searchParams }} />
+		</>
 	);
 }
