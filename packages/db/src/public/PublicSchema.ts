01ec290f513bf4f0eb4219e51e5b379e9f96929d
allisonking <aking@knowledgefutures.org>
2024-09-23 12:26:53 -0400

    Migration for not null constraint and to add Null to CoreSchemaType

--- packages/db/src/public/PublicSchema.ts
+++ packages/db/src/public/PublicSchema.ts
@@ -38,6 +38,18 @@ import type { StagesTable } from "./Stages";
 import type { UsersTable } from "./Users";
 
 export interface PublicSchema {
+	api_access_tokens: ApiAccessTokensTable;
+
+	api_access_logs: ApiAccessLogsTable;
+
+	api_access_permissions: ApiAccessPermissionsTable;
+
+	form_to_permissions: FormToPermissionsTable;
+
+	form_elements: FormElementsTable;
+
+	sessions: SessionsTable;
+
 	_prisma_migrations: PrismaMigrationsTable;
 
 	users: UsersTable;
@@ -95,16 +107,4 @@ export interface PublicSchema {
 	action_runs: ActionRunsTable;
 
 	forms: FormsTable;
-
-	api_access_tokens: ApiAccessTokensTable;
-
-	api_access_logs: ApiAccessLogsTable;
-
-	api_access_permissions: ApiAccessPermissionsTable;
-
-	form_to_permissions: FormToPermissionsTable;
-
-	form_elements: FormElementsTable;
-
-	sessions: SessionsTable;
 }
