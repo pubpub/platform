0570087c428b83b8a180fa061687ecbb97b4315f
Thomas F. K. Jorna <hello@tefkah.com>
2024-09-24 15:29:40 +0200

    refactor: rewrite the API token permission field logic to be correct and more readable

--- core/app/c/[communitySlug]/settings/tokens/PermissionField.tsx
+++ core/app/c/[communitySlug]/settings/tokens/PermissionField.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { createContext, useContext, useMemo } from "react";
+import { ControllerRenderProps } from "react-hook-form";
+
 import type { ApiAccessScope } from "db/public";
 import type { ApiAccessPermissionConstraintsInput, CreateTokenFormContext } from "db/types";
 import { ApiAccessType } from "db/public";
@@ -9,7 +12,7 @@ import { FormControl, FormField, FormItem, FormLabel, FormMessage } from "ui/for
 import { MultiSelect } from "ui/multi-select";
 import { Popover, PopoverContent, PopoverTrigger } from "ui/popover";
 
-import type { CreateTokenForm } from "./CreateTokenForm";
+import type { CreateTokenForm, CreateTokenFormSchema } from "./CreateTokenForm";
 
 /**
  * This is a type for a configuration object for form fields. It allows you to specify
@@ -28,6 +31,7 @@ import type { CreateTokenForm } from "./CreateTokenForm";
  * 		read: boolean | undefined
  *   },
  *   pub: {
+ * 		read: boolean | undefined,
  * 		write: boolean | {
  * 			stages: Stages[]
  * 		} | undefined
@@ -37,121 +41,170 @@ import type { CreateTokenForm } from "./CreateTokenForm";
  * into a type that looks like
  * ```ts
  * {
- * 	 community: {
- * 		read: ({ context, form, value, onChange }: {
- * 			context: CreateTokenFormContext;
- * 			form: CreateTokenForm;
- * 			value: boolean;
- * 			onChange: (...args: any[]) => void;
- *        }) => JSX.Element
- * },
+ * 	 community: null,
  *   pub: {
- * 		write?: ({ context, form, value, onChange }: {
- * 			context: CreateTokenFormContext;
- * 			form: CreateTokenForm;
+ * 		write: ({ context, form, value, onChange }: {
  * 			value: boolean | {
  * 				stages: Stages[]
  * 			};
  * 			onChange: (...args: any[]) => void;
  *        }) => JSX.Element
- *   } | undefined
+ *   }
  * }
  * ```
- * The idea is that you can customize what form fields look like depending on the
+ * The idea is that you mustcustomize what form fields look like depending on the
  * `permissionsSchema` configuration.
  * E.g. the `write` field of the `pub` scope can be configured to only write to specific stages.
  * You should be able to configure a custom element to do this.
  *
  */
-type PermissionContraintMap = {
-	[K in keyof ApiAccessPermissionConstraintsInput as ApiAccessPermissionConstraintsInput[K] extends never
-		? never
-		: K]: {
-		[T in keyof ApiAccessPermissionConstraintsInput[K]]: ({
-			form,
-			context,
-			value,
-		}: {
-			form: CreateTokenForm;
-			context: CreateTokenFormContext;
-			value: boolean | ApiAccessPermissionConstraintsInput[K][T];
-			onChange: (...args: any[]) => void;
-		}) => JSX.Element;
+type PermissionContraintMap<I = ApiAccessPermissionConstraintsInput> = {
+	[K in ScopesWithSpecialConstraint<I>]: {
+		[T in SpecialConstraintKeys<I[K]>]: CustomConstraintFormElement<I[K][T]>;
+	} & {
+		[T in NormalConstraintKeys<I[K]>]?: undefined;
 	};
+} & {
+	[K in ScopesWithOnlyNormalConstraints<I>]: null;
 };
+
+/**
+ * The keys of a scope that have a constraint different from `boolean | undefined`
+ *
+ * @example
+ * type Keys = SpecialConstraintKeys<{
+ * 	write: boolean | undefined
+ * 	read: boolean | {
+ * 		stages: Stages[]
+ * 	}
+ * }>
+ * // Keys = 'read', because it has a signature different from `boolean | undefined`
+ */
+type SpecialConstraintKeys<Scope, Keys extends keyof Scope = keyof Scope> = Keys extends Keys
+	? Scope[Keys] extends boolean | undefined
+		? never
+		: Keys
+	: never;
+
+type NormalConstraintKeys<Scope, Keys extends keyof Scope = keyof Scope> = Exclude<
+	Keys,
+	SpecialConstraintKeys<Scope, Keys>
+>;
+
+/**
+ * Only those scopes that have a constraint different from `boolean | undefined`
+ *
+ * @example
+ * type Keys = ScopesWithSpecialConstraint<{
+ *   pub: {
+ *     write: boolean | undefined | number
+ *   },
+ *   community: {
+ *     read: boolean | undefined
+ *   }
+ * }>
+ * // Keys = 'pub', because it has a signature different from `boolean | undefined`
+ *
+ */
+type ScopesWithSpecialConstraint<
+	I = ApiAccessPermissionConstraintsInput,
+	K extends keyof I = keyof I,
+> = K extends K
+	? keyof I[K] extends NormalConstraintKeys<I[K]>
+		? never // this means the scope only has normal constraints
+		: K
+	: never;
+
+type ScopesWithOnlyNormalConstraints<
+	I = ApiAccessPermissionConstraintsInput,
+	K extends keyof I = keyof I,
+> = Exclude<K, ScopesWithSpecialConstraint<I, K>>;
+
+type CustomConstraintFormElement<Value> = (props: {
+	value: boolean | Value;
+	onChange: (...args: any[]) => void;
+}) => JSX.Element;
+
+const CreateTokenFormContextContext = createContext<CreateTokenFormContext>({ stages: [] });
 
 /**
  * Here you configure the specific form elements for each permission type
  */
 const permissionContraintMap: PermissionContraintMap = {
-	community: {},
+	community: null,
 	pub: {
-		write: ({ context, form, value, onChange }) => (
-			<div className="flex flex-col gap-2">
-				<h3 className="text-lg font-semibold">Stages</h3>
-				<span className="text-sm text-muted-foreground">
-					Select the stages this token can create pubs in/move pubs to
-				</span>
-				<MultiSelect
-					variant="inverted"
-					options={context.stages.map((stage) => ({
-						label: stage.name,
-						value: stage.id,
-					}))}
-					/**
-					 * This just means: if it is set to `true`, allow it to act on all stages.
-					 * If it is set to `false`, allow it to act on no stages.
-					 * Otherwise just reuse the value of the `stages` field. (this is a bit of a cop-out as this situation should never come to pass)
-					 */
-					defaultValue={
-						value === true
-							? context.stages.map((stage) => stage.id)
-							: !value
-								? []
-								: value.stages
-					}
-					onValueChange={(value) => {
-						onChange(
-							value.length > 0 && value.length !== context.stages.length
-								? { stages: value }
-								: true
-						);
-					}}
-					animation={0}
-				/>
-			</div>
-		),
+		[ApiAccessType.write]: ({ value, onChange }) => {
+			const context = useContext(CreateTokenFormContextContext);
+			return (
+				<div className="flex flex-col gap-2">
+					<h3 className="text-lg font-semibold">Stages</h3>
+					<span className="text-sm text-muted-foreground">
+						Select the stages this token can create pubs in/move pubs to
+					</span>
+					<MultiSelect
+						variant="inverted"
+						options={context.stages.map((stage) => ({
+							label: stage.name,
+							value: stage.id,
+						}))}
+						/**
+						 * This just means: if it is set to `true`, allow it to act on all stages.
+						 * If it is set to `false`, allow it to act on no stages.
+						 * Otherwise just reuse the value of the `stages` field. (this is a bit of a cop-out as this situation should never come to pass)
+						 */
+						defaultValue={
+							value === true
+								? context.stages.map((stage) => stage.id)
+								: !value
+									? []
+									: value.stages
+						}
+						onValueChange={(value) => {
+							onChange(
+								value.length > 0 && value.length !== context.stages.length
+									? { stages: value }
+									: true
+							);
+						}}
+						animation={0}
+					/>
+				</div>
+			);
+		},
 	},
 	stage: {
-		read: ({ context, form, value, onChange }) => (
-			<div className="flex flex-col gap-2">
-				<h3 className="text-lg font-semibold">Stages</h3>
-				<span className="text-sm text-muted-foreground">
-					Select the stages this token can read
-				</span>
-				<MultiSelect
-					variant="inverted"
-					options={context.stages.map((stage) => ({
-						label: stage.name,
-						value: stage.id,
-					}))}
-					defaultValue={
-						value === true
-							? context.stages.map((stage) => stage.id)
-							: !value
-								? []
-								: value.stages
-					}
-					onValueChange={(value) => {
-						onChange(value.length > 0 ? value : true);
-					}}
-					animation={0}
-				/>
-			</div>
-		),
+		[ApiAccessType.read]: ({ value, onChange }) => {
+			const context = useContext(CreateTokenFormContextContext);
+			return (
+				<div className="flex flex-col gap-2">
+					<h3 className="text-lg font-semibold">Stages</h3>
+					<span className="text-sm text-muted-foreground">
+						Select the stages this token can read
+					</span>
+					<MultiSelect
+						variant="inverted"
+						options={context.stages.map((stage) => ({
+							label: stage.name,
+							value: stage.id,
+						}))}
+						defaultValue={
+							value === true
+								? context.stages.map((stage) => stage.id)
+								: !value
+									? []
+									: value.stages
+						}
+						onValueChange={(value) => {
+							onChange(value.length > 0 ? value : true);
+						}}
+						animation={0}
+					/>
+				</div>
+			);
+		},
 	},
-	member: {},
-	pubType: {},
+	member: null,
+	pubType: null,
 };
 
 export const PermissionField = ({
@@ -166,65 +219,114 @@ export const PermissionField = ({
 	context: CreateTokenFormContext;
 }) => {
 	return (
-		<FormField
-			control={form.control}
-			name={`permissions.${name}`}
-			render={({ field }) => (
-				<div className="">
-					<h3 className="text-sm">{prettyName}</h3>
-					<div className="grid h-10 grid-cols-3 gap-2">
-						{Object.values(ApiAccessType).map((type) => (
-							<FormField
-								key={type}
-								control={form.control}
-								name={`permissions.${name}.${type}`}
-								render={({ field }) => {
-									const extraContraints = permissionContraintMap[name]?.[type];
-
-									return (
-										<FormItem className="flex items-center gap-x-2 space-y-0">
-											<FormControl>
-												<Checkbox
-													checked={Boolean(field.value)}
-													onCheckedChange={(change) => {
-														if (typeof change === "boolean") {
-															field.onChange(change);
-														}
-													}}
-												/>
-											</FormControl>
-											<FormLabel>{type}</FormLabel>
-											<FormMessage />
-											{field.value && extraContraints && (
-												<Popover>
-													<PopoverTrigger asChild>
-														<Button
-															variant="secondary"
-															type="button"
-															size="sm"
-														>
-															Options
-														</Button>
-													</PopoverTrigger>
-													<PopoverContent>
-														{extraContraints({
-															context,
-															form,
-															// @ts-ignore FIXME: add better error handling
-															value: field.value,
-															onChange: field.onChange,
-														})}
-													</PopoverContent>
-												</Popover>
-											)}
-										</FormItem>
-									);
-								}}
-							/>
-						))}
+		<CreateTokenFormContextContext.Provider value={context}>
+			<FormField
+				control={form.control}
+				name={`permissions.${name}`}
+				render={({ field }) => (
+					<div className="">
+						<h3 className="text-sm">{prettyName}</h3>
+						<div className="grid h-10 grid-cols-3 gap-2">
+							{Object.values(ApiAccessType).map((type) => (
+								<FormField
+									key={type}
+									control={form.control}
+									name={`permissions.${name}.${type}`}
+									render={ConstraintFormFieldRender}
+								/>
+							))}
+						</div>
 					</div>
-				</div>
-			)}
-		/>
+				)}
+			/>
+		</CreateTokenFormContextContext.Provider>
+	);
+};
+
+function FormItemWrapper({
+	children,
+	checked,
+
+	onChange,
+	type,
+}: {
+	children?: React.ReactNode;
+	checked: boolean;
+	onChange: (change: boolean) => void;
+	type: ApiAccessType;
+}) {
+	return (
+		<FormItem className="flex items-center gap-x-2 space-y-0">
+			<FormControl>
+				<Checkbox
+					checked={checked}
+					onCheckedChange={(change) => {
+						if (typeof change === "boolean") {
+							onChange(change);
+						}
+					}}
+				/>
+			</FormControl>
+			<FormLabel>{type}</FormLabel>
+			{children}
+			<FormMessage />
+		</FormItem>
+	);
+}
+
+type SplitPath<Path extends `${string}.${string}.${string}`> =
+	Path extends `${infer _permissions extends "permissions"}.${infer scope extends ApiAccessScope}.${infer type extends ApiAccessType}`
+		? [_permissions, scope, type]
+		: never;
+
+export const ConstraintFormFieldRender = ({
+	field,
+}: {
+	field: ControllerRenderProps<
+		CreateTokenFormSchema,
+		`permissions.${ApiAccessScope}.${ApiAccessType}`
+	>;
+}) => {
+	const [_permissions, scope, type] = field.name.split(".") as SplitPath<typeof field.name>;
+
+	const ExtraContrainstsFormItem = useMemo(() => {
+		const scopeConstraints = permissionContraintMap[scope];
+
+		if (!scopeConstraints || !(type in scopeConstraints)) {
+			return null;
+		}
+
+		if (!(type in scopeConstraints)) {
+			return null;
+		}
+
+		const ExtraContraints = scopeConstraints[type];
+
+		if (!ExtraContraints) {
+			return null;
+		}
+
+		return ExtraContraints;
+	}, [scope, type]);
+
+	if (!ExtraContrainstsFormItem) {
+		return (
+			<FormItemWrapper checked={Boolean(field.value)} onChange={field.onChange} type={type} />
+		);
+	}
+
+	return (
+		<FormItemWrapper checked={Boolean(field.value)} onChange={field.onChange} type={type}>
+			<Popover>
+				<PopoverTrigger asChild>
+					<Button variant="secondary" type="button" size="sm">
+						Options
+					</Button>
+				</PopoverTrigger>
+				<PopoverContent>
+					<ExtraContrainstsFormItem value={field.value} onChange={field.onChange} />
+				</PopoverContent>
+			</Popover>
+		</FormItemWrapper>
 	);
 };
