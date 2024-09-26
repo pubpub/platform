174e65a1e084b7ba0ff9b647dea952b2a8043da1
Thomas F. K. Jorna <hello@tefkah.com>
2024-09-26 13:37:57 +0200

    fix: make next build use less memory

--- core/next.config.mjs
+++ core/next.config.mjs
@@ -5,14 +5,15 @@ import { withSentryConfig } from "@sentry/nextjs";
 
 import "./lib/env/env.mjs";
 
-// import { PHASE_DEVELOPMENT_SERVER } from "next/constants";
-
 /**
  * @type {import("next").NextConfig}
  */
 const nextConfig = {
 	eslint: {
 		ignoreDuringBuilds: true,
+	},
+	typescript: {
+		ignoreBuildErrors: true,
 	},
 	reactStrictMode: true,
 	/**
@@ -38,6 +39,7 @@ const nextConfig = {
 	},
 	experimental: {
 		instrumentationHook: true,
+		webpackBuildWorker: true,
 		serverComponentsExternalPackages: [
 			"@aws-sdk",
 			// without this here, next will sort of implode and no longer compile and serve pages properly
@@ -52,42 +54,46 @@ const nextConfig = {
 		if (isServer) {
 			config.ignoreWarnings = [{ module: /opentelemetry/ }];
 		}
+
+		// this decreases (somethow) the amount of memory used by wepbpack
+		// https://nextjs.org/docs/app/building-your-application/optimizing/memory-usage#disable-webpack-cache
+		if (config.cache && !dev) {
+			config.cache = Object.freeze({
+				type: "memory",
+			});
+		}
+
 		return config;
 	},
 };
 
 const modifiedConfig = withPreconstruct(
-	withSentryConfig(
-		nextConfig,
-		{
-			// For all available options, see:
-			// https://github.com/getsentry/sentry-webpack-plugin#options
+	withSentryConfig(nextConfig, {
+		// For all available options, see:
+		// https://github.com/getsentry/sentry-webpack-plugin#options
 
-			// Suppresses source map uploading logs during build
-			silent: true,
-			org: "kfg",
-			project: "v7-core",
-		},
-		{
-			// For all available options, see:
-			// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+		// Suppresses source map uploading logs during build
+		silent: true,
+		org: "kfg",
+		project: "v7-core",
+		// For all available options, see:
+		// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
 
-			// Upload a larger set of source maps for prettier stack traces (increases build time)
-			widenClientFileUpload: true,
+		// Upload a larger set of source maps for prettier stack traces (increases build time)
+		widenClientFileUpload: true,
 
-			// Transpiles SDK to be compatible with IE11 (increases bundle size)
-			transpileClientSDK: true,
+		// Transpiles SDK to be compatible with IE11 (increases bundle size)
+		// transpileClientSDK: true,
 
-			// Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
-			// tunnelRoute: "/monitoring",
+		// Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+		// tunnelRoute: "/monitoring",
 
-			// Hides source maps from generated client bundles
-			hideSourceMaps: true,
+		// Hides source maps from generated client bundles
+		hideSourceMaps: true,
 
-			// Automatically tree-shake Sentry logger statements to reduce bundle size
-			disableLogger: true,
-		}
-	)
+		// Automatically tree-shake Sentry logger statements to reduce bundle size
+		disableLogger: true,
+	})
 );
 
 export default modifiedConfig;
