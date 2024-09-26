07be5c21d79b78878cebe52e2a542508fd371726
Allison King <aking@knowledgefutures.org>
2024-09-26 11:51:33 -0400

    Use two partial indexes instead of NOT NULLS which is not available in pg14 (#661)

--- core/prisma/migrations/20240923161145_add_null_core_schema_type_and_make_pubvalues_unique_constraint_not_null_unique/migration.sql
+++ core/prisma/migrations/20240923161145_add_null_core_schema_type_and_make_pubvalues_unique_constraint_not_null_unique/migration.sql
@@ -5,5 +5,9 @@ ALTER TYPE "CoreSchemaType" ADD VALUE 'Null';
 -- We want to be able to still have a unique constraint on just pubId and fieldId if there is a null relatedPubId
 BEGIN;
 DROP INDEX "pub_values_pubId_relatedPubId_fieldId_key";
-CREATE UNIQUE INDEX "pub_values_pubId_relatedPubId_fieldId_key" ON "pub_values"("pubId", "relatedPubId", "fieldId") NULLS NOT DISTINCT;
+CREATE UNIQUE INDEX "pub_values_pubId_relatedPubId_fieldId_key" ON "pub_values"("pubId", "relatedPubId", "fieldId")
+WHERE "relatedPubId" IS NOT NULL;
+
+CREATE UNIQUE INDEX "pub_values_pubId_fieldId_key" ON "pub_values"("pubId", "fieldId")
+WHERE "relatedPubId" IS NULL;
 COMMIT;
