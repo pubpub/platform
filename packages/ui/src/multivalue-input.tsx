c262243f30816730994bff8b4eee2de41584e426
allisonking <aking@knowledgefutures.org>
2024-09-26 17:51:49 -0400

    Sort components

--- packages/ui/src/multivalue-input.tsx
+++ packages/ui/src/multivalue-input.tsx
@@ -1,8 +1,11 @@
 // Adapted from https://gist.github.com/enesien/03ba5340f628c6c812b306da5fedd1a4
 
+import type { Active, DragEndEvent } from "@dnd-kit/core";
 import type { Dispatch, SetStateAction } from "react";
 
 import React, { forwardRef, useState } from "react";
+import { DndContext } from "@dnd-kit/core";
+import { arrayMove, SortableContext, useSortable } from "@dnd-kit/sortable";
 
 import type { InputProps } from "./input";
 import { Badge } from "./badge";
@@ -15,9 +18,48 @@ type MultiValueInputProps = Omit<InputProps, "onChange"> & {
 	onChange: Dispatch<SetStateAction<string[]>>;
 };
 
+const SortableValue = ({
+	value,
+	onRemove,
+	isActive,
+}: {
+	value: string;
+	onRemove: (v: string) => void;
+	isActive: boolean;
+}) => {
+	const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id: value });
+
+	const style = transform
+		? {
+				transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+				transition,
+				zIndex: isActive ? 50 : undefined,
+			}
+		: undefined;
+
+	return (
+		<Badge ref={setNodeRef} style={style} {...attributes} className="bg-muted-foreground py-1">
+			<Button {...listeners} variant="ghost" className="mr-1 h-5 px-0">
+				<GripVertical size="12"></GripVertical>
+			</Button>
+			{value}
+			<Button
+				onClick={() => {
+					onRemove(value);
+				}}
+				variant="ghost"
+				className="ml-2 h-3 p-0"
+			>
+				<XCircle size="12"></XCircle>
+			</Button>
+		</Badge>
+	);
+};
+
 export const MultiValueInput = forwardRef<HTMLInputElement, MultiValueInputProps>(
 	({ values, onChange, ...props }, ref) => {
 		const [pendingValue, setPendingValue] = useState("");
+		const [activeDrag, setActiveDrag] = useState<Active | null>(null);
 
 		const addPendingValue = () => {
 			if (pendingValue) {
@@ -27,8 +69,18 @@ export const MultiValueInput = forwardRef<HTMLInputElement, MultiValueInputProps
 			}
 		};
 
+		const handleDragEnd = (event: DragEndEvent) => {
+			const { active, over } = event;
+			if (over && active.id !== over.id) {
+				const activeIndex: number = values.findIndex((v) => v === active.id);
+				const overIndex: number = values.findIndex((v) => v === over.id);
+				onChange(arrayMove(values, activeIndex, overIndex));
+			}
+			setActiveDrag(null);
+		};
+
 		return (
-			<div className="flex flex-col gap-2">
+			<div className="isolate flex flex-col gap-2">
 				<Input
 					value={pendingValue}
 					onChange={(e) => setPendingValue(e.target.value)}
@@ -43,27 +95,29 @@ export const MultiValueInput = forwardRef<HTMLInputElement, MultiValueInputProps
 					ref={ref}
 				/>
 
-				<div className="flex flex-wrap gap-x-2 gap-y-2">
-					{values.map((value) => {
-						return (
-							<Badge key={value} className="bg-muted-foreground py-1">
-								<Button variant="ghost" className="mr-1 h-5 px-0">
-									<GripVertical size="12"></GripVertical>
-								</Button>
-								{value}
-								<Button
-									onClick={() => {
-										onChange(values.filter((v) => v !== value));
-									}}
-									variant="ghost"
-									className="ml-2 h-3 p-0"
-								>
-									<XCircle size="12"></XCircle>
-								</Button>
-							</Badge>
-						);
-					})}
-				</div>
+				<DndContext
+					onDragStart={({ active }) => {
+						setActiveDrag(active);
+					}}
+					onDragEnd={handleDragEnd}
+				>
+					<SortableContext items={values}>
+						<div className="flex flex-wrap gap-x-2 gap-y-2">
+							{values.map((value) => {
+								return (
+									<SortableValue
+										key={value}
+										value={value}
+										onRemove={(valueToRemove) =>
+											onChange(values.filter((v) => v !== valueToRemove))
+										}
+										isActive={activeDrag?.id === value}
+									/>
+								);
+							})}
+						</div>
+					</SortableContext>
+				</DndContext>
 			</div>
 		);
 	}
