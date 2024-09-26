f86a93a43be7e99e91612f86d31c266b164e89ac
Thomas F. K. Jorna <hello@tefkah.com>
2024-09-26 15:15:27 +0200

    list

--- core/playwright.config.ts
+++ core/playwright.config.ts
@@ -40,7 +40,7 @@ export default defineConfig({
 	],
 	timeout: process.env.CI ? 10 * 1000 : 10 * 60 * 1000,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
-	reporter: "html",
+	reporter: process.env.CI ? "list" : "html",
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 	use: {
 		/* Base URL to use in actions like `await page.goto('/')`. */
