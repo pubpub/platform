7236f0481454eb0314a8410baff70d0860086703
Allison King <aking@knowledgefutures.org>
2024-10-01 06:11:52 -0700

    Multivalue component (#667)
    * Add story for MultiSelect
    
    * Initial MultiValueInput
    
    * Tag styling
    
    * Sort components
    
    * Add forwardRef to badge component
    
    * Close tag
    
    * Add storybook component test
    
    * Add placeholder example
    
    * CSS tweaks
    
    * Add form usage
    
    * Configure ui package with new component

---
+++ packages/ui/src/stories/MultiSelect.stories.tsx
@@ -0,0 +1,39 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import React from "react";
+import { fn } from "@storybook/test";
+
+import { MultiSelect } from "../multi-select";
+
+const meta: Meta<typeof MultiSelect> = {
+	title: "MultiSelect",
+	component: MultiSelect,
+	tags: ["autodocs"],
+	argTypes: {},
+	args: { onValueChange: fn() },
+};
+export default meta;
+
+type Story = StoryObj<typeof MultiSelect>;
+
+export const NoneSelected: Story = {
+	args: {
+		defaultValue: [],
+		options: [
+			{ label: "Cat", value: "cat" },
+			{ label: "Dog", value: "dog" },
+			{ label: "Squirrel", value: "squirrel" },
+		],
+	},
+};
+
+export const Selected: Story = {
+	args: {
+		defaultValue: ["cat", "dog"],
+		options: [
+			{ label: "Cat", value: "cat" },
+			{ label: "Dog", value: "dog" },
+			{ label: "Squirrel", value: "squirrel" },
+		],
+	},
+};
