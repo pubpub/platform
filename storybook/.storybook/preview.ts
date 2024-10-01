7ddf51d19fcf8cb7906968fed86496a7c28d4de0
allisonking <aking@knowledgefutures.org>
2024-10-01 16:43:15 -0700

    Add storybook to top level

---
+++ storybook/.storybook/preview.ts
@@ -0,0 +1,17 @@
+import type { Preview } from "@storybook/react";
+
+// Import our tailwind styles
+import "ui/styles.css";
+
+const preview: Preview = {
+	parameters: {
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i,
+			},
+		},
+	},
+};
+
+export default preview;
