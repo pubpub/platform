c83e2247f37d4f54d06fcff603870f4f858b37b9
Thomas F. K. Jorna <hello@tefkah.com>
2024-10-01 13:35:19 +0200

    dev: up test timeout to 20seconds in CI

--- core/playwright.config.ts
+++ core/playwright.config.ts
@@ -34,7 +34,8 @@ export default defineConfig({
 			reuseExistingServer: true,
 		},
 	],
-	timeout: process.env.CI ? 10 * 1000 : 10 * 60 * 1000,
+	// max 20 seconds per test in CI
+	timeout: process.env.CI ? 20 * 1000 : 10 * 60 * 1000,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
 	reporter: process.env.CI ? "list" : "html",
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
