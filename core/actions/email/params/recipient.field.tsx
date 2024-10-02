6edc095055a7b0c45792e3fadc17395520b5878a
Kalil Smith-Nuevelle <kalilsn@gmail.com>
2024-10-02 16:25:06 -0500

    Fix some missed renames

--- core/actions/email/params/recipient.field.tsx
+++ core/actions/email/params/recipient.field.tsx
@@ -1,7 +1,7 @@
 import type { MembersId } from "db/public";
 
 import { defineActionFormFieldServerComponent } from "~/actions/_lib/custom-form-field/defineConfigServerComponent";
-import { UserSelectServer } from "~/app/components/UserSelect/UserSelectServer";
+import { MemberSelectServer } from "~/app/components/MemberSelect/MemberSelectServer";
 import { db } from "~/kysely/database";
 import { autoCache } from "~/lib/server/cache/autoCache";
 import { action } from "../action";
@@ -18,7 +18,7 @@ const component = defineActionFormFieldServerComponent(
 		const query = pageContext.searchParams?.[queryParamName] as string | undefined;
 
 		return (
-			<UserSelectServer
+			<MemberSelectServer
 				fieldName="recipient"
 				fieldLabel="Recipient email address"
 				community={community}
