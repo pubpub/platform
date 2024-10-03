42c837324e6e41d80b81aed19aeceae0646051d4
Kalil Smith-Nuevelle <kalilsn@gmail.com>
2024-10-03 13:09:41 -0500

    Remove incorrect comments

--- core/prisma/migrations/20241002220742_remove_label_column_from_inputs/migration.sql
+++ core/prisma/migrations/20241002220742_remove_label_column_from_inputs/migration.sql
@@ -1,15 +1,7 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `label` on the `form_elements` table. All the data in the column will be lost.
-  - A unique constraint covering the columns `[pubId,relatedPubId,fieldId]` on the table `pub_values` will be added. If there are existing duplicate values, this will fail.
-
-*/
-
 -- Move existing labels into the config object
-UPDATE "form_elements" 
-  SET config = jsonb_build_object('label', "form_elements"."label") 
+UPDATE "form_elements"
+  SET config = jsonb_build_object('label', "form_elements"."label")
   WHERE component != 'checkbox'::"InputComponent" AND type = 'pubfield'::"ElementType";
-UPDATE "form_elements" 
+UPDATE "form_elements"
   SET config = jsonb_build_object('groupLabel', "form_elements"."label")
   WHERE component = 'checkbox'::"InputComponent" AND type = 'pubfield'::"ElementType";
