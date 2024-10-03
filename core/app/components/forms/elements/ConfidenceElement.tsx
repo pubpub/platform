36ccc32944aa28db5d766cf883d9a84f5af07a5b
allisonking <aking@knowledgefutures.org>
2024-10-03 08:24:10 -0700

    Fix onChange

--- core/app/components/forms/elements/ConfidenceElement.tsx
+++ core/app/components/forms/elements/ConfidenceElement.tsx
@@ -27,22 +27,27 @@ export const Vector3Element = ({ label, name }: ElementProps) => {
 			control={control}
 			name={name}
 			defaultValue={[0, 0, 0]}
-			render={({ field }) => (
-				<FormItem className="mb-6">
-					<FormLabel className="text-[0.9em]">{label}</FormLabel>
-					<FormControl>
-						<Confidence
-							{...field}
-							disabled={!isEnabled}
-							min={0}
-							max={100}
-							onValueChange={(event) => field.onChange(event)}
-							className="confidence"
-						/>
-					</FormControl>
-					<FormMessage />
-				</FormItem>
-			)}
+			render={({ field }) => {
+				// Need to pass the field's onChange as onValueChange in Confidence
+				// and make sure it is not passed in as the default onChange
+				const { onChange, ...fieldProps } = field;
+				return (
+					<FormItem className="mb-6">
+						<FormLabel className="text-[0.9em]">{label}</FormLabel>
+						<FormControl>
+							<Confidence
+								{...fieldProps}
+								disabled={!isEnabled}
+								min={0}
+								max={100}
+								onValueChange={onChange}
+								className="confidence"
+							/>
+						</FormControl>
+						<FormMessage />
+					</FormItem>
+				);
+			}}
 		/>
 	);
 };
