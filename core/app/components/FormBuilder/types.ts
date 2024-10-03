2ec0c4683e9503fe2e63a182c70609487fe4e48e
Kalil Smith-Nuevelle <kalilsn@gmail.com>
2024-10-03 15:11:45 -0500

    Add comment about label

--- core/app/components/FormBuilder/types.ts
+++ core/app/components/FormBuilder/types.ts
@@ -27,6 +27,7 @@ export type InputElement = baseElement & {
 	type: ElementType.pubfield;
 	fieldId: PubFieldsId;
 	required: boolean | null;
+	// label is only used by elements with type: ElementType.button. Pubfield inputs put everything in config
 	label: never;
 	placeholder?: string | null;
 	help?: string | null;
