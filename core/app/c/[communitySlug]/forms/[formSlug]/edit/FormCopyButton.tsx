45f300e66dbfa2427b821294cedbd39b892a53c4
allisonking <aking@knowledgefutures.org>
2024-10-07 10:25:22 -0400

    Fix url for copying a form

--- core/app/c/[communitySlug]/forms/[formSlug]/edit/FormCopyButton.tsx
+++ core/app/c/[communitySlug]/forms/[formSlug]/edit/FormCopyButton.tsx
@@ -6,7 +6,7 @@ import { useCommunity } from "~/app/components/providers/CommunityProvider";
 
 export const FormCopyButton = ({ formSlug }: { formSlug: string }) => {
 	const community = useCommunity();
-	const link = `${window.location.origin}/c/${community.slug}/public/forms/${formSlug}`;
+	const link = `${window.location.origin}/c/${community.slug}/public/forms/${formSlug}/fill`;
 	return (
 		<CopyButton className="flex h-8 w-auto gap-1 p-3" value={link}>
 			Copy link to live form
