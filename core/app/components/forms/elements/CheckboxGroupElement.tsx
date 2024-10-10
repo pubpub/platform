de323b81185cdcbca1d12a8be744934a8cb1090a
allisonking <aking@knowledgefutures.org>
2024-10-10 15:14:48 -0400

    Handle checkbox other field

--- core/app/components/forms/elements/CheckboxGroupElement.tsx
+++ core/app/components/forms/elements/CheckboxGroupElement.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import type { ChangeEvent } from "react";
+
 import { CoreSchemaType } from "@prisma/client";
 import { Value } from "@sinclair/typebox/value";
 import { useFormContext } from "react-hook-form";
@@ -7,6 +9,7 @@ import { checkboxGroupConfigSchema } from "schemas";
 
 import { Checkbox } from "ui/checkbox";
 import { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "ui/form";
+import { Input } from "ui/input";
 
 import type { ElementProps } from "../types";
 import { useFormElementToggleContext } from "../FormElementToggleContext";
@@ -26,17 +29,32 @@ export const CheckboxGroupElement = ({ name, config, schemaName }: ElementProps)
 		<FormField
 			control={control}
 			name={name}
-			render={() => {
+			render={({ field }) => {
+				const handleOtherField = (e: ChangeEvent<HTMLInputElement>) => {
+					const configValues: (string | number)[] = config.values;
+					const checkboxValues = field.value?.filter((f: string | number) =>
+						configValues.includes(f)
+					);
+					const value = isNumeric ? e.target.valueAsNumber : e.target.value;
+					const inputIsEmpty = value === "" || (isNumeric && isNaN(value as number));
+					if (inputIsEmpty) {
+						field.onChange(checkboxValues);
+					} else {
+						field.onChange([...checkboxValues, value]);
+					}
+				};
 				return (
 					<FormItem>
-						<FormLabel>{config.label ?? name}</FormLabel>
+						<FormLabel>
+							{config.label ?? name} {`${field.value}`}
+						</FormLabel>
 						{config.values.map((v) => {
 							return (
 								<FormField
 									key={v}
 									control={control}
 									name={name}
-									render={({ field }) => {
+									render={() => {
 										return (
 											<FormItem
 												className="flex items-center gap-2 space-y-0"
@@ -68,6 +86,18 @@ export const CheckboxGroupElement = ({ name, config, schemaName }: ElementProps)
 								/>
 							);
 						})}
+						{config.includeOther ? (
+							<FormItem className="flex items-center gap-2 space-y-0">
+								<FormLabel>Other</FormLabel>
+								<FormControl>
+									<Input
+										type={isNumeric ? "number" : undefined}
+										className="h-6"
+										onChange={handleOtherField}
+									/>
+								</FormControl>
+							</FormItem>
+						) : null}
 						<FormDescription>{config.help}</FormDescription>
 						<FormMessage />
 					</FormItem>
