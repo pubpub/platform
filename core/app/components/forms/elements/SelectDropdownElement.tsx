4f4c7e011d7ac019d193145911d397deea33233e
allisonking <aking@knowledgefutures.org>
2024-10-15 15:10:37 -0400

    Handle disabled

--- core/app/components/forms/elements/SelectDropdownElement.tsx
+++ core/app/components/forms/elements/SelectDropdownElement.tsx
@@ -4,16 +4,18 @@ import { Value } from "@sinclair/typebox/value";
 import { useFormContext } from "react-hook-form";
 import { selectDropdownConfigSchema } from "schemas";
 
+import { CoreSchemaType } from "db/public";
 import { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "ui/form";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "ui/select";
 
 import type { ElementProps } from "../types";
 import { useFormElementToggleContext } from "../FormElementToggleContext";
 
-export const SelectDropdownElement = ({ name, config }: ElementProps) => {
+export const SelectDropdownElement = ({ name, config, schemaName }: ElementProps) => {
 	const { control } = useFormContext();
 	const formElementToggle = useFormElementToggleContext();
 	const isEnabled = formElementToggle.isEnabled(name);
+	const isNumeric = schemaName === CoreSchemaType.NumericArray;
 
 	Value.Default(selectDropdownConfigSchema, config);
 	if (!Value.Check(selectDropdownConfigSchema, config)) {
@@ -25,12 +27,17 @@ export const SelectDropdownElement = ({ name, config }: ElementProps) => {
 			control={control}
 			name={name}
 			render={({ field }) => {
+				const handleChange = (value: string) => {
+					field.onChange([isNumeric ? +value : value]);
+				};
 				return (
 					<FormItem>
 						<FormLabel className="flex">{config.label ?? name}</FormLabel>
 						<Select
-							onValueChange={field.onChange}
-							defaultValue={field.value}
+							onValueChange={handleChange}
+							defaultValue={
+								Array.isArray(field.value) ? `${field.value[0]}` : undefined
+							}
 							disabled={!isEnabled}
 						>
 							<FormControl>
