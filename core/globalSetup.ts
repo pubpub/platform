54076d757863a564b31c3d281bb8fc5b5c14be0a
Thomas F. K. Jorna <hello@tefkah.com>
2024-10-15 15:05:14 +0200

    feat: use different db for tests and reset test db before every run

--- core/globalSetup.ts
+++ core/globalSetup.ts
@@ -1,9 +1,30 @@
+import { spawnSync } from "child_process";
+
 import { config } from "dotenv";
 
 export const setup = async () => {
 	config({
-		path: ["./.env.local", "./.env.development", "./.env.test", "./.env.test.local"],
+		path: ["./.env.test", "./.env.test.local"],
 	});
 
-	await import("./prisma/seed");
+	if (process.env.SKIP_RESET) {
+		return;
+	}
+
+	console.log("Resetting database...");
+	const result = spawnSync(
+		"pnpm --filter core exec dotenv -e ./.env.test -e ./.env.test.local prisma migrate reset -- --preview-feature --force",
+		{
+			shell: true,
+			stdio: "inherit",
+		}
+	);
+	const { stderr, error } = result;
+
+	if (error) {
+		console.error(
+			"Something went wrong while trying to reset the database before running tests."
+		);
+		throw error;
+	}
 };
