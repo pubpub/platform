7a96902b9e4f27ccfeb799ba307bcda828030583
Thomas F. K. Jorna <hello@tefkah.com>
2024-10-21 16:20:03 +0200

    fix: revert changes to the schema package

--- packages/schemas/src/index.ts
+++ packages/schemas/src/index.ts
@@ -3,38 +3,87 @@ import type { Static } from "@sinclair/typebox";
 import { CoreSchemaType } from "db/public";
 
 import * as Schemas from "./schemas";
+import {
+	Boolean,
+	DateTime,
+	Email,
+	FileUpload,
+	MemberId,
+	Null,
+	Number,
+	NumericArray,
+	String,
+	StringArray,
+	URL,
+	Vector3,
+} from "./schemas";
 
-export function getJsonSchemaByCoreSchemaType<C extends CoreSchemaType>(
-	coreSchemaType: C
-): (typeof Schemas)[C] {
-	return Schemas[coreSchemaType];
+export function getJsonSchemaByCoreSchemaType(coreSchemaType: CoreSchemaType) {
+	switch (coreSchemaType) {
+		case CoreSchemaType.Boolean:
+			return Boolean;
+		case CoreSchemaType.DateTime:
+			return DateTime;
+		case CoreSchemaType.Email:
+			return Email;
+		case CoreSchemaType.FileUpload:
+			return FileUpload;
+		case CoreSchemaType.MemberId:
+			return MemberId;
+		case CoreSchemaType.Null:
+			return Null;
+		case CoreSchemaType.Number:
+			return Number;
+		case CoreSchemaType.NumericArray:
+			return NumericArray;
+		case CoreSchemaType.String:
+			return String;
+		case CoreSchemaType.StringArray:
+			return StringArray;
+		case CoreSchemaType.URL:
+			return URL;
+		case CoreSchemaType.Vector3:
+			return Vector3;
+		default:
+			const _exhaustiveCheck: never = coreSchemaType;
+			return _exhaustiveCheck;
+	}
 }
 
 export type JSONSchemaForCoreSchemaType<C extends CoreSchemaType> = (typeof Schemas)[C];
 
 export type InputTypeForCoreSchemaType<C extends CoreSchemaType> = Static<(typeof Schemas)[C]>;
 
-const DefaultValueSchemaTypeMap = {
-	[CoreSchemaType.Boolean]: undefined,
-	[CoreSchemaType.DateTime]: undefined,
-	[CoreSchemaType.Email]: "",
-	[CoreSchemaType.FileUpload]: undefined,
-	[CoreSchemaType.MemberId]: "",
-	[CoreSchemaType.Null]: undefined,
-	[CoreSchemaType.Number]: undefined,
-	[CoreSchemaType.NumericArray]: [],
-	[CoreSchemaType.String]: "",
-	[CoreSchemaType.StringArray]: [],
-	[CoreSchemaType.URL]: "",
-	[CoreSchemaType.Vector3]: [0, 50, 100],
-} as const satisfies {
-	[K in CoreSchemaType]: Static<(typeof Schemas)[K]> | undefined;
-};
-
-export function getDefaultValueByCoreSchemaType<C extends CoreSchemaType>(
-	coreSchemaType: C
-): (typeof DefaultValueSchemaTypeMap)[C] {
-	return DefaultValueSchemaTypeMap[coreSchemaType];
+export function getDefaultValueByCoreSchemaType(coreSchemaType: CoreSchemaType) {
+	switch (coreSchemaType) {
+		case CoreSchemaType.Boolean:
+			return undefined;
+		case CoreSchemaType.DateTime:
+			return undefined;
+		case CoreSchemaType.Email:
+			return "";
+		case CoreSchemaType.FileUpload:
+			return undefined;
+		case CoreSchemaType.MemberId:
+			return "";
+		case CoreSchemaType.Null:
+			return undefined;
+		case CoreSchemaType.Number:
+			return undefined;
+		case CoreSchemaType.NumericArray:
+			return [];
+		case CoreSchemaType.String:
+			return "";
+		case CoreSchemaType.StringArray:
+			return [];
+		case CoreSchemaType.URL:
+			return "";
+		case CoreSchemaType.Vector3:
+			return [0, 50, 100];
+		default:
+			const _exhaustiveCheck: never = coreSchemaType;
+			return _exhaustiveCheck;
+	}
 }
 
 export { zodTypeToCoreSchemaType } from "./zodTypesToCoreSchemas";
