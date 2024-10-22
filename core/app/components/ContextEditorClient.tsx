1b7d27c33c16949636d9d81779cb832fdf8af50d
allisonking <aking@knowledgefutures.org>
2024-10-22 13:58:51 -0400

    Render editor state

--- core/app/components/ContextEditorClient.tsx
+++ core/app/components/ContextEditorClient.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useCallback } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { ContextEditor } from "context-editor";
 
 import type { GetPubsResult, GetPubTypesResult } from "~/lib/server";
@@ -12,6 +12,8 @@ export const ContextEditorClient = ({
 	pubs: GetPubsResult;
 	pubTypes: GetPubTypesResult;
 }) => {
+	// TODO: should probably be of type EditorState from prosemirror-state
+	const [editorState, setEditorState] = useState<any>(null);
 	const getPubs = useCallback(
 		(filter: string) => {
 			return new Promise<any[]>((resolve, reject) => {
@@ -21,8 +23,8 @@ export const ContextEditorClient = ({
 		[pubs]
 	);
 
-	return (
-		<div className="border">
+	const memoEditor = useMemo(() => {
+		return (
 			<ContextEditor
 				pubId={pubs[0].id}
 				pubTypeId={pubTypes[0].id}
@@ -31,9 +33,18 @@ export const ContextEditorClient = ({
 				getPubById={() => {}}
 				atomRenderingComponent={() => {}}
 				onChange={(state) => {
-					console.log({ state });
+					setEditorState(state);
 				}}
 			/>
+		);
+	}, [pubs, pubTypes]);
+
+	return (
+		<div className="grid grid-cols-2">
+			<div>{memoEditor}</div>
+			<div className="overflow-auto text-xs">
+				<pre>{JSON.stringify(editorState?.doc.toJSON(), null, 2)}</pre>
+			</div>
 		</div>
 	);
 };
