f7e8efa7dc4fa105fcb9cb6262db461d1c1bb202
Allison King <aking@knowledgefutures.org>
2024-10-22 13:13:55 -0700

    Rich text pub field type (#730)
    * Add new rich text type
    
    * Configure form components
    
    * Update icon and schema a little
    
    * Add prose mirror validation, maybe
    
    * Fix validation logic
    
    * Move file to ~/lib/server
    
    * Add one test for validateFields

--- core/actions/_lib/runActionInstance.ts
+++ core/actions/_lib/runActionInstance.ts
@@ -13,10 +13,10 @@ import type { ClientException, ClientExceptionOptions } from "~/lib/serverAction
 import { db } from "~/kysely/database";
 import { getPub, getPubCached } from "~/lib/server";
 import { autoRevalidate } from "~/lib/server/cache/autoRevalidate";
+import { validatePubValues } from "../../lib/server/validateFields";
 import { getActionByName } from "../api";
 import { getActionRunByName } from "./getRuns";
 import { resolveWithPubfields } from "./resolvePubfields";
-import { validatePubValues } from "./validateFields";
 
 export type ActionInstanceRunResult = ClientException | ClientExceptionOptions | ActionSuccess;
 
