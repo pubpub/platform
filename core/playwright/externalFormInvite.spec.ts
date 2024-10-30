03503bcdb8b6d0c97c0092b3da82f98f45fbb7d6
Kalil Smith-Nuevelle <kalilsn@gmail.com>
2024-10-30 17:50:23 -0500

    Add email action form invite test

---
+++ core/playwright/externalFormInvite.spec.ts
@@ -0,0 +1,101 @@
+import type { Page } from "@playwright/test";
+
+import { expect, test } from "@playwright/test";
+
+import { Action } from "db/public";
+
+import { FormsPage } from "./fixtures/forms-page";
+import { PubDetailsPage } from "./fixtures/pub-details-page";
+import { PubsPage } from "./fixtures/pubs-page";
+import { StagesManagePage } from "./fixtures/stages-manage-page";
+import { createCommunity, inbucketClient, login } from "./helpers";
+
+const now = new Date().getTime();
+const COMMUNITY_SLUG = `playwright-test-community-${now}`;
+const FORM_SLUG = `playwright-test-form-${now}`;
+const ACTION_NAME = "Invite evaluator";
+
+test.describe.configure({ mode: "serial" });
+
+let page: Page;
+
+test.beforeAll(async ({ browser }) => {
+	page = await browser.newPage();
+	await login({ page });
+	await createCommunity({
+		page,
+		community: { name: `test community ${now}`, slug: COMMUNITY_SLUG },
+	});
+
+	const formsPage = new FormsPage(page, COMMUNITY_SLUG);
+	await formsPage.goto();
+	await formsPage.addForm("Evaluation", FORM_SLUG);
+
+	const stagesManagePage = new StagesManagePage(page, COMMUNITY_SLUG);
+	await stagesManagePage.goTo();
+	await stagesManagePage.addStage("Evaluating");
+	await stagesManagePage.addAction("Evaluating", Action.email, ACTION_NAME);
+});
+
+test.afterAll(async () => {
+	await page.close();
+});
+
+test.only("Invite a user to fill out the form", async () => {
+	const pubsPage = new PubsPage(page, COMMUNITY_SLUG);
+	await pubsPage.goTo();
+	const pubId = await pubsPage.createPub({
+		stage: "Evaluating",
+		values: { title: "The Activity of Snails" },
+	});
+	expect(pubId).toBeTruthy();
+
+	const pubDetailsPage = new PubDetailsPage(page, COMMUNITY_SLUG, pubId!);
+	await pubDetailsPage.goTo();
+
+	await page.getByRole("button", { name: "Run action", exact: true }).click();
+	await page
+		.getByRole("menu", { name: "Run action", exact: true })
+		.getByRole("button", { name: ACTION_NAME, exact: true })
+		.click();
+
+	const runActionDialog = page.getByRole("dialog", { name: ACTION_NAME, exact: true });
+	await runActionDialog.waitFor();
+
+	// Invite a new user to fill out the form
+	await runActionDialog.getByRole("combobox").fill("evaluator@example.com");
+
+	const memberDialog = runActionDialog.getByRole("listbox", { name: "Suggestions", exact: true });
+	await memberDialog
+		.getByRole("button", {
+			name: "Member not found Click to add a user to your community",
+			exact: true,
+		})
+		.click();
+
+	await memberDialog.getByLabel("First Name").fill("Anonymous");
+	await memberDialog.getByLabel("Last Name").fill("Malacologist");
+	await memberDialog
+		.getByRole("button", { name: "Submit", exact: true })
+		.click({ timeout: 15000 });
+	await runActionDialog
+		.getByRole("option", { name: "Anonymous Malacologist", exact: true })
+		.click({ timeout: 15000 });
+
+	await memberDialog.waitFor({ state: "hidden" });
+
+	await runActionDialog
+		.getByLabel("Email subject")
+		.fill("Test invitation for :RecipientFirstName");
+	await runActionDialog
+		.getByLabel("Email body")
+		.fill(`Please fill out :link[this form]{form=${FORM_SLUG}}`);
+
+	await runActionDialog.getByRole("button", { name: "Run", exact: true }).click();
+	await runActionDialog.waitFor({ state: "hidden" });
+
+	const { message } = await (await inbucketClient.getMailbox("evaluator")).getLatestMessage();
+	const url = message.body.text.match(/a href="([^"]+)"/)?.[0];
+	expect(url).toBeTruthy();
+	await page.goto(url!);
+});
