21f68da28ea42e1f884689cb29a33219cd928a56
Kalil Smith-Nuevelle <kalilsn@gmail.com>
2024-10-31 15:22:03 -0500

    Make member select form update as the email changes

--- core/app/components/MemberSelect/MemberSelectAddUserForm.tsx
+++ core/app/components/MemberSelect/MemberSelectAddUserForm.tsx
@@ -1,12 +1,13 @@
 import type { SyntheticEvent } from "react";
 
-import { useCallback, useTransition } from "react";
+import { useCallback, useEffect, useTransition } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
 import type { Communities } from "db/public";
 import { MemberRole } from "db/public";
+import { logger } from "logger";
 import { Button } from "ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "ui/card";
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "ui/form";
@@ -40,6 +41,10 @@ export const MemberSelectAddUserForm = ({ email, community }: Props) => {
 		},
 	});
 
+	useEffect(() => {
+		form.setValue("email", email);
+	}, [form, email]);
+
 	// NOTE: We run `form.handleSubmit` manually here because the UserSelect
 	// component may be used within a <form> and that breaks HTML semantics/
 	// a11y practices.
@@ -48,21 +53,30 @@ export const MemberSelectAddUserForm = ({ email, community }: Props) => {
 			e.preventDefault();
 
 			startTransition(() => {
-				return form.handleSubmit(async ({ email, firstName, lastName }) => {
-					const result = await runCreateUserWithMembership({
-						email,
-						firstName,
-						lastName,
-						role: MemberRole.contributor,
-					});
-
-					if (didSucceed(result)) {
-						toast({
-							title: "Success",
-							description: "User successfully invited",
+				return form.handleSubmit(
+					async ({ email, firstName, lastName }) => {
+						const result = await runCreateUserWithMembership({
+							email,
+							firstName,
+							lastName,
+							role: MemberRole.contributor,
 						});
+
+						if (didSucceed(result)) {
+							toast({
+								title: "Success",
+								description: "User successfully invited",
+							});
+						}
+					},
+					(errors) => {
+						logger.error({
+							msg: "user couldn't be created because of validation errors",
+							errors,
+						});
+						// TODO: we should render this error somewhere
 					}
-				})();
+				)();
 			});
 		},
 		[form, community]
