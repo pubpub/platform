eb558b50565e3f83a68023bb62d4b0285b595897
allisonking <aking@knowledgefutures.org>
2024-11-05 12:04:05 -0500

    Use email and url instead of richtext and number for pubfield titles

--- core/app/c/[communitySlug]/types/utils.ts
+++ core/app/c/[communitySlug]/types/utils.ts
@@ -3,7 +3,7 @@ import { CoreSchemaType } from "db/public";
 export const pubFieldCanBeTitle = (pubField: { schemaName: CoreSchemaType | null }) => {
 	return (
 		pubField.schemaName === CoreSchemaType.String ||
-		pubField.schemaName === CoreSchemaType.RichText ||
-		pubField.schemaName === CoreSchemaType.Number
+		pubField.schemaName === CoreSchemaType.Email ||
+		pubField.schemaName === CoreSchemaType.URL
 	);
 };
