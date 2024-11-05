22321099aa67a9b69eef4acb2b503961d58c0cf9
allisonking <aking@knowledgefutures.org>
2024-11-05 15:05:10 -0500

    Install prosemirror adapter

--- core/app/components/ContextEditor/AtomRenderer.tsx
+++ core/app/components/ContextEditor/AtomRenderer.tsx
@@ -1,5 +1,14 @@
+import { useState } from "react";
+import { useNodeViewContext } from "@prosemirror-adapter/react";
 import { Node } from "prosemirror-model";
 
 export const ContextAtom = ({ nodeProp }: { nodeProp: Node }) => {
+	const { contentRef, node, selected } = useNodeViewContext();
+	const [activeData, setActiveData] = useState("");
+	const activeNode = nodeProp || node;
+	// console.log({ activeNode });
+	if (!activeNode) {
+		return null;
+	}
 	return "TODO";
 };
