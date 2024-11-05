851d44127391ab8b65b475021e2913f24abd2971
allisonking <aking@knowledgefutures.org>
2024-11-05 12:03:16 -0500

    Truncate migration for new decision where isTitle is not required

--- core/prisma/migrations/20241030175616_add_is_title_to_pubfield_pubtype_join_table/migration.sql
+++ core/prisma/migrations/20241030175616_add_is_title_to_pubfield_pubtype_join_table/migration.sql
@@ -8,11 +8,7 @@ ALTER INDEX "_PubFieldToPubType_AB_unique" RENAME TO "_PubFieldToPubType_A_B_key
 -- This is done manually because prisma doesn't currently support defining unique constraints this way in the schema file
 CREATE UNIQUE INDEX unique_pubType_isTitle on "_PubFieldToPubType" ("B") where "isTitle" is true;
 
-
--- Migrate so that all pub types will have some sort of title field
-BEGIN;
-
--- First try to match on if there's an existing 'title' or 'name' field and set that field to isTitle=true
+-- Try to match on if there's an existing 'title' or 'name' field and set that field to isTitle=true
 WITH FirstMatchingPubField AS (
     SELECT
       "_PubFieldToPubType"."A" as "pubfield",
@@ -23,7 +19,7 @@ WITH FirstMatchingPubField AS (
       "_PubFieldToPubType"
       JOIN pub_fields ON "_PubFieldToPubType"."A" = pub_fields.id 
     WHERE
-      (pub_fields."schemaName" = 'String' OR pub_fields."schemaName" = 'RichText' OR pub_fields."schemaName" = 'Number')
+      (pub_fields."schemaName" = 'String' OR pub_fields."schemaName" = 'Email' OR pub_fields."schemaName" = 'URL')
       AND (
           pub_fields.name ILIKE '%title%'
           OR pub_fields.name ILIKE '%name%'
@@ -36,62 +32,3 @@ WHERE
   "_PubFieldToPubType"."A" = FirstMatchingPubField.pubfield
   AND "_PubFieldToPubType"."B" = FirstMatchingPubField.pubtype 
   AND FirstMatchingPubField.row_num = 1;
-
--- If there's no match, just set the first field that is string, rich text, or number
-WITH FirstAvailablePubField AS (
-    SELECT
-      "_PubFieldToPubType"."A" as "pubfield",
-      "_PubFieldToPubType"."B" as "pubtype",
-      pub_fields."slug",
-      pub_types.name,
-      ROW_NUMBER() OVER (PARTITION BY "_PubFieldToPubType"."B" ORDER BY pub_fields."createdAt" ASC) AS row_num
-    FROM
-      "_PubFieldToPubType"
-      JOIN pub_fields ON "_PubFieldToPubType"."A" = pub_fields.id 
-      JOIN pub_types ON "_PubFieldToPubType"."B" = pub_types.id
-    WHERE
-      (pub_fields."schemaName" = 'String' OR pub_fields."schemaName" = 'RichText' OR pub_fields."schemaName" = 'Number')
-      AND NOT EXISTS (
-        SELECT 1
-        FROM "_PubFieldToPubType" AS "sub"
-        WHERE "sub"."B" = "_PubFieldToPubType"."B"
-        AND "sub"."isTitle" = true
-      )
-)
-UPDATE "_PubFieldToPubType"
-SET "isTitle" = true 
-FROM FirstAvailablePubField
-WHERE 
-  "_PubFieldToPubType"."A" = FirstAvailablePubField.pubfield
-  AND "_PubFieldToPubType"."B" = FirstAvailablePubField.pubtype 
-  AND FirstAvailablePubField.row_num = 1;
-
-INSERT INTO "_PubFieldToPubType" ("A", "B", "isTitle")
-SELECT
-  "newField"."id" AS "A",
-  pub_types.id AS "B",
-  true AS isTitle
-FROM
-  pub_types
-  LEFT JOIN LATERAL (
-    SELECT 
-      id, slug 
-    FROM pub_fields 
-    WHERE (pub_fields."schemaName" = 'String' OR pub_fields."schemaName" = 'RichText' OR pub_fields."schemaName" = 'Number')
-      AND (
-          pub_fields.name ILIKE '%title%'
-          OR pub_fields.name ILIKE '%name%'
-      )
-    AND pub_fields."communityId" = pub_types."communityId"
-    LIMIT 1
-  ) AS "newField" ON true
-WHERE
-  NOT EXISTS (
-    SELECT 1
-    FROM "_PubFieldToPubType"
-    WHERE "_PubFieldToPubType"."B" = pub_types.id
-    AND "_PubFieldToPubType"."isTitle" = true
-  )
-  AND "newField".id IS NOT NULL;
-
-COMMIT;
