5836d6fec7076fbc1af736bc845bc0981194fbde
Kalil Smith-Nuevelle <kalilsn@gmail.com>
2024-11-05 23:51:47 -0600

    Specify columns to select in migration

--- core/prisma/migrations/20241105184158_copy_membership_data/migration.sql
+++ core/prisma/migrations/20241105184158_copy_membership_data/migration.sql
@@ -1,16 +1,14 @@
--- This is an empty migration.
-
 BEGIN;
 
-INSERT INTO "community_memberships" 
-    SELECT * FROM "members" 
+INSERT INTO "community_memberships"
+    SELECT "id", "role", "communityId", "userId", "createdAt", "updatedAt" FROM "members"
     ON CONFLICT ("id") DO NOTHING;
 
 INSERT INTO "form_memberships" 
     SELECT "formId", "userId" FROM "form_to_permissions"
     JOIN "permissions"
         ON "permissions"."id" = "form_to_permissions"."permissionId"
-    JOIN "members" 
+    JOIN "members"
         ON "members"."id" = "permissions"."memberId";
 
 COMMIT;
