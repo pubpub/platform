d45a9bd53a42dda5696d8ce247e4d80c1e292e84
Kalil Smith-Nuevelle <kalilsn@gmail.com>
2024-11-10 16:20:40 -0600

    Add default generateIdentifierType implementation

--- packages/db/.kanelrc.js
+++ packages/db/.kanelrc.js
@@ -8,6 +8,12 @@ const {
 	kanelDatabaseDefaultExportFixPreRenderHook,
 } = require("./src/kanel/kanel-database-default-export-fix-hook.cjs");
 const { cleanupEnumComments } = require("./src/kanel/kanel-cleanup-enum-comments.cjs");
+const { escapeIdentifier } = require("kanel");
+const { resolveType } = require("kanel");
+const { escapeString } = require("kanel");
+const { recase } = require("@kristiandupont/recase");
+
+const toPascalCase = recase(null, "pascal");
 
 /** @type {import('kanel').Config} */
 module.exports = {
@@ -16,6 +22,31 @@ module.exports = {
 
 	preDeleteOutputFolder: false,
 	enumStyle: "enum",
+	generateIdentifierType: (column, details, config) => {
+		const name = escapeIdentifier(toPascalCase(details.name) + toPascalCase(column.name));
+		const innerType = resolveType(column, details, {
+			...config,
+			// Explicitly disable identifier resolution so we get the actual inner type here
+			generateIdentifierType: undefined,
+		});
+		const imports = [];
+
+		let type = innerType;
+		if (typeof innerType === "object") {
+			// Handle non-primitives
+			type = innerType.name;
+			imports.push(...innerType.typeImports);
+		}
+
+		return {
+			declarationType: "typeDeclaration",
+			name,
+			exportAs: "named",
+			typeDefinition: [`${type} & { __brand: '${escapeString(name)}' }`],
+			typeImports: imports,
+			comment: [`Identifier type for ${details.schemaName}.${details.name}`],
+		};
+	},
 	preRenderHooks: [
 		makeKyselyHook(),
 		generateZodSchemas,
