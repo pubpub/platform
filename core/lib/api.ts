79bb482a0564523bb61f9a7c03231b9c74947160
Kalil Smith-Nuevelle <kalilsn@gmail.com>
2024-11-25 16:21:09 -0600

    Handle api client base url in a way that doesn't prevent building

--- core/lib/api.ts
+++ core/lib/api.ts
@@ -2,6 +2,8 @@ import { initTsrReactQuery } from "@ts-rest/react-query/v5";
 
 import { siteApi } from "contracts";
 
+import { env } from "./env/env.mjs";
+
 export const client = initTsrReactQuery(siteApi, {
-	baseUrl: window.location.origin,
+	baseUrl: typeof window === "undefined" ? env.PUBPUB_URL : window.location.origin,
 });
