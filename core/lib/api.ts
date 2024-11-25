4e3dbc719d80f9c002a0d295f2100ca66b346b18
Kalil Smith-Nuevelle <kalilsn@gmail.com>
2024-11-25 09:24:14 -0600

    Use window.location.origin instead of frontend env var

--- core/lib/api.ts
+++ core/lib/api.ts
@@ -2,8 +2,6 @@ import { initTsrReactQuery } from "@ts-rest/react-query/v5";
 
 import { siteApi } from "contracts";
 
-import { env } from "./env/env.mjs";
-
 export const client = initTsrReactQuery(siteApi, {
-	baseUrl: env.NEXT_PUBLIC_PUBPUB_URL,
+	baseUrl: window.location.origin,
 });
