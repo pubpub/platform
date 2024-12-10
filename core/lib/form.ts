51d1a66b5485c8fe6eef951050872ba3db9e1c44
Kalil Smith-Nuevelle <kalilsn@gmail.com>
2024-12-10 15:15:47 -0500

    Warn users about the default form when they modify a pubtype (#831)
    * Warn users about the default form when they add or remove fields from a type
    
    * Remove pub type deletion

---
+++ core/lib/form.ts
@@ -0,0 +1,5 @@
+import { slugifyString } from "./string";
+
+export const defaultFormName = (pubTypeName: string) => `${pubTypeName} Editor (Default)`;
+export const defaultFormSlug = (pubTypeName: string) =>
+	`${slugifyString(pubTypeName)}-default-editor`;
