3158181f22ef4e698a080840acb8f3811c82c6ce
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2024-12-11 18:44:24 -0500

    fix broken deposits

--- core/actions/datacite/run.ts
+++ core/actions/datacite/run.ts
@@ -16,17 +16,18 @@ import { defineRun } from "../types";
 
 type DataciteDoiPayload = components["schemas"]["Doi"];
 
-const PUBLISHED_AT_FIELD = "arcadia:publication-year";
-const CREATOR_FIELD = "arcadia:author";
-const AUTHOR_FIRST_NAME = "arcadia:first-name";
-const AUTHOR_LAST_NAME = "arcadia:last-name";
+const TEMP_TITLE_FIELD_SLUG = "croccroc:title";
 
-const makeDataciteCreatorFromAuthorPub = (pub: ProcessedPub) => {
+const makeDataciteCreatorFromAuthorPub = (
+	pub: ProcessedPub,
+	firstNameFieldSlug: string,
+	lastNameFieldSlug: string
+) => {
 	const firstName = expect(
-		pub.values.find((value) => value.fieldSlug === AUTHOR_FIRST_NAME)?.value
+		pub.values.find((value) => value.fieldSlug === firstNameFieldSlug)?.value
 	);
 	const lastName = expect(
-		pub.values.find((value) => value.fieldSlug === AUTHOR_LAST_NAME)?.value
+		pub.values.find((value) => value.fieldSlug === lastNameFieldSlug)?.value
 	);
 	return {
 		name: `${firstName} ${lastName}`,
@@ -38,35 +39,54 @@ const makeDataciteCreatorFromAuthorPub = (pub: ProcessedPub) => {
 const encodeDataciteCredentials = (username: string, password: string) =>
 	Buffer.from(`${username}:${password}`).toString("base64");
 
+// TODO: Use existing types/make this better!
+type Schema = z.infer<(typeof action)["config"]["schema"]>;
+type Config = Schema & { pubFields: { [K in keyof Schema]?: string[] } };
+
 const createDataciteDeposit = async (
 	pub: ActionPub<ActionPubType>,
-	config: z.infer<(typeof action)["config"]["schema"]>
+	config: Config
 ): Promise<DataciteDoiPayload> => {
 	const { values } = await getPubsWithRelatedValuesAndChildren({
 		pubId: pub.id as PubsId,
 		communityId: pub.communityId,
 	});
+
+	// TODO: error messages
+	const urlFieldSlug = expect(config.pubFields.url?.[0]);
+	const authorFieldSlug = expect(config.pubFields.author?.[0]);
+	const authorFirstNameFieldSlug = expect(config.pubFields.authorFirstName?.[0]);
+	const authorLastNameFieldSlug = expect(config.pubFields.authorLastName?.[0]);
+	const publicationDateFieldSlug = expect(config.pubFields.publicationDate?.[0]);
+
 	const relatedPubs = values.filter((v) => v.relatedPub != null);
 	const relatedAuthorPubs = relatedPubs
-		.filter((v) => v.fieldSlug === CREATOR_FIELD)
+		.filter((v) => v.fieldSlug === authorFieldSlug)
 		.map((v) => v.relatedPub!);
 
-	const creators = relatedAuthorPubs.map(makeDataciteCreatorFromAuthorPub);
+	const creators = relatedAuthorPubs.map((pub) =>
+		makeDataciteCreatorFromAuthorPub(pub, authorFirstNameFieldSlug, authorLastNameFieldSlug)
+	);
 
-	const publishedAt = pub.values[PUBLISHED_AT_FIELD];
-	assert(typeof publishedAt === "string");
+	const title = pub.values[TEMP_TITLE_FIELD_SLUG];
+	assert(typeof title === "string");
 
-	const publicationYear = new Date(publishedAt).getFullYear();
+	const url = pub.values[urlFieldSlug];
+	assert(typeof url === "string");
+
+	const publicationDate = pub.values[publicationDateFieldSlug];
+	assert(typeof publicationDate === "string");
+
+	const publicationYear = new Date(publicationDate).getFullYear();
 
 	return {
 		data: {
 			type: "dois",
 			attributes: {
 				doi: config.doi,
-				event: "publish",
+				titles: [{ title }],
 				creators,
 				publisher: config.publisher,
-				// TODO:
 				publicationYear,
 				dates: [
 					{
@@ -74,27 +94,52 @@ const createDataciteDeposit = async (
 						dateType: "Created",
 					},
 				],
+				types: {
+					resourceTypeGeneral: "Preprint",
+				},
+				url,
 			},
 		},
 	};
 };
 
-const createPubDeposit = async (payload: DataciteDoiPayload) => {
-	const response = await fetch("https://api.test.datacite.org/dois", {
-		method: "POST",
+const checkDoi = async (doi: string) => {
+	const response = await fetch(`https://api.test.datacite.org/dois/${doi}`, {
+		method: "GET",
 		headers: {
-			"Content-Type": "application/vnd.api+json",
+			Accept: "application/vnd.api+json",
 			Authorization:
 				"Basic " +
 				encodeDataciteCredentials(
 					String(env.DATACITE_REPOSITORY_ID),
 					String(env.DATACITE_PASSWORD)
 				),
+			"Content-Type": "application/json",
+		},
+	});
+
+	return response.ok;
+};
+
+const createPubDeposit = async (payload: DataciteDoiPayload) => {
+	console.log("CREATE!");
+	const response = await fetch("https://api.test.datacite.org/dois", {
+		method: "POST",
+		headers: {
+			Accept: "application/vnd.api+json",
+			Authorization:
+				"Basic " +
+				encodeDataciteCredentials(
+					String(env.DATACITE_REPOSITORY_ID),
+					String(env.DATACITE_PASSWORD)
+				),
+			"Content-Type": "application/json",
 		},
 		body: JSON.stringify(payload),
 	});
 
 	if (!response.ok) {
+		console.log(await response.json());
 		return {
 			title: "Failed to create DOI",
 			error: "An error occurred while depositing the pub to DataCite.",
@@ -103,17 +148,18 @@ const createPubDeposit = async (payload: DataciteDoiPayload) => {
 };
 
 const updatePubDeposit = async (payload: DataciteDoiPayload) => {
-	const doi = expect(typeof payload?.data?.attributes?.doi);
+	const doi = expect(payload?.data?.attributes?.doi);
 	const response = await fetch(`https://api.test.datacite.org/dois/${doi}`, {
 		method: "PUT",
 		headers: {
-			"Content-Type": "application/vnd.api+json",
+			Accept: "application/vnd.api+json",
 			Authorization:
 				"Basic " +
 				encodeDataciteCredentials(
 					String(env.DATACITE_REPOSITORY_ID),
 					String(env.DATACITE_PASSWORD)
 				),
+			"Content-Type": "application/json",
 		},
 		body: JSON.stringify(payload),
 	});
@@ -126,47 +172,17 @@ const updatePubDeposit = async (payload: DataciteDoiPayload) => {
 	}
 };
 
-const updatePubDoi = async (pub: ActionPub<ActionPubType>, doi: string, doiFieldSlug: string) =>
-	updatePub({
-		pubId: pub.id as PubsId,
-		communityId: pub.communityId,
-		pubValues: {
-			[doiFieldSlug]: doi,
-		},
-		continueOnValidationError: false,
-	});
-
 export const run = defineRun<typeof action>(async ({ pub, config, args }) => {
-	const payload = await createDataciteDeposit(pub, { ...config, ...args });
-	const doiFieldSlug = expect(
-		config.pubFields.doi?.[0],
-		"The DataCite action's DOI setting must be configured with a pub field."
-	);
-	const doi = expect(payload?.data?.attributes?.doi);
+	const depositConfig = { ...config, ...args };
+	const depositPayload = await createDataciteDeposit(pub, depositConfig);
+	const depositResult = (await checkDoi(depositConfig.doi))
+		? await updatePubDeposit(depositPayload)
+		: await createPubDeposit(depositPayload);
 
-	if (doi === undefined) {
-		const result = await createPubDeposit(payload);
-		if (isClientExceptionOptions(result)) {
-			return result;
-		}
-		try {
-			await updatePubDoi(pub, doi, doiFieldSlug);
-		} catch (error) {
-			return {
-				title: "Failed to update DOI",
-				error: "The pub was deposited in DataCite successfully, but we failed to update the DOI in PubPub.",
-				cause: error,
+	return isClientExceptionOptions(depositResult)
+		? depositResult
+		: {
+				data: {},
+				success: true,
 			};
-		}
-	} else {
-		const result = await updatePubDeposit(payload);
-		if (isClientExceptionOptions(result)) {
-			return result;
-		}
-	}
-
-	return {
-		data: {},
-		success: true,
-	};
 });
