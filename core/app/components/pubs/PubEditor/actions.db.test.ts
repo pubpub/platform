a224dceab6a35cf29fa6475ec2c818366e399718
allisonking <aking@knowledgefutures.org>
2024-12-11 13:25:46 -0500

    Parameterize test

--- core/app/components/pubs/PubEditor/actions.db.test.ts
+++ core/app/components/pubs/PubEditor/actions.db.test.ts
@@ -1,75 +1,44 @@
-import { describe, expect, it } from "vitest";
+import { describe, expect, test } from "vitest";
 
+import type { UsersId } from "db/public";
 import { CoreSchemaType, MemberRole } from "db/public";
 
-import type { ClientException } from "~/lib/serverActions";
 import { mockServerCode } from "~/lib/__tests__/utils";
-import { isClientException } from "~/lib/serverActions";
 
 const { createForEachMockedTransaction, getLoginData } = await mockServerCode();
 
 const { getTrx } = createForEachMockedTransaction();
 
 describe("createPubRecursive", () => {
-	it("Needs a logged in user", async () => {
-		const trx = getTrx();
-		getLoginData.mockImplementation(() => {
-			return undefined;
-		});
-		const { seedCommunity } = await import("~/prisma/seed/seedCommunity");
-		const { community, pubTypes } = await seedCommunity({
-			community: {
-				name: "test",
-				slug: "test-server-pub",
+	test.each([
+		{
+			name: "Needs a logged in user",
+			loginUser: undefined,
+			userRole: MemberRole.admin,
+			expected: { error: "Not logged in" },
+		},
+		{
+			name: "Needs an authorized user",
+			loginUser: { id: crypto.randomUUID() as UsersId },
+			userRole: MemberRole.contributor,
+			expected: { error: "You are not authorized to perform this action" },
+		},
+		{
+			name: "Can create if all permissions are satisfied",
+			loginUser: { id: crypto.randomUUID() as UsersId },
+			userRole: MemberRole.admin,
+			expected: {
+				report: "Successfully created a new Pub",
+				success: true,
 			},
-			pubFields: {
-				Title: { schemaName: CoreSchemaType.String },
-				Description: { schemaName: CoreSchemaType.String },
-			},
-			pubTypes: {
-				"Minimal Pub": {
-					Title: { isTitle: true },
-				},
-			},
-			stages: {
-				"Stage 1": {},
-			},
-			pubs: [
-				{
-					pubType: "Minimal Pub",
-					values: {
-						Title: "Some title",
-					},
-					stage: "Stage 1",
-				},
-			],
-			users: {},
-		});
-
-		const { createPubRecursive } = await import("./actions");
-
-		const result = await createPubRecursive({
-			communityId: community.id,
-			body: {
-				pubTypeId: pubTypes["Minimal Pub"].id,
-				values: {
-					[`${community.slug}:title`]: "test title",
-				},
-			},
-			trx,
-		});
-
-		expect(isClientException(result)).toBeTruthy();
-		expect((result as ClientException).error).toEqual("Not logged in");
-	});
-
-	it("Needs an authorized user", async () => {
+		},
+	])("$name", async ({ loginUser, userRole, expected }) => {
 		const trx = getTrx();
 		const { seedCommunity } = await import("~/prisma/seed/seedCommunity");
 		const { community, pubTypes, users } = await seedCommunity({
 			community: {
 				name: "test",
-				slug: "test-server-pub",
+				slug: "test-actions-create-pub",
 			},
 			pubFields: {
 				Title: { schemaName: CoreSchemaType.String },
@@ -94,15 +63,16 @@ describe("createPubRecursive", () => {
 			],
 			users: {
 				john: {
+					id: loginUser ? loginUser.id : undefined,
 					firstName: "John",
-					role: MemberRole.contributor,
+					role: userRole,
 					password: "john-password",
 					email: "john@example.com",
 				},
 			},
 		});
 		getLoginData.mockImplementation(() => {
-			return { user: { id: users.john.id } };
+			return { user: loginUser ? { id: users.john.id } : undefined };
 		});
 
 		const { createPubRecursive } = await import("./actions");
@@ -117,71 +87,6 @@ describe("createPubRecursive", () => {
 			},
 			trx,
 		});
-
-		expect(isClientException(result)).toBeTruthy();
-		expect((result as ClientException).error).toEqual(
-			"You are not authorized to perform this action"
-		);
-	});
-
-	it("Can create if all permissions are satisfied", async () => {
-		const trx = getTrx();
-		const { seedCommunity } = await import("~/prisma/seed/seedCommunity");
-		const { community, pubTypes, users } = await seedCommunity({
-			community: {
-				name: "test",
-				slug: "test-server-pub",
-			},
-			pubFields: {
-				Title: { schemaName: CoreSchemaType.String },
-				Description: { schemaName: CoreSchemaType.String },
-			},
-			pubTypes: {
-				"Minimal Pub": {
-					Title: { isTitle: true },
-				},
-			},
-			stages: {
-				"Stage 1": {},
-			},
-			pubs: [
-				{
-					pubType: "Minimal Pub",
-					values: {
-						Title: "Some title",
-					},
-					stage: "Stage 1",
-				},
-			],
-			users: {
-				john: {
-					firstName: "John",
-					role: MemberRole.admin,
-					password: "john-password",
-					email: "john@example.com",
-				},
-			},
-		});
-		getLoginData.mockImplementation(() => {
-			return { user: { id: users.john.id } };
-		});
-
-		const { createPubRecursive } = await import("./actions");
-
-		const result = await createPubRecursive({
-			communityId: community.id,
-			body: {
-				pubTypeId: pubTypes["Minimal Pub"].id,
-				values: {
-					[`${community.slug}:title`]: "test title",
-				},
-			},
-			trx,
-		});
-
-		expect(result).toMatchObject({
-			report: "Successfully created a new Pub",
-			success: true,
-		});
+		expect(result).toMatchObject(expected);
 	});
 });
