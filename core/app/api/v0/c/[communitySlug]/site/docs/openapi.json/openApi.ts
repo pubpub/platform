a01054a8214801603997693e8a3c08b911e526b9
Thomas F. K. Jorna <hello@tefkah.com>
2024-12-18 14:28:27 +0100

    fix: show types for query params

--- core/app/api/v0/c/[communitySlug]/site/docs/openapi.json/openApi.ts
+++ core/app/api/v0/c/[communitySlug]/site/docs/openapi.json/openApi.ts
@@ -31,40 +31,32 @@ export const createOpenApiDocument = (communitySlug?: string): OpenAPIObject => 
 		});
 	}
 
-	return generateOpenApi(
-		siteApi,
-		{
-			openapi: "3.0.0",
-			info: {
-				title: "Pub Pub Integrations API",
-				summary: "Endpoints for PubPub integrations",
-				description:
-					"The endpoints here can be used by integrations to query and update pub fields defined by their manifest",
-				termsOfService: "https://example.com/terms/",
-				contact: {
-					name: "PubPub",
-					url: "https://help.pubpub.org",
-					email: "hello@pubpub.org",
-				},
-				license: {
-					name: "Licence Placeholder",
-					url: "https://opensource.org/licenses/",
-				},
-				version: "0.1",
+	return generateOpenApi(siteApi, {
+		openapi: "3.0.0",
+		info: {
+			title: "PubPub Site building API",
+			summary: "Endpoints for PubPub",
+			description: "",
+			contact: {
+				name: "PubPub",
+				url: "https://help.pubpub.org",
+				email: "hello@pubpub.org",
 			},
-			servers: [
-				{
-					url: "http://localhost:3000/",
-					description: "The development API server",
-				},
-				{
-					url: "https://pubpub.org/",
-					description: "The production API server",
-				},
-			],
+			license: {
+				name: "GPL v2.0+",
+				url: "https://raw.githubusercontent.com/pubpub/platform/refs/heads/main/LICENSE",
+			},
+			version: "0.0",
 		},
-		{
-			jsonQuery: true,
-		}
-	);
+		servers: [
+			{
+				url: "http://localhost:3000/",
+				description: "The development API server",
+			},
+			{
+				url: "https://app.pubpub.org/",
+				description: "The production API server",
+			},
+		],
+	});
 };
