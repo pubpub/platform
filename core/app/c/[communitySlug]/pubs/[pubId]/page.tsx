e37cd7e17829991005f475bc2f3be1f3954fb97d
Allison King <aking@knowledgefutures.org>
2024-12-18 07:59:15 -0800

    Render URLs as links (#848)
    * Render URLs as links
    
    * Underline the link
    
    Co-authored-by: Thomas F. K. Jorna <hello@tefkah.com>
    
    ---------
    
    Co-authored-by: Thomas F. K. Jorna <hello@tefkah.com>

--- core/app/c/[communitySlug]/pubs/[pubId]/page.tsx
+++ core/app/c/[communitySlug]/pubs/[pubId]/page.tsx
@@ -168,6 +168,7 @@ export default async function Page({
 										{renderPubValue({
 											fieldName: value.fieldName,
 											value: value.value as JsonValue,
+											schemaName: value.schemaName,
 										})}
 									</div>
 								</div>
