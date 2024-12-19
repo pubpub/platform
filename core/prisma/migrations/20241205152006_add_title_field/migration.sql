a57197798f56fc5af66b6181af8fe6f9a2f803ca
Thomas F. K. Jorna <hello@tefkah.com>
2024-12-19 14:18:02 +0100

    fix: create a new migration instead of updating old one

--- core/prisma/migrations/20241205152006_add_title_field/migration.sql
+++ core/prisma/migrations/20241205152006_add_title_field/migration.sql
@@ -58,13 +58,11 @@ BEGIN
     SET
         "updatedAt" = CURRENT_TIMESTAMP,
         "title" = CASE 
-        WHEN tmp."pubId" IS NULL THEN
-            pubs."title"
-        WHEN TG_OP = 'DELETE' OR tmp."value" IS NULL THEN
-            NULL
-        ELSE
-            tmp."value"
-        END
+            WHEN TG_OP = 'DELETE' OR tmp."value" IS NULL THEN
+                NULL
+            ELSE
+                tmp."value"
+            END
     FROM ( SELECT DISTINCT
             "pubId"
         FROM
