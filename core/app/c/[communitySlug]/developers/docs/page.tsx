8a5d9d00fdc0c1fd7ca2aab257d0096b893133cf
Thomas F. K. Jorna <hello@tefkah.com>
2024-12-20 14:36:35 +0100

    fix: secure docs page

--- core/app/c/[communitySlug]/developers/docs/page.tsx
+++ core/app/c/[communitySlug]/developers/docs/page.tsx
@@ -4,6 +4,10 @@ import Script from "next/script";
 
 import "./stoplight.styles.css";
 
+import { redirect } from "next/navigation";
+
+import { getPageLoginData } from "~/lib/authentication/loginData";
+import { isCommunityAdmin } from "~/lib/authentication/roles";
 import { env } from "~/lib/env/env.mjs";
 
 export const dynamic = "force-dynamic";
@@ -19,22 +23,29 @@ export default async function IndexPage({
 }: {
 	params: { communitySlug: string };
 }) {
+	const { user } = await getPageLoginData();
+
+	// TODO: add capability for this
+	if (!isCommunityAdmin(user, { slug: communitySlug })) {
+		return redirect(`/c/${communitySlug}/unauthorized`);
+	}
+
 	return (
-		<>
+		<div className="absolute inset-0 min-h-screen">
 			<Script src="https://unpkg.com/@stoplight/elements@8.5.2/web-components.min.js" />
-			<section className="container">
-				<div
-					dangerouslySetInnerHTML={{
-						__html: `<elements-api
-						apiDescriptionUrl="/api/v0/c/${communitySlug}/site/docs/openapi.json"
+			<div
+				className="h-full"
+				dangerouslySetInnerHTML={{
+					__html: `<elements-api
+						style="min-height: 100vh;"
+						apiDescriptionUrl="/c/${communitySlug}/developers/docs/openapi.json"
 						router="hash"
 						logo="/logos/icon.svg"
-						layout="responsive"
+						layout="sidebar"
 						${env.NODE_ENV === "production" ? 'hideTryItPanel="true"' : ""}
 								  />`,
-					}}
-				/>
-			</section>
-		</>
+				}}
+			/>
+		</div>
 	);
 }
