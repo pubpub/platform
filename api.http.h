e55c7304b5275716da40e8c4770811e06355e821
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2024-12-20 09:14:51 -0500

    use string or date as publication date (#863)

---
+++ api.http.h
@@ -0,0 +1,4 @@
+GET https://app.pubpub.org/api/v0/c/arcadia/site/pubs
+Authorization: Bearer cc901fa7-8881-43de-b72e-e08afd4ab23f.poMh0j16v_4ZXrcV3AL3hQ
+Prefer: "return=representation"
+Accept: "*/*"
