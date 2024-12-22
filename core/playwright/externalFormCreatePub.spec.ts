e75feedebb34d6e8830be07b6681bec477787f95
Kalil Smith-Nuevelle <kalilsn@gmail.com>
2024-12-21 23:18:24 -0600

    Use a different string in test

--- core/playwright/externalFormCreatePub.spec.ts
+++ core/playwright/externalFormCreatePub.spec.ts
@@ -170,7 +170,7 @@ test.describe("Multivalue inputs", () => {
 		await page.goto(`/c/${COMMUNITY_SLUG}/pubs`);
 		await page.getByRole("link", { name: title }).click();
 		// Make sure pub details page has loaded before making assertions
-		await page.getByText("Actions").waitFor();
+		await page.getByText("Assignee").waitFor();
 		await expect(page.getByText(numberElement.name)).toHaveCount(1);
 		await expect(page.getByTestId(`${numberElement.name}-value`)).toHaveText("0");
 		await expect(page.getByText(animalElement.name)).toHaveCount(1);
