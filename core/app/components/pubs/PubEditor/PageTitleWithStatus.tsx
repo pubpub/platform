d692f2327689c1e763d29b04e18ee4e3b9365d9c
Thomas F. K. Jorna <hello@tefkah.com>
2025-01-10 18:57:40 +0100

    fix: remove test errors

--- core/app/components/pubs/PubEditor/PageTitleWithStatus.tsx
+++ core/app/components/pubs/PubEditor/PageTitleWithStatus.tsx
@@ -3,7 +3,6 @@
 import { useSaveStatus } from "~/app/components/pubs/PubEditor/SaveStatus";
 
 export const PageTitleWithStatus = ({ title }: { title: string }) => {
-	throw new Error("Test error for testing sourcemaps, do not be alarmed.");
 	const status = useSaveStatus({ defaultMessage: "Form will save when you click save" });
 	return (
 		<div className="flex flex-col items-center">
