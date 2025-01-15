17e158ddac41e4667599b7522ebe12bce1514e0a
Thomas F. K. Jorna <hello@tefkah.com>
2025-01-15 19:00:54 +0100

    fix: ignore eslint warning

--- packages/ui/src/sidebar.tsx
+++ packages/ui/src/sidebar.tsx
@@ -83,6 +83,7 @@ const SidebarProvider = React.forwardRef<
 				}
 
 				// This sets the cookie to keep the sidebar state.
+				// eslint-disable-next-line react-compiler/react-compiler
 				document.cookie = `${SIDEBAR_COOKIE_NAME}=${openState}; path=/; max-age=${SIDEBAR_COOKIE_MAX_AGE}`;
 			},
 			[setOpenProp, open]
