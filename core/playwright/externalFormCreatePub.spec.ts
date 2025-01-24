ef7321f646c8433d4e6eb7b09780192faf011241
allisonking <aking@knowledgefutures.org>
2025-01-23 19:24:15 -0500

    Try increasing timeout

--- core/playwright/externalFormCreatePub.spec.ts
+++ core/playwright/externalFormCreatePub.spec.ts
@@ -234,6 +234,7 @@ test.describe("Rich text editor", () => {
 
 test.describe("Related pubs", () => {
 	test("Can add related pubs", async () => {
+		test.setTimeout(60_000);
 		// Create a related pub we can link to
 		const relatedPubTitle = "related pub";
 		const pubsPage = new PubsPage(page, COMMUNITY_SLUG);
