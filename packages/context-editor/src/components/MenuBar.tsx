55dac64e3769410f72cab368e27e336f570218e1
allisonking <aking@knowledgefutures.org>
2025-02-25 14:54:07 -0500

    Fix label of selected element

--- packages/context-editor/src/components/MenuBar.tsx
+++ packages/context-editor/src/components/MenuBar.tsx
@@ -1,6 +1,6 @@
 import type { ReactNode } from "react";
 
-import React from "react";
+import React, { useState } from "react";
 import { usePluginViewContext } from "@prosemirror-adapter/react";
 
 import { Button } from "ui/button";
@@ -20,19 +20,20 @@ import {
 import { emToggle, strongToggle } from "../commands/marks";
 
 type MenuItem = {
-	name: string;
+	key: string;
+	name?: string;
 	icon: ReactNode;
 	command: CommandSpec;
 };
 
 const menuItems: MenuItem[] = [
 	{
-		name: "strong",
+		key: "strong",
 		icon: "B",
 		command: strongToggle,
 	},
 	{
-		name: "em",
+		key: "em",
 		icon: <span className="italic">I</span>,
 		command: emToggle,
 	},
@@ -40,37 +41,44 @@ const menuItems: MenuItem[] = [
 
 const paragraphTypeItems: MenuItem[] = [
 	{
-		name: "paragraph",
+		key: "paragraph",
+		name: "Paragraph",
 		icon: "Paragraph",
 		command: paragraphToggle,
 	},
 	{
-		name: "h1",
+		key: "h1",
+		name: "Heading 1",
 		icon: <span className="text-3xl font-bold">Heading 1</span>,
 		command: heading1Toggle,
 	},
 	{
-		name: "h2",
+		key: "h2",
+		name: "Heading 2",
 		icon: <span className="text-2xl font-bold">Heading 2</span>,
 		command: heading2Toggle,
 	},
 	{
-		name: "h3",
+		key: "h3",
+		name: "Heading 3",
 		icon: <span className="text-xl">Heading 3</span>,
 		command: heading3Toggle,
 	},
 	{
-		name: "h4",
+		key: "h4",
+		name: "Heading 4",
 		icon: <span className="text-lg">Heading 4</span>,
 		command: heading4Toggle,
 	},
 	{
-		name: "h5",
+		key: "h5",
+		name: "Heading 5",
 		icon: <span className="text-base">Heading 5</span>,
 		command: heading5Toggle,
 	},
 	{
-		name: "h6",
+		key: "h6",
+		name: "Heading 6",
 		icon: <span className="text-sm font-normal">Heading 6</span>,
 		command: heading6Toggle,
 	},
@@ -84,9 +92,9 @@ const ParagraphDropdown = () => {
 	})[0];
 	return (
 		<Select
-			value={activeType?.name}
+			value={activeType?.key}
 			onValueChange={(value) => {
-				const item = paragraphTypeItems.find((i) => i.name === value);
+				const item = paragraphTypeItems.find((i) => i.key === value);
 				if (!item) {
 					return;
 				}
@@ -95,14 +103,17 @@ const ParagraphDropdown = () => {
 				view.focus();
 				run();
 			}}
+			disabled={!activeType}
 		>
 			<SelectTrigger className="w-fit border-none">
-				<SelectValue placeholder="Paragraph" />
+				<SelectValue>
+					{activeType ? activeType.name || activeType.key : "Paragraph"}
+				</SelectValue>
 			</SelectTrigger>
 			<SelectContent className="bg-white">
-				{paragraphTypeItems.map(({ name, icon, command }) => {
+				{paragraphTypeItems.map(({ key, icon, command }) => {
 					return (
-						<SelectItem key={name} value={name}>
+						<SelectItem key={key} value={key}>
 							{icon}
 						</SelectItem>
 					);
@@ -121,11 +132,11 @@ export const MenuBar = () => {
 			aria-label="Formatting tools"
 		>
 			{menuItems.map((menuItem) => {
-				const { name, icon, command } = menuItem;
+				const { key, icon, command } = menuItem;
 				const { run, canRun, isActive } = command(view)(view.state);
 				return (
 					<Button
-						key={name}
+						key={key}
 						onClick={() => {
 							view.focus();
 							run();
