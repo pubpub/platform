b7e63e2086e1d22803b8cbaffe42f511204e2080
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2025-03-05 16:55:00 -0500

    feat: complete migrate-hierarchy

---
+++ core/kysely/scripts/migrate-hierarchy.ts
@@ -0,0 +1,99 @@
+import pluralize from "pluralize";
+
+import type { CommunitiesId, PubsId } from "db/public";
+import { CoreSchemaType } from "db/public";
+import { expect } from "utils";
+
+import { db } from "~/kysely/database";
+import { createLastModifiedBy } from "~/lib/lastModifiedBy";
+import { slugifyString } from "~/lib/string";
+
+type Relation = {
+	parentPubId: PubsId;
+	childPubId: PubsId;
+	childPubTypeName: string;
+	communityId: CommunitiesId;
+	communitySlug: string;
+	relationSlug: string;
+};
+
+const makeRelationsByCommunityId = (relations: Relation[]) => {
+	const relationsByCommunityId = new Map<CommunitiesId, Map<string, Relation>>();
+	for (const relation of relations) {
+		let communityRelationSlugs = relationsByCommunityId.get(relation.communityId);
+		if (communityRelationSlugs === undefined) {
+			communityRelationSlugs = new Map();
+			relationsByCommunityId.set(relation.communityId, communityRelationSlugs);
+		}
+		communityRelationSlugs.set(relation.relationSlug, relation);
+	}
+	return relationsByCommunityId;
+};
+
+const migrateHierarchy = async () => {
+	const relations: Relation[] = (
+		await db
+			.selectFrom("pubs as children")
+			.innerJoin("pubs as parents", "children.parentId", "parents.id")
+			.innerJoin("pub_types", "pub_types.id", "children.pubTypeId")
+			.innerJoin("communities", "communities.id", "pub_types.communityId")
+			.select([
+				"parents.id as parentPubId",
+				"children.id as childPubId",
+				"pub_types.name as childPubTypeName",
+				"communities.id as communityId",
+				"communities.slug as communitySlug",
+			])
+			.execute()
+	).map((relation) => {
+		return {
+			...relation,
+			relationSlug: `${relation.communitySlug}:${slugifyString(pluralize(relation.childPubTypeName))}`,
+		};
+	});
+
+	const relationsByCommunityId = makeRelationsByCommunityId(relations);
+
+	const relationFieldInsertExpressions = Array.from(relationsByCommunityId.entries()).flatMap(
+		([communityId, communityRelationSlugs]) =>
+			Array.from(communityRelationSlugs).map(([, communityRelation]) => ({
+				communityId,
+				name: pluralize(communityRelation.childPubTypeName),
+				slug: communityRelation.relationSlug,
+				schemaName: CoreSchemaType.Null,
+				isRelation: true,
+			}))
+	);
+
+	// upsert new relation fields
+	const relationResults = await db
+		.insertInto("pub_fields")
+		.values(relationFieldInsertExpressions)
+		.returningAll()
+		// Field slugs are unique, so do nothing if the slug already exists
+		.onConflict((b) => b.doNothing())
+		.execute();
+
+	const persistedRelationsByRelationSlug = new Map(
+		relationResults.map((relationResult) => [relationResult.slug, relationResult])
+	);
+
+	const relationValueInsertExpressions = relations.map((relation) => ({
+		fieldId: expect(persistedRelationsByRelationSlug.get(relation.relationSlug)).id,
+		pubId: relation.parentPubId,
+		relatedPubId: relation.childPubId,
+		lastModifiedBy: createLastModifiedBy("system"),
+		value: null,
+	}));
+
+	// upsert relation values
+	await db
+		.insertInto("pub_values")
+		.values(relationValueInsertExpressions)
+		// [pubId, relatedPubId, fieldId] is unique, so do nothing if the
+		// relationship already exists
+		.onConflict((b) => b.doNothing())
+		.execute();
+};
+
+migrateHierarchy();
