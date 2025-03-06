8f975332dfe58d352de6beb26d89c25cb060ec2d
Thomas F. K. Jorna <hello@tefkah.com>
2025-03-06 15:33:00 +0100

    fix: correct link for main

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -24,7 +24,7 @@ const navbar = (
 			</div>
 		}
 		projectLink="https://github.com/pubpub/platform"
-		logoLink="/"
+		logoLink={path("/")}
 	/>
 );
 const footer = <Footer>MIT {new Date().getFullYear()} © Knowledge Futures Inc.</Footer>;
