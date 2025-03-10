c80b138753653cc5f4e5ac382ec5ccd9cb3ec1ae
allisonking <aking@knowledgefutures.org>
2025-03-10 16:16:14 -0400

    Add bold test

---
+++ packages/context-editor/playwright/marks.spec.ts
@@ -0,0 +1,35 @@
+import { expect, test } from "@playwright/test";
+
+const BLANK_EDITOR_STORY = "/iframe.html?id=contexteditor--blank";
+
+test.describe("bold", () => {
+	test.beforeEach(async ({ page }) => {
+		await page.goto(BLANK_EDITOR_STORY);
+		const editor = page.locator(".ProseMirror");
+		await editor.click();
+	});
+	test("can use markdown shortcut", async ({ page }) => {
+		await page.keyboard.type("hi **bold** ");
+		expect(page.getByRole("strong")).toHaveText("bold");
+	});
+
+	test("can use menu bar", async ({ page }) => {
+		await page.keyboard.type("hi ");
+		await page.getByRole("button", { name: "Bold" }).click();
+		await page.keyboard.type("bold");
+		await page.getByRole("button", { name: "Bold" }).click();
+		await page.keyboard.type("not bold");
+		expect(page.getByRole("strong")).toHaveText("bold");
+	});
+
+	test("can use menu bar to affect selection", async ({ page }) => {
+		await page.keyboard.type("hello world");
+		// Highlight the text
+		for (let i = 0; i < "world".length; i++) {
+			await page.keyboard.press("Shift+ArrowLeft");
+		}
+		await page.getByRole("button", { name: "Bold" }).click();
+		expect(page.getByRole("strong")).toHaveText("world");
+		await page.getByRole("button", { name: "Bold" }).click();
+	});
+});
