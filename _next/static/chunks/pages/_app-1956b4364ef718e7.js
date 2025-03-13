68085e0339263475ca1bd34ebc38dc5ef66c964e
allisonking <allisonjuliaking@gmail.com>
2025-03-13 17:40:15 +0000

    Deploying to gh-pages from @ pubpub/platform@ab39f7b8774a70cb383d224564f0bd2adc139163 🚀

---
+++ _next/static/chunks/pages/_app-1956b4364ef718e7.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{6830:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(8361)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(6830),n(1947))),_N_E=_.O()}]);
