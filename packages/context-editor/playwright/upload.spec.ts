e0987de73258f0a2a93a45ba3bbd599f83932a15
allisonking <aking@knowledgefutures.org>
2025-03-13 12:58:55 -0400

    Move test.use

--- packages/context-editor/playwright/upload.spec.ts
+++ packages/context-editor/playwright/upload.spec.ts
@@ -3,6 +3,9 @@ import { expect, test } from "@playwright/test";
 import { BLANK_EDITOR_STORY } from "./constants";
 
 test.describe("image upload", () => {
+	// https://stackoverflow.com/questions/78333672/playwright-test-with-file-uploading-says-target-is-closed
+	test.use({ headless: false });
+
 	test.beforeEach(async ({ page }) => {
 		await page.goto(BLANK_EDITOR_STORY);
 		const editor = page.locator(".ProseMirror");
@@ -10,10 +13,6 @@ test.describe("image upload", () => {
 	});
 
 	test("can upload an image", async ({ page, browserName }) => {
-		// https://stackoverflow.com/questions/78333672/playwright-test-with-file-uploading-says-target-is-closed
-		if (browserName === "firefox") {
-			test.use({ headless: false });
-		}
 		await page.getByRole("button", { name: "Image" }).click();
 		await page.getByRole("button", { name: "browse files" }).click();
 		await page
