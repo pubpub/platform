ea83bb1ac9f501b6cd6eed358a24ee14e1c55d77
allisonking <aking@knowledgefutures.org>
2025-03-13 12:51:12 -0400

    Add exception for firefox??

--- packages/context-editor/playwright/upload.spec.ts
+++ packages/context-editor/playwright/upload.spec.ts
@@ -9,7 +9,11 @@ test.describe("image upload", () => {
 		await editor.click();
 	});
 
-	test("can upload an image", async ({ page }) => {
+	test("can upload an image", async ({ page, browserName }) => {
+		// https://stackoverflow.com/questions/78333672/playwright-test-with-file-uploading-says-target-is-closed
+		if (browserName === "firefox") {
+			test.use({ headless: false });
+		}
 		await page.getByRole("button", { name: "Image" }).click();
 		await page.getByRole("button", { name: "browse files" }).click();
 		await page
