741bc0de86b3ea6c6cf353455260e26f275f6fc2
Thomas F. K. Jorna <hello@tefkah.com>
2025-03-26 18:28:39 +0100

    refactor: move join page to community signup page and make it work

--- core/app/c/(public)/[communitySlug]/public/signup/page.tsx
+++ core/app/c/(public)/[communitySlug]/public/signup/page.tsx
@@ -1,18 +1,28 @@
-import { notFound, redirect } from "next/navigation";
+import { notFound, redirect, RedirectType, unstable_rethrow } from "next/navigation";
 
+import { MemberRole } from "db/public";
+import { logger } from "logger";
+
+import { JoinCommunityForm } from "~/app/components/Signup/JoinCommunityForm";
 import { PublicSignupForm } from "~/app/components/Signup/PublicSignupForm";
 import { getLoginData } from "~/lib/authentication/loginData";
 import { findCommunityBySlug } from "~/lib/server/community";
 import { publicSignupsAllowed } from "~/lib/server/user";
 
 export default async function Page({
+	params,
 	searchParams,
 }: {
+	params: Promise<{ communitySlug: string }>;
 	searchParams: Promise<{ redirectTo?: string }>;
 }) {
 	const [community, { user }] = await Promise.all([findCommunityBySlug(), getLoginData()]);
 
 	if (!community) {
+		logger.debug({
+			msg: "Community not found on signup page",
+			communitySlug: (await params).communitySlug,
+		});
 		notFound();
 	}
 
@@ -27,15 +37,19 @@ export default async function Page({
 
 	if (user) {
 		if (user.memberships.some((m) => m.communityId === community.id)) {
-			redirect(redirectTo ?? "/");
+			redirect(redirectTo ?? `/c/${community.slug}/stages`);
 			// TODO: redirect to wherever they were redirected to before signing up
 			throw new Error("User is already member of community");
 		}
 
-		// TODO: user is already member of community
+		// TODO: figure this out based on the invite
+		const joinRole = MemberRole.contributor;
 
-		// TODO: redirect to join community page instead
-		throw new Error("User is already logged in");
+		return (
+			<div className="m-auto max-w-lg">
+				<JoinCommunityForm community={community} role={joinRole} />
+			</div>
+		);
 	}
 
 	return (
--- core/app/components/Signup/JoinCommunityForm.tsx
+++ core/app/components/Signup/JoinCommunityForm.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useCallback } from "react";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 
 import type { Communities } from "db/public";
@@ -8,18 +10,30 @@ import { Button } from "ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "ui/card";
 import { Form } from "ui/form";
 
+import { publicJoinCommunity } from "~/lib/authentication/actions";
+import { useServerAction } from "~/lib/serverActions";
+
 export const JoinCommunityForm = ({
 	community,
 	role = MemberRole.contributor,
+	redirectTo,
 }: {
 	community: Communities;
 	role?: MemberRole;
+	redirectTo?: string;
 }) => {
 	const form = useForm();
+	const runJoin = useServerAction(publicJoinCommunity);
+	const router = useRouter();
+
+	const onSubmit = useCallback(async () => {
+		await runJoin();
+		router.push(redirectTo ?? `/c/${community.slug}`);
+	}, [redirectTo, runJoin]);
 
 	return (
 		<Form {...form}>
-			<form onSubmit={form.handleSubmit(props.onSubmit)}>
+			<form onSubmit={form.handleSubmit(onSubmit)}>
 				<Card className="mx-auto max-w-sm">
 					<CardHeader>
 						<CardTitle className="text-xl">Join {community.name}</CardTitle>
@@ -30,7 +44,7 @@ export const JoinCommunityForm = ({
 					<CardContent>
 						<div className="grid gap-4">
 							<Button type="submit" className="w-full">
-								Finish sign up
+								Join {community.name}
 							</Button>
 						</div>
 					</CardContent>
