a987ccd6b4e90c5d8311c2e50b3f1252df62f95d
Kalil Smith-Nuevelle <kalilsn@gmail.com>
2025-04-02 08:54:52 -0500

    Don't rerender on selection change

--- packages/context-editor/src/plugins/onChange.ts
+++ packages/context-editor/src/plugins/onChange.ts
@@ -1,6 +1,5 @@
 import { Plugin } from "prosemirror-state";
 
-import { attributePanelKey } from "./attributePanel";
 import { reactPropsKey } from "./reactProps";
 
 export default () => {
@@ -9,15 +8,6 @@ export default () => {
 			return {
 				update: (editorView) => {
 					const { onChange } = reactPropsKey.getState(editorView.state);
-					const { panelPosition, setPanelPosition } = attributePanelKey.getState(
-						editorView.state
-					);
-					if (panelPosition.pos !== editorView.state.selection.$from.pos) {
-						setPanelPosition({
-							...panelPosition,
-							pos: editorView.state.selection.$from.pos,
-						});
-					}
 					onChange(editorView.state);
 				},
 			};
