2a01202a297ee0b844cf46df0149e463717c01d2
allisonking <aking@knowledgefutures.org>
2025-04-09 14:23:59 -0400

    Handle expired tokens

--- core/app/(user)/verify/page.tsx
+++ core/app/(user)/verify/page.tsx
@@ -4,18 +4,25 @@ import { AuthTokenType } from "db/public";
 
 import { getLoginData } from "~/lib/authentication/loginData";
 import { createRedirectUrl } from "~/lib/redirect";
+import { TokenFailureReason } from "~/lib/server/token";
 import { ResendVerificationButton } from "./ResendVerificationButton";
 
-export default async function Page({
-	searchParams,
-}: {
-	searchParams: Promise<{ redirectTo?: string }>;
-}) {
+type SearchParams =
+	| {
+			redirectTo?: string;
+	  }
+	| {
+			redirectTo?: string;
+			token: string;
+			reason: string;
+	  };
+
+export default async function Page({ searchParams }: { searchParams: Promise<SearchParams> }) {
 	const { user, session } = await getLoginData({
 		allowedSessions: [AuthTokenType.generic, AuthTokenType.verifyEmail],
 	});
 
-	const { redirectTo } = await searchParams;
+	const { redirectTo, ...search } = await searchParams;
 
 	if (!user || !session) {
 		const verifyUrl = redirectTo ? `/verify?redirectTo=${redirectTo}` : "/verify";
@@ -24,8 +31,11 @@ export default async function Page({
 
 	let description = "Check your email and click the link to verify your email address.";
 
+	if ("reason" in search && search.reason === TokenFailureReason.expired) {
+		description = "Your token has expired. Please request a new one.";
+	}
+
 	if (user.isVerified) {
-		description = "Your email has been verified!";
 		const url = redirectTo ? createRedirectUrl(redirectTo, { verified: "true" }) : "/";
 		redirect(url.toString());
 	}
