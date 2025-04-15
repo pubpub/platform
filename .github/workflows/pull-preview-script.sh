f661c2e1dd544966a17df563191f3df4db5c6d7b
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2025-04-15 14:26:32 -0400

    chore: remove test log/comment

--- .github/workflows/pull-preview-script.sh
+++ .github/workflows/pull-preview-script.sh
@@ -1,5 +1,5 @@
 # install latest version of docker compose, by default it's using an ancient version
 sudo curl -sL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-"$(uname -m)" \
   -o "$(which docker-compose)" && sudo chmod +x "$(which docker-compose)" 
-# small change to check whether deploy takes forever
+
 docker image prune -a -f
