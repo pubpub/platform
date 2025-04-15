4d0a1287f040a51c671b2958deccc0ceb8140831
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2025-04-15 14:33:03 -0400

    chore: print disk usage

--- .github/workflows/pull-preview-script.sh
+++ .github/workflows/pull-preview-script.sh
@@ -3,3 +3,5 @@ sudo curl -sL https://github.com/docker/compose/releases/latest/download/docker-
   -o "$(which docker-compose)" && sudo chmod +x "$(which docker-compose)" 
 
 docker image prune -a -f
+
+df -h
