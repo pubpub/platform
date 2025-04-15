18e93583160b12144f6356231c11205fc5a421eb
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2025-04-15 11:34:37 -0400

    chore: prune dangling docker images in preview environments

--- .github/workflows/pull-preview-script.sh
+++ .github/workflows/pull-preview-script.sh
@@ -1,3 +1,5 @@
 # install latest version of docker compose, by default it's using an ancient version
 sudo curl -sL https://github.com/docker/compose/releases/latest/download/docker-compose-linux-"$(uname -m)" \
   -o "$(which docker-compose)" && sudo chmod +x "$(which docker-compose)" 
+
+docker image prune -a -f
