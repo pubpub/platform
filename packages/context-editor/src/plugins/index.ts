64043b6ca5520dfb01d4c9c90451ef253d794372
Allison King <aking@knowledgefutures.org>
2025-04-17 12:56:06 -0700

    Use react-prosemirror (#1159)
    * Get attribute panel kind of working with marks
    
    * Add copy of AttributePanel for easier comparison
    
    * Restore original AttributePanel
    
    * Get block decoration working sorta
    
    * get attribute panel working better
    
    * Integrate AtomRenderer
    
    * Add inline decoration
    
    * Suggest panel
    
    * Handle onChange separately
    
    * Make menu bar work again
    
    * Fix SitePanel
    
    * Fix image uploading
    
    * Make buttons buttons
    
    * Restore disabled and hide menu states
    
    * Update core's AtomRenderer
    
    * Fix panel alignment with menu bar
    
    * Make sure upload test waits for image to be inserted into the editor itself
    
    * Remove other prosemirror react packages
    
    * Fix bad setState
    
    * Fix bug where active nodes would become inactive while setting ids
    
    * Fix attribute key funkiness
    
    * Restore menu bar stickiness
    
    * Memoize node view per docs
    
    * Simplify setting of atom rendering component
    
    * Make attribute panel labels and inputs more accessible
    
    * Add suggest panel and atom renderer tests
    
    * Remove fragment
    
    ---------
    
    Co-authored-by: Eric McDaniel <eric.g.mcdaniel@gmail.com>

--- packages/context-editor/src/plugins/index.ts
+++ packages/context-editor/src/plugins/index.ts
@@ -2,13 +2,11 @@ import { mathPlugin } from "@benrbray/prosemirror-math";
 import { exampleSetup } from "prosemirror-example-setup";
 import { Schema } from "prosemirror-model";
 
-import type { ContextEditorProps, PanelProps } from "../ContextEditor";
-import attributePanel from "./attributePanel";
+import type { ContextEditorProps } from "../ContextEditor";
 import code from "./code";
 import contextSuggest from "./contextSuggest";
 import inputRules from "./inputRules";
 import keymap from "./keymap";
-import onChange from "./onChange";
 import pasteRules from "./pasteRules";
 import reactProps from "./reactProps";
 import structureDecorations from "./structureDecorations";
@@ -16,8 +14,6 @@ import structureDecorations from "./structureDecorations";
 export const basePlugins = (
 	schema: Schema,
 	props: ContextEditorProps,
-	panelPosition: PanelProps,
-	setPanelPosition: React.Dispatch<React.SetStateAction<PanelProps>>,
 	suggestData: any,
 	setSuggestData: any
 ) => {
@@ -29,8 +25,6 @@ export const basePlugins = (
 		...exampleSetup({ schema, menuBar: false }),
 		reactProps(props),
 		structureDecorations(),
-		attributePanel(panelPosition, setPanelPosition),
-		onChange(),
 		pasteRules(schema),
 		inputRules(schema),
 		mathPlugin,
