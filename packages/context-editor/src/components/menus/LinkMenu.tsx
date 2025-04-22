5c7ac68f3b0bcfb958974ffbc7135551062b2615
allisonking <aking@knowledgefutures.org>
2025-04-22 10:53:37 -0400

    Add form errors

--- packages/context-editor/src/components/menus/LinkMenu.tsx
+++ packages/context-editor/src/components/menus/LinkMenu.tsx
@@ -1,22 +1,16 @@
 import type { Static } from "@sinclair/typebox";
 import type { Mark } from "prosemirror-model";
 
-import React from "react";
+import React, { useMemo } from "react";
 import { useEditorEventCallback } from "@handlewithcare/react-prosemirror";
 import { typeboxResolver } from "@hookform/resolvers/typebox";
 import { Type } from "@sinclair/typebox";
+import { TypeCompiler } from "@sinclair/typebox/compiler";
 import { useForm } from "react-hook-form";
+import { registerFormats } from "schemas";
 
 import { Button } from "ui/button";
-import {
-	Form,
-	FormControl,
-	FormDescription,
-	FormField,
-	FormItem,
-	FormLabel,
-	FormMessage,
-} from "ui/form";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "ui/form";
 import { ExternalLink, Trash } from "ui/icon";
 import { Input } from "ui/input";
 import { Switch } from "ui/switch";
@@ -24,10 +18,14 @@ import { Switch } from "ui/switch";
 import { toggleMarkExpandEmpty } from "../../commands/marks";
 import { baseSchema } from "../../schemas";
 
+registerFormats();
+
 const formSchema = Type.Object({
-	href: Type.String(),
+	href: Type.String({ format: "uri" }),
 	openInNewTab: Type.Boolean({ default: false }),
 });
+
+const compiledSchema = TypeCompiler.Compile(formSchema);
 
 type FormSchema = Static<typeof formSchema>;
 
@@ -48,16 +46,19 @@ export const LinkMenu = ({ mark, onChange }: LinkMenuProps) => {
 		})
 	);
 
-	const form = useForm<FormSchema>({
-		resolver: typeboxResolver(formSchema),
-		// defaultValues: {
-		// 	href: node?.attrs?.href ?? "",
-		// 	openInNewTab: node?.attrs?.target === "_blank",
-		// },
-	});
-
 	const url = mark.attrs?.href ?? "";
 	const openInNewTab = mark.attrs?.target === "_blank" ? true : false;
+
+	const resolver = useMemo(() => typeboxResolver(compiledSchema), []);
+
+	const form = useForm<FormSchema>({
+		resolver,
+		mode: "onBlur",
+		defaultValues: {
+			href: url,
+			openInNewTab,
+		},
+	});
 
 	return (
 		<>
@@ -67,37 +68,42 @@ export const LinkMenu = ({ mark, onChange }: LinkMenuProps) => {
 					<FormField
 						name="href"
 						control={form.control}
-						render={({ field }) => (
-							<FormItem className="flex items-center gap-2">
-								<FormLabel>URL</FormLabel>
-								<FormDescription></FormDescription>
-								<Input
-									defaultValue={url}
-									onChange={(event) => {
-										onChange(field.name, event.target.value);
-									}}
-									type="url"
-									placeholder="https://example.com"
-								/>
-								<FormMessage />
-								<div className="flex items-center">
-									<a
-										href={url}
-										target="_blank"
-										className="cursor-pointer text-gray-500"
-									>
-										<ExternalLink strokeWidth="1px" size="20" />
-									</a>
-									<Button
-										className="px-2 text-gray-500"
-										variant="ghost"
-										onClick={removeLink}
-									>
-										<Trash />
-									</Button>
-								</div>
-							</FormItem>
-						)}
+						render={({ field }) => {
+							return (
+								<FormItem className="flex flex-col">
+									<div className="flex items-center gap-2 space-y-0">
+										<FormLabel>URL</FormLabel>
+										<Input
+											defaultValue={url}
+											onChange={(event) => {
+												onChange(field.name, event.target.value);
+												field.onChange(event);
+											}}
+											onBlur={field.onBlur}
+											type="url"
+											placeholder="https://example.com"
+										/>
+										<div className="flex items-center">
+											<a
+												href={url}
+												target="_blank"
+												className="cursor-pointer text-gray-500"
+											>
+												<ExternalLink strokeWidth="1px" size="20" />
+											</a>
+											<Button
+												className="px-2 text-gray-500"
+												variant="ghost"
+												onClick={removeLink}
+											>
+												<Trash />
+											</Button>
+										</div>
+									</div>
+									<FormMessage />
+								</FormItem>
+							);
+						}}
 					/>
 					<hr />
 					<FormField
@@ -112,8 +118,9 @@ export const LinkMenu = ({ mark, onChange }: LinkMenuProps) => {
 										checked={openInNewTab}
 										onCheckedChange={(checked) => {
 											onChange("target", checked ? "_blank" : null);
-											return field.onChange(event);
+											field.onChange(checked);
 										}}
+										onBlur={field.onBlur}
 									/>
 								</FormControl>
 								<FormMessage />
