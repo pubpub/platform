94fb175683c45f88432ea87c01cd386b239b4deb
Kalil Smith-Nuevelle <kalilsn@gmail.com>
2025-04-23 16:35:58 -0500

    Wait longer

--- core/playwright/fixtures/forms-edit-page.ts
+++ core/playwright/fixtures/forms-edit-page.ts
@@ -36,6 +36,6 @@ export class FormsEditPage {
 	async saveForm() {
 		await this.page.getByTestId("save-form-button").click();
 		await this.page.getByText("Form Successfully Saved", { exact: true }).waitFor();
-		await this.page.waitForTimeout(1000);
+		await this.page.waitForTimeout(5000);
 	}
 }
