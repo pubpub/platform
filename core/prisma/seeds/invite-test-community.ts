40f29531301821de66496c38f4279f402a1cc1f3
Thomas F. K. Jorna <hello@tefkah.com>
2025-04-24 20:23:35 +0200

    fix: deterministically create invite urls

--- core/prisma/seeds/invite-test-community.ts
+++ core/prisma/seeds/invite-test-community.ts
@@ -2,7 +2,7 @@ import crypto from "crypto";
 
 import { faker } from "@faker-js/faker";
 
-import type { PubsId, UsersId } from "db/public";
+import type { InvitesId, PubsId, UsersId } from "db/public";
 import {
 	Action,
 	CoreSchemaType,
@@ -50,6 +50,21 @@ const communityFormSlug = "community-form";
 const invitedUserId = crypto.randomUUID() as UsersId;
 
 const pub1Id = crypto.randomUUID() as PubsId;
+
+const createTokenOfOnly = (letter: string, index: number = 0): string => {
+	const uuidParts = [
+		letter.repeat(8),
+		letter.repeat(4),
+		`${4}${letter.repeat(3)}`,
+		letter.repeat(4),
+		index.toString().repeat(12),
+	];
+
+	const uuid = uuidParts.join("-");
+	const randomPart = `${letter.repeat(8)}${index.toString().repeat(8)}`;
+
+	return `${uuid}.${randomPart}`;
+};
 
 const seed = createSeed({
 	community: {
@@ -180,31 +195,37 @@ const seed = createSeed({
 	},
 	invites: {
 		expiredEmailInvite: {
+			token: createTokenOfOnly("a", 0),
 			email: email1,
 			status: InviteStatus.pending,
 			lastSentAt: new Date(),
 			expiresAt: new Date(Date.now() - 1000 * 60 * 60 * 24),
 		},
 		acceptedEmailInvite: {
+			token: createTokenOfOnly("a", 1),
 			email: email3,
 			status: InviteStatus.accepted,
 			lastSentAt: new Date(),
 		},
 		rejectedEmailInvite: {
+			token: createTokenOfOnly("a", 2),
 			email: `${faker.person.firstName()}@example.com`,
 			status: InviteStatus.rejected,
 			lastSentAt: new Date(),
 		},
 		revokedEmailInvite: {
+			token: createTokenOfOnly("a", 3),
 			email: `${faker.person.firstName()}@example.com`,
 			status: InviteStatus.revoked,
 			lastSentAt: new Date(),
 		},
 		createdEmailInvite: {
+			token: createTokenOfOnly("a", 4),
 			email: `${faker.person.firstName()}@example.com`,
 			status: InviteStatus.created,
 		},
 		happyPathEmailInvite: {
+			token: createTokenOfOnly("b", 5),
 			email: email2,
 			communityLevelFormSlugs: ["Evaluation"],
 			communityRole: MemberRole.contributor,
@@ -212,37 +233,44 @@ const seed = createSeed({
 			lastSentAt: new Date(),
 		},
 		expiredUserInvite: {
+			token: createTokenOfOnly("a", 6),
 			userId: invitedUserId,
 			status: InviteStatus.pending,
 			lastSentAt: new Date(),
 			expiresAt: new Date(Date.now() - 1000 * 60 * 60 * 24),
 		},
 		acceptedUserInvite: {
+			token: createTokenOfOnly("a", 7),
 			userId: invitedUserId,
 			status: InviteStatus.accepted,
 			lastSentAt: new Date(),
 		},
 		rejectedUserInvite: {
+			token: createTokenOfOnly("a", 8),
 			userId: invitedUserId,
 			status: InviteStatus.rejected,
 			lastSentAt: new Date(),
 		},
 		revokedUserInvite: {
+			token: createTokenOfOnly("a", 9),
 			userId: invitedUserId,
 			status: InviteStatus.revoked,
 			lastSentAt: new Date(),
 		},
 		createdUserInvite: {
+			token: createTokenOfOnly("c", 0),
 			userId: invitedUserId,
 			status: InviteStatus.created,
 		},
 		happyPathUserInvite: {
+			token: createTokenOfOnly("b", 1),
 			userId: invitedUserId,
 			communityLevelFormSlugs: ["Evaluation"],
 			status: InviteStatus.pending,
 			lastSentAt: new Date(),
 		},
 		rejectEmailInvite: {
+			token: createTokenOfOnly("b", 3),
 			email: email2,
 			pubId: pub1Id,
 			pubOrStageFormSlugs: ["Evaluation"],
@@ -254,6 +282,7 @@ const seed = createSeed({
 		},
 
 		pubLevelFormInvite: {
+			token: createTokenOfOnly("b", 4),
 			email: email4,
 			pubId: pub1Id,
 			pubOrStageFormSlugs: ["CommunityForm"],
@@ -264,6 +293,7 @@ const seed = createSeed({
 			expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24),
 		},
 		adminRoleInvite: {
+			token: createTokenOfOnly("b", 6),
 			email: email5,
 			communityRole: MemberRole.admin,
 			status: InviteStatus.pending,
@@ -271,6 +301,7 @@ const seed = createSeed({
 			expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24),
 		},
 		multipleInvite1: {
+			token: createTokenOfOnly("b", 7),
 			email: email6,
 			communityRole: MemberRole.contributor,
 			status: InviteStatus.pending,
@@ -278,6 +309,7 @@ const seed = createSeed({
 			expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24),
 		},
 		multipleInvite2: {
+			token: createTokenOfOnly("b", 8),
 			email: email6,
 			communityRole: MemberRole.contributor,
 			status: InviteStatus.pending,
@@ -285,6 +317,7 @@ const seed = createSeed({
 			expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24),
 		},
 		nearlyExpiredInvite: {
+			token: createTokenOfOnly("b", 9),
 			email: email1,
 			communityRole: MemberRole.contributor,
 			status: InviteStatus.pending,
