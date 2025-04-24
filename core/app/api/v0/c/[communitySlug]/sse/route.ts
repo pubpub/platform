06dc3c56e9ad5b1fca94d1008c48e43173ea78b2
Thomas F. K. Jorna <hello@tefkah.com>
2025-04-24 13:43:20 +0200

    feat: make notify channels per table per community

--- core/app/api/v0/c/[communitySlug]/sse/route.ts
+++ core/app/api/v0/c/[communitySlug]/sse/route.ts
@@ -9,6 +9,7 @@ import { logger } from "logger";
 
 import { pool } from "~/kysely/database";
 import { getLoginData } from "~/lib/authentication/loginData";
+import { findCommunityBySlug } from "~/lib/server/community";
 
 type Tables = (typeof databaseTableNames)[number];
 
@@ -17,6 +18,12 @@ type Tables = (typeof databaseTableNames)[number];
  */
 const notifyTables = ["action_runs"] as const satisfies Tables[];
 export type NotifyTables = (typeof notifyTables)[number];
+
+const parseNotifyTables = (tables: string[]): NotifyTables[] => {
+	return tables.filter((table): table is NotifyTables =>
+		notifyTables.includes(table as NotifyTables)
+	);
+};
 
 export const dynamic = "force-dynamic";
 export type ChangeNotification<T extends NotifyTables> = {
@@ -37,24 +44,39 @@ const handleClose = (client?: PoolClient) => {
 	};
 };
 
+const constructChangeChannel = (communityId: string, table: NotifyTables) => {
+	return `change_${communityId}_${table}`;
+};
+
 // bit awkward since we want to read the search params here, but the next-use-sse does not expose the request
 export const GET = (req: NextRequest) => {
 	return createSSEHandler(async (send, close, { onClose }) => {
-		const listen = req.nextUrl.searchParams.get("listen");
-		const listenTables = listen && !Array.isArray(listen) ? [listen] : listen;
+		const listen = parseNotifyTables(req.nextUrl.searchParams.getAll("listen"));
+
+		if (!listen?.length) {
+			logger.info("no listen tables, closing sse connection");
+			return handleClose();
+		}
 
 		logger.info("opening sse connection");
-		const { user } = await getLoginData();
+		const [{ user }, community] = await Promise.all([getLoginData(), findCommunityBySlug()]);
 
 		if (!user) {
 			logger.info("no user found, closing sse connection");
 			return handleClose();
 		}
 
+		if (!community) {
+			logger.info("no community found, closing sse connection");
+			return handleClose();
+		}
+
 		const client = await pool.connect();
 
-		// listen for action run updates
-		await client.query(`LISTEN change`);
+		listen.forEach(async (table) => {
+			const channelName = constructChangeChannel(community.id, table);
+			await client.query(`LISTEN "${channelName}"`);
+		});
 
 		// handle postgres notifications
 		client.on("notification", async (msg) => {
@@ -63,11 +85,8 @@ export const GET = (req: NextRequest) => {
 			try {
 				const notification = JSON.parse(msg.payload) as ChangeNotification<NotifyTables>;
 
-				if (
-					listenTables &&
-					listenTables.length &&
-					!listenTables.includes(notification.table)
-				) {
+				if (!listen.includes(notification.table)) {
+					logger.info("not listening to this table, skipping");
 					return;
 				}
 
