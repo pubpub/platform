c55429ab2c53c88877bb5c580cf7038bb5df9dcd
Allison King <aking@knowledgefutures.org>
2025-04-30 10:17:04 -0700

    Link menu (#1177)
    * Add id and class to link attributes
    
    * Add link menu based on #1147
    
    Co-authored-by: Kalil Smith-Nuevelle <kalilsn@gmail.com>
    
    * Make attribute panel open/close a bit more intuitively
    
    * Make link menu update attributes
    
    * Add form errors
    
    * Add advanced options
    
    * Attribute panel opening and closing tests
    
    * Add some link tests
    
    * Add state to blank story
    
    * Fix locators
    
    * Fix type
    
    * Add failing test
    
    * Fix some browser differences
    
    * Only autofocus when there is no url
    
    * Change the link form to submit on blur so that we always have valid data in the pm doc
    
    * Clean up
    
    * Clean up some more
    
    * Make scroll work kinda
    
    * Make panel appear on right properly
    
    * Fix browser clicking discrepencies
    
    * Update selector for hard breaks test
    
    * await expect :facepalm:
    
    * useRef for container and restore `relative`
    
    * Remove extra prop
    
    * Add exception in test for webkit CI
    
    * Properly pass ref. should make tests more stable
    
    ---------
    
    Co-authored-by: Kalil Smith-Nuevelle <kalilsn@gmail.com>

--- packages/context-editor/src/schemas/link.ts
+++ packages/context-editor/src/schemas/link.ts
@@ -3,8 +3,9 @@ import type { DOMOutputSpec, MarkSpec } from "prosemirror-model";
 export default {
 	inclusive: false,
 	attrs: {
+		id: { default: null },
+		class: { default: null },
 		href: { default: "" },
-		title: { default: null },
 		target: { default: null },
 	},
 	parseDOM: [
@@ -12,8 +13,9 @@ export default {
 			tag: "a[href]",
 			getAttrs: (dom) => {
 				return {
+					id: dom.getAttribute("id"),
+					class: dom.getAttribute("class"),
 					href: dom.getAttribute("href"),
-					title: dom.getAttribute("title"),
 					target: dom.getAttribute("target"),
 				};
 			},
