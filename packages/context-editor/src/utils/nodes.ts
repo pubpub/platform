2264f50b98504c42d58417b790d9a65d01ac93a8
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2025-05-12 13:43:01 -0400

    remove more unused code

--- packages/context-editor/src/utils/nodes.ts
+++ packages/context-editor/src/utils/nodes.ts
@@ -53,51 +53,6 @@ export const insertMedia = (state: EditorState, dispatch: Dispatch, attrs: Image
 	dispatch(transaction);
 };
 
-export const insertNodeAtPos = (
-	state: EditorState,
-	dispatch: Dispatch,
-	pos: number,
-	childNodeType: string,
-	attrs?: Node["attrs"]
-) => {
-	const { schema, tr } = state;
-	const nodeSchema = schema.nodes[childNodeType];
-	const node = nodeSchema.create(attrs);
-	const transaction = tr.insert(pos, node);
-	dispatch(transaction);
-};
-
 export const isNode = (node: Node | Mark): node is Node => {
 	return "children" in node;
 };
-
-// export const insertNodeAsFirstChildOfActiveNode = (
-// 	state: EditorState,
-// 	dispatch: Dispatch,
-// 	nodeType: string,
-// 	attrs?: Node["attrs"]
-// ) => {
-// 	const { schema, tr } = state;
-// 	const nodeSchema = schema.nodes[nodeType];
-// 	const node = nodeSchema.create(attrs);
-// 	const { $from } = state.selection;
-// 	const parentPos = $from.before($from.depth);
-// 	const transaction = tr.insert(parentPos, node);
-// 	dispatch(transaction);
-// };
-
-// export const removeNthChildOfActiveNode = (state: EditorState, dispatch: Dispatch, nth: number) => {
-// 	const { tr, selection } = state;
-// 	const { $from } = selection;
-// 	const parentPos = $from.before($from.depth);
-// 	const parentNode = $from.node($from.depth);
-// 	const childNode = parentNode.child(nth);
-
-// 	if (childNode) {
-// 		const transaction = tr.delete(
-// 			parentPos + childNode.pos,
-// 			parentPos + childNode.pos + childNode.nodeSize
-// 		);
-// 		dispatch(transaction);
-// 	}
-// };
