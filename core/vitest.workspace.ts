11bda48f67c8eac6656f337721b9e30580f9366a
allisonking <aking@knowledgefutures.org>
2025-05-27 12:16:26 -0400

    Add storybook to core

---
+++ core/vitest.workspace.ts
@@ -0,0 +1,30 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+import { storybookTest } from "@storybook/experimental-addon-test/vitest-plugin";
+import { defineWorkspace } from "vitest/config";
+
+const dirname =
+	typeof __dirname !== "undefined" ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+
+// More info at: https://storybook.js.org/docs/writing-tests/test-addon
+export default defineWorkspace([
+	"vitest.config.mts",
+	{
+		plugins: [
+			// The plugin will run tests for the stories defined in your Storybook config
+			// See options at: https://storybook.js.org/docs/writing-tests/test-addon#storybooktest
+			storybookTest({ configDir: path.join(dirname, ".storybook") }),
+		],
+		test: {
+			name: "storybook",
+			browser: {
+				enabled: true,
+				headless: true,
+				provider: "playwright",
+				instances: [{ browser: "chromium" }],
+			},
+			setupFiles: [".storybook/vitest.setup.ts"],
+		},
+	},
+]);
