23f26ea3a941f26ededc0c3241d8ade4040adfe4
allisonking <aking@knowledgefutures.org>
2025-05-28 17:42:22 -0400

    Prettier

--- core/.storybook/vitest.setup.ts
+++ core/.storybook/vitest.setup.ts
@@ -1,6 +1,7 @@
-import { beforeAll } from 'vitest';
-import { setProjectAnnotations } from '@storybook/experimental-nextjs-vite';
-import * as projectAnnotations from './preview';
+import { setProjectAnnotations } from "@storybook/experimental-nextjs-vite";
+import { beforeAll } from "vitest";
+
+import * as projectAnnotations from "./preview";
 
 // This is an important step to apply the right configuration when testing your stories.
 // More info at: https://storybook.js.org/docs/api/portable-stories/portable-stories-vitest#setprojectannotations
