8ed9767c7bf7d912a16e9c1018395cd513b06334
allisonking <aking@knowledgefutures.org>
2025-05-29 14:30:05 -0400

    Still render when there are no sources or destinations

--- core/app/c/[communitySlug]/stages/components/Move.tsx
+++ core/app/c/[communitySlug]/stages/components/Move.tsx
@@ -93,10 +93,6 @@ export default function Move(props: Props) {
 		setPopoverIsOpen(false);
 	};
 
-	if (destinations.length === 0 && sources.length === 0) {
-		return null;
-	}
-
 	return (
 		<Popover open={popoverIsOpen} onOpenChange={setPopoverIsOpen}>
 			<PopoverTrigger asChild>
