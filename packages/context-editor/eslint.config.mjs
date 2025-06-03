bde6318a9be447d276fb43e0ff747aafcd35ae0c
Thomas F. K. Jorna <hello@tefkah.com>
2025-06-03 15:21:54 +0200

    feat: add eslint config to context-editor

---
+++ packages/context-editor/eslint.config.mjs
@@ -0,0 +1,19 @@
+// @ts-check
+
+import baseConfig from "@pubpub/eslint-config/base";
+import reactConfig from "@pubpub/eslint-config/react";
+
+/** @type {import('typescript-eslint').Config} */
+export default [
+	{
+		ignores: ["dist/**", ".storybook/**"],
+		languageOptions: {
+			parserOptions: {
+				projectService: true,
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
+	},
+	...baseConfig,
+	...reactConfig,
+];
