ccea5b060d399e1eddda6794c29ab0f1f0e150f4
Thomas F. K. Jorna <hello@tefkah.com>
2025-06-11 15:38:36 +0200

    fix: don't dynamically import .js, dont work

--- core/actions/_lib/getRuns.ts
+++ core/actions/_lib/getRuns.ts
@@ -5,7 +5,7 @@ import type * as Runs from "../runs";
 export const getActionRunByName = async <T extends keyof typeof Runs>(name: T) => {
 	logger.info({ msg: `Loading action run for ${name}` });
 	try {
-		return (await import(`../${name}/run.js`)).run as (typeof Runs)[T];
+		return (await import(`../${name}/run`)).run as (typeof Runs)[T];
 	} catch (error) {
 		logger.error({ msg: `Failed to load action run for ${name}`, error });
 		return null;
