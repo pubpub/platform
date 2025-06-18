060833f90d9a477f40a829c31fb76c9ffb7a3780
Thomas F. K. Jorna <hello@tefkah.com>
2025-06-18 14:23:35 +0200

    feat: properly track server update status for highlighting

--- core/app/c/[communitySlug]/pubs/PubSearchInput.tsx
+++ core/app/c/[communitySlug]/pubs/PubSearchInput.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useDeferredValue, useEffect, useState } from "react";
 import { Search } from "lucide-react";
 import { parseAsInteger, parseAsString, useQueryStates } from "nuqs";
 import { useDebouncedCallback } from "use-debounce";
@@ -10,6 +10,8 @@ import { Input } from "ui/input";
 import { cn } from "utils";
 
 type PubSearchProps = React.PropsWithChildren<{}>;
+
+const DEBOUNCE_TIME = 300;
 
 export const PubSearch = (props: PubSearchProps) => {
 	const [query, setQuery] = useQueryStates(
@@ -21,15 +23,34 @@ export const PubSearch = (props: PubSearchProps) => {
 			shallow: false,
 		}
 	);
-	const [value, setValue] = useState(query.query);
+
+	// local input state for immediate UI responsiveness + sync with URL
+	// otherwise, when navigating back/forward or refreshing, the input will be empty
+	const [inputValue, setInputValue] = useState(query.query);
+
+	// deferred query to keep track of server updates
+	// without this, we can't only check if inputValue !== query.query,
+	// which only tells us that the debounce has happened
+	const deferredQuery = useDeferredValue(query.query);
+
+	// sync input with URL when navigating back/forward
+	useEffect(() => {
+		if (query.query === inputValue) {
+			return;
+		}
+		setInputValue(query.query);
+	}, [query.query]);
 
 	const { symbol, platform } = usePlatformModifierKey();
 
 	const debouncedSetQuery = useDebouncedCallback((value: string) => {
 		if (value.length >= 2 || value.length === 0) {
-			setQuery({ query: value });
+			setQuery({ query: value, page: 1 }); // reset to page 1 on new search
 		}
-	}, 500);
+	}, DEBOUNCE_TIME);
+
+	// determine if content is stale, in order to provide a visual feedback to the user
+	const isStale = inputValue !== deferredQuery;
 
 	return (
 		<div className="relative flex flex-col gap-4">
@@ -39,9 +60,9 @@ export const PubSearch = (props: PubSearchProps) => {
 					size={16}
 				/>
 				<Input
-					value={value}
+					value={inputValue}
 					onChange={(e) => {
-						setValue(e.target.value);
+						setInputValue(e.target.value);
 						debouncedSetQuery(e.target.value);
 					}}
 					placeholder="Search updates as you type..."
@@ -51,7 +72,9 @@ export const PubSearch = (props: PubSearchProps) => {
 					<span className={cn(platform === "mac" && "text-lg")}>{symbol}</span> K
 				</span>
 			</div>
-			<div className={cn(value !== query.query && "opacity-50")}>{props.children}</div>
+			<div className={cn(isStale && "opacity-50 transition-opacity duration-200")}>
+				{props.children}
+			</div>
 		</div>
 	);
 };
