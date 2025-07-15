3abcbe5853396fa6c370d427e5cf2702e9f1ce6e
Thomas F. K. Jorna <hello@tefkah.com>
2025-07-15 17:36:28 +0200

    fix: better shadow

--- core/app/login/LoginForm.tsx
+++ core/app/login/LoginForm.tsx
@@ -45,7 +45,7 @@ export default function LoginForm() {
 	return (
 		<Form {...form}>
 			<form onSubmit={form.handleSubmit(handleSubmit)}>
-				<Card className="w-full max-w-sm shadow-lg">
+				<Card className="w-full max-w-sm shadow-lg transition-shadow duration-300 hover:shadow-xl">
 					<CardHeader>
 						<CardTitle className="text-2xl">Login</CardTitle>
 						<CardDescription>
