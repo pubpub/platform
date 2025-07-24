841a4f3e946be2ac9ec422b5f5b5790156a2f89a
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2025-07-24 10:16:15 -0400

    fix: add user to form when creating pub to allow editing (#1320)
    * fix: add user to form when creating pub to allow editing
    
    * fix: do not always include default form; rm Editor suffix from default forms

--- core/lib/form.ts
+++ core/lib/form.ts
@@ -1,5 +1,4 @@
 import { slugifyString } from "./string";
 
-export const defaultFormName = (pubTypeName: string) => `${pubTypeName} Editor (Default)`;
-export const defaultFormSlug = (pubTypeName: string) =>
-	`${slugifyString(pubTypeName)}-default-editor`;
+export const defaultFormName = (pubTypeName: string) => `${pubTypeName} (Default)`;
+export const defaultFormSlug = (pubTypeName: string) => `${slugifyString(pubTypeName)}-default`;
