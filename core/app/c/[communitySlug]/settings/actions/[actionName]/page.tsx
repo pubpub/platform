63720817332be20eab459704427c94a4886c5c61
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2025-07-30 15:39:14 -0400

    scaffold page

---
+++ core/app/c/[communitySlug]/settings/actions/[actionName]/page.tsx
@@ -0,0 +1,8 @@
+export default async function Page() {
+	return (
+		<div>
+			<h1>Action Settings</h1>
+			<p>Manage X action settings here.</p>
+		</div>
+	);
+}
---
+++ core/app/c/[communitySlug]/settings/actions/page.tsx
@@ -0,0 +1,8 @@
+export default async function Page() {
+	return (
+		<div>
+			<h1>Actions Settings</h1>
+			<p>Manage your actions settings here.</p>
+		</div>
+	);
+}
