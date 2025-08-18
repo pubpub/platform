f7acc530661973f34eaf61e575b4374289784d82
Thomas F. K. Jorna <hello@tefkah.com>
2025-08-18 15:02:06 +0200

    fix: make sure migration runs properly in both reset and migrate

--- core/prisma/migrations/20250818120745_add_ranks_to_pubfields/migration.sql
+++ core/prisma/migrations/20250818120745_add_ranks_to_pubfields/migration.sql
@@ -6,27 +6,28 @@
 -- AlterTable
 -- BEGIN;
 ALTER TABLE "_PubFieldToPubType"
-  ADD COLUMN "rank" text COLLATE "C" NOT NULL;
+  ADD COLUMN "rank" text COLLATE "C";
 
 CREATE TEMP TABLE IF NOT EXISTS "mudder_ranks"(
   index SERIAL PRIMARY KEY,
   rank text
 );
 
+
+-- when running `prisma reset`, the previous values are still there
+-- but when running `prisma migrate dev`, the previous values are not there
+TRUNCATE TABLE "mudder_ranks";
+
 -- /*
 --  * This temp table holds 200 mudder generated keys which we use to assign initial ranks to existing
 --  * form elements and related pubs in the migration.
 --  * Generated with: mudder.base62.mudder(200).map((rank) => `('${rank}')`).join(", ")
 --  */
--- INSERT INTO "mudder_ranks"("index", "rank")
---   VALUES (0, '0J');
--- INSERT INTO "mudder_ranks"("rank")
---   VALUES ('0c'), ('0v'), ('1'), ('1X'), ('1q'), ('2'), ('2S'), ('2m'), ('3'), ('3O'), ('3h'), ('4'), ('4J'), ('4c'), ('4v'), ('5'), ('5Y'), ('5r'), ('6'), ('6T'), ('6m'), ('7'), ('7O'), ('7i'), ('8'), ('8K'), ('8d'), ('8w'), ('9'), ('9Y'), ('9r'), ('A'), ('AU'), ('An'), ('B'), ('BP'), ('Bi'), ('C'), ('CK'), ('Ce'), ('Cx'), ('D'), ('DZ'), ('Ds'), ('E'), ('EU'), ('En'), ('F'), ('FQ'), ('Fj'), ('G'), ('GL'), ('Ge'), ('Gx'), ('H'), ('Ha'), ('Ht'), ('I'), ('IV'), ('Io'), ('J'), ('JQ'), ('Jj'), ('K'), ('KM'), ('Kf'), ('Ky'), ('L'), ('La'), ('Lt'), ('M'), ('MW'), ('Mp'), ('N'), ('NR'), ('Nk'), ('O'), ('OM'), ('Of'), ('Oz'), ('P'), ('Pb'), ('Pu'), ('Q'), ('QW'), ('Qp'), ('R'), ('RS'), ('Rl'), ('S'), ('SN'), ('Sg'), ('Sz'), ('T'), ('Tb'), ('Tv'), ('U'), ('UX'), ('Uq'), ('V'), ('VS'), ('Vl'), ('W'), ('WO'), ('Wh'), ('X'), ('XJ'), ('Xc'), ('Xv'), ('Y'), ('YX'), ('Yr'), ('Z'), ('ZT'), ('Zm'), ('a'), ('aO'), ('ah'), ('b'), ('bK'), ('bd'), ('bw'), ('c'), ('cY'), ('cr'), ('d'), ('dT'), ('dn'), ('e'), ('eP'), ('ei'), ('f'), ('fK'), ('fd'), ('fw'), ('g'), ('gZ'), ('gs'), ('h'), ('hU'), ('hn'), ('i'), ('iP'), ('ij'), ('j'), ('jL'), ('je'), ('jx'), ('k'), ('kZ'), ('ks'), ('l'), ('lV'), ('lo'), ('m'), ('mQ'), ('mj'), ('n'), ('nL'), ('nf'), ('ny'), ('o'), ('oa'), ('ot'), ('p'), ('pV'), ('po'), ('q'), ('qR'), ('qk'), ('r'), ('rM'), ('rf'), ('ry'), ('s'), ('sb'), ('su'), ('t'), ('tW'), ('tp'), ('u'), ('uR'), ('uk'), ('v'), ('vN'), ('vg'), ('vz'), ('w'), ('wb'), ('wu'), ('x'), ('xX'), ('xq'), ('y'), ('yS'), ('yl'), ('z'), ('zN'), ('zg');
--- Add rank to form_elements
--- Uses "C" collation order to ensure uppercase letters sort before lowercase to match mudder
--- ALTER TABLE "_PubFieldToPubType"
---   ADD COLUMN "rank" text COLLATE "C";
--- Set initial rank values for form elements based on 'order', grouped by pubType
+INSERT INTO "mudder_ranks"("index", "rank")
+  VALUES (0, '0J');
+INSERT INTO "mudder_ranks"("rank")
+  VALUES ('0c'), ('0v'), ('1'), ('1X'), ('1q'), ('2'), ('2S'), ('2m'), ('3'), ('3O'), ('3h'), ('4'), ('4J'), ('4c'), ('4v'), ('5'), ('5Y'), ('5r'), ('6'), ('6T'), ('6m'), ('7'), ('7O'), ('7i'), ('8'), ('8K'), ('8d'), ('8w'), ('9'), ('9Y'), ('9r'), ('A'), ('AU'), ('An'), ('B'), ('BP'), ('Bi'), ('C'), ('CK'), ('Ce'), ('Cx'), ('D'), ('DZ'), ('Ds'), ('E'), ('EU'), ('En'), ('F'), ('FQ'), ('Fj'), ('G'), ('GL'), ('Ge'), ('Gx'), ('H'), ('Ha'), ('Ht'), ('I'), ('IV'), ('Io'), ('J'), ('JQ'), ('Jj'), ('K'), ('KM'), ('Kf'), ('Ky'), ('L'), ('La'), ('Lt'), ('M'), ('MW'), ('Mp'), ('N'), ('NR'), ('Nk'), ('O'), ('OM'), ('Of'), ('Oz'), ('P'), ('Pb'), ('Pu'), ('Q'), ('QW'), ('Qp'), ('R'), ('RS'), ('Rl'), ('S'), ('SN'), ('Sg'), ('Sz'), ('T'), ('Tb'), ('Tv'), ('U'), ('UX'), ('Uq'), ('V'), ('VS'), ('Vl'), ('W'), ('WO'), ('Wh'), ('X'), ('XJ'), ('Xc'), ('Xv'), ('Y'), ('YX'), ('Yr'), ('Z'), ('ZT'), ('Zm'), ('a'), ('aO'), ('ah'), ('b'), ('bK'), ('bd'), ('bw'), ('c'), ('cY'), ('cr'), ('d'), ('dT'), ('dn'), ('e'), ('eP'), ('ei'), ('f'), ('fK'), ('fd'), ('fw'), ('g'), ('gZ'), ('gs'), ('h'), ('hU'), ('hn'), ('i'), ('iP'), ('ij'), ('j'), ('jL'), ('je'), ('jx'), ('k'), ('kZ'), ('ks'), ('l'), ('lV'), ('lo'), ('m'), ('mQ'), ('mj'), ('n'), ('nL'), ('nf'), ('ny'), ('o'), ('oa'), ('ot'), ('p'), ('pV'), ('po'), ('q'), ('qR'), ('qk'), ('r'), ('rM'), ('rf'), ('ry'), ('s'), ('sb'), ('su'), ('t'), ('tW'), ('tp'), ('u'), ('uR'), ('uk'), ('v'), ('vN'), ('vg'), ('vz'), ('w'), ('wb'), ('wu'), ('x'), ('xX'), ('xq'), ('y'), ('yS'), ('yl'), ('z'), ('zN'), ('zg');
+
 -- Fields with isTitle=true get rank 0, others follow in order
 UPDATE
   "_PubFieldToPubType"
@@ -48,4 +49,7 @@ WHERE
   "_PubFieldToPubType"."A" = ranked_fields."A"
   AND "_PubFieldToPubType"."B" = ranked_fields."B";
 
--- COMMIT;
+
+-- set rank to not null
+ALTER TABLE "_PubFieldToPubType"
+  ALTER COLUMN "rank" SET NOT NULL;
