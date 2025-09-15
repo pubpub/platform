516981a0b297f9a7c6a38d841493320855176266
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2025-09-15 16:36:51 -0400

    feat: pass available forms

--- core/app/c/[communitySlug]/members/MemberTable.tsx
+++ core/app/c/[communitySlug]/members/MemberTable.tsx
@@ -2,11 +2,19 @@
 
 import * as React from "react";
 
+import type { FormsId } from "db/public";
+
 import type { TableMember } from "./getMemberTableColumns";
 import { DataTable } from "~/app/components/DataTable/DataTable";
 import { getMemberTableColumns } from "./getMemberTableColumns";
 
-export const MemberTable = ({ members }: { members: TableMember[] }) => {
-	const memberTableColumns = getMemberTableColumns();
+export const MemberTable = ({
+	members,
+	availableForms,
+}: {
+	members: TableMember[];
+	availableForms: { id: FormsId; name: string; isDefault: boolean }[];
+}) => {
+	const memberTableColumns = getMemberTableColumns(availableForms);
 	return <DataTable columns={memberTableColumns} data={members} searchBy="email" />;
 };
