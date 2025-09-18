766177cf3b0af099d64ab39fd9a2030d48ed4b9e
Thomas F. K. Jorna <hello@tefkah.com>
2025-09-18 21:08:04 +0200

    fix: make site building work again + fix some sandbox issues (#1360)

--- core/app/(user)/communities/CommunityTable.tsx
+++ core/app/(user)/communities/CommunityTable.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 import type { TableCommunity } from "./getCommunityTableColumns";
 import { DataTable } from "~/app/components/DataTable/DataTable";
