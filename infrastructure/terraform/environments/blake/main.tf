df064b8013ed63c8ce211559a3843ea29fd3a3ab
Thomas F. K. Jorna <hello@tefkah.com>
2025-09-25 18:03:24 +0200

    feat: add site builder deployment to stevie (#1364)
    * fix: update env situation in site-builder
    
    * feat: add site-builder deployment
    
    * feat: use existing s3 vars for site builder service in tf
    
    * chore: deploy site builder
    
    * chore: skip flaky tests temporarily
    
    ---------
    
    Co-authored-by: Eric McDaniel <eric.g.mcdaniel@gmail.com>

--- infrastructure/terraform/environments/blake/main.tf
+++ infrastructure/terraform/environments/blake/main.tf
@@ -39,21 +39,22 @@ locals {
   environment = "staging"
   region      = "us-east-1"
 
-  pubpub_hostname = "blake.duqduq.org"
+  pubpub_hostname       = "blake.duqduq.org"
+  site_builder_hostname = "bob.duqduq.org" # get it, like the builder
+
   route53_zone_id = "Z059164612717GL8VGM95"
   ecr_repository_urls = {
-    core  = "246372085946.dkr.ecr.us-east-1.amazonaws.com/pubpub-v7-core"
-    jobs  = "246372085946.dkr.ecr.us-east-1.amazonaws.com/pubpub-v7-jobs"
-    nginx = "246372085946.dkr.ecr.us-east-1.amazonaws.com/nginx"
-    root  = "246372085946.dkr.ecr.us-east-1.amazonaws.com/pubpub-v7"
+    core         = "246372085946.dkr.ecr.us-east-1.amazonaws.com/pubpub-v7-core"
+    jobs         = "246372085946.dkr.ecr.us-east-1.amazonaws.com/pubpub-v7-jobs"
+    nginx        = "246372085946.dkr.ecr.us-east-1.amazonaws.com/nginx"
+    root         = "246372085946.dkr.ecr.us-east-1.amazonaws.com/pubpub-v7"
+    site_builder = "246372085946.dkr.ecr.us-east-1.amazonaws.com/pubpub-v7-site-builder"
   }
 
-  MAILGUN_SMTP_USERNAME           = "v7@mg.pubpub.org"
-  NEXT_PUBLIC_SUPABASE_URL        = "https://dsleqjuvzuoycpeotdws.supabase.co"
-  NEXT_PUBLIC_SUPABASE_PUBLIC_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRzbGVxanV2enVveWNwZW90ZHdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODIzNTE0MjEsImV4cCI6MTk5NzkyNzQyMX0.3HHC0f7zlFXP77N0U8cS3blr7n6hhjqdYI6_ciQJams"
-  ASSETS_BUCKET_NAME              = "assets.blake.pubpub.org"
-  HOSTNAME                        = "0.0.0.0"
-  DATACITE_API_URL                = "https://api.test.datacite.org"
+  MAILGUN_SMTP_USERNAME = "v7@mg.pubpub.org"
+  ASSETS_BUCKET_NAME    = "assets.blake.pubpub.org"
+  HOSTNAME              = "0.0.0.0"
+  DATACITE_API_URL      = "https://api.test.datacite.org"
 }
 
 
--- infrastructure/terraform/environments/cloudflare/main.tf
+++ infrastructure/terraform/environments/cloudflare/main.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 2.0"
+      version = ">= 6.0"
     }
 
     cloudflare = {
