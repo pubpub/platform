5fc6cb565f27e0ab0d23e9f8c1bdeba56c7c5579
Thomas F. K. Jorna <hello@tefkah.com>
2025-10-06 16:13:30 +0200

    fix: persist current tab in stage manage page through url param

--- core/app/c/[communitySlug]/stages/manage/components/panel/StagePanel.tsx
+++ core/app/c/[communitySlug]/stages/manage/components/panel/StagePanel.tsx
@@ -1,9 +1,13 @@
 import type { User } from "lucia";
 
+import Link from "next/link";
+
 import type { StagesId } from "db/public";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "ui/tabs";
 
 import { getStage } from "~/lib/db/queries";
+import { getCommunitySlug } from "~/lib/server/cache/getCommunitySlug";
+import { capitalize } from "~/lib/string";
 import { StagePanelActions } from "./actionsTab/StagePanelActions";
 import { StagePanelRules } from "./actionsTab/StagePanelRules";
 import { StagePanelMembers } from "./StagePanelMembers";
@@ -13,11 +17,12 @@ import { StagePanelSheet } from "./StagePanelSheet";
 
 type Props = {
 	stageId: StagesId | undefined;
-	searchParams: Record<string, unknown>;
+	searchParams: Record<string, string>;
 	user: User;
 };
 
 export const StagePanel = async (props: Props) => {
+	const communitySlug = await getCommunitySlug();
 	let open = Boolean(props.stageId);
 
 	if (!props.stageId) {
@@ -31,14 +36,32 @@ export const StagePanel = async (props: Props) => {
 		}
 	}
 
+	const defaultTab = props.searchParams.tab || "overview";
+
 	return (
 		<StagePanelSheet open={open}>
-			<Tabs defaultValue="overview">
+			<Tabs defaultValue={defaultTab}>
 				<TabsList className="grid grid-cols-4">
-					<TabsTrigger value="overview">Overview</TabsTrigger>
-					<TabsTrigger value="pubs">Pubs</TabsTrigger>
-					<TabsTrigger value="actions">Actions</TabsTrigger>
-					<TabsTrigger value="members">Members</TabsTrigger>
+					<TabLink
+						tab="overview"
+						communitySlug={communitySlug}
+						searchParams={props.searchParams}
+					/>
+					<TabLink
+						tab="pubs"
+						communitySlug={communitySlug}
+						searchParams={props.searchParams}
+					/>
+					<TabLink
+						tab="actions"
+						communitySlug={communitySlug}
+						searchParams={props.searchParams}
+					/>
+					<TabLink
+						tab="members"
+						communitySlug={communitySlug}
+						searchParams={props.searchParams}
+					/>
 				</TabsList>
 				<TabsContent value="overview">
 					<StagePanelOverview stageId={props.stageId} userId={props.user.id} />
@@ -61,3 +84,24 @@ export const StagePanel = async (props: Props) => {
 		</StagePanelSheet>
 	);
 };
+
+function TabLink({
+	tab,
+	communitySlug,
+	searchParams,
+}: {
+	tab: string;
+	communitySlug: string;
+	searchParams: Record<string, string>;
+}) {
+	const sparams = new URLSearchParams(searchParams);
+	sparams.set("tab", tab);
+
+	return (
+		<TabsTrigger value={tab} asChild>
+			<Link href={`/c/${communitySlug}/stages/manage?${sparams.toString()}`}>
+				{capitalize(tab)}
+			</Link>
+		</TabsTrigger>
+	);
+}
