563acd71241f42fc02e8e3624aaa51843600a88c
Thomas F. K. Jorna <hello@tefkah.com>
2025-10-21 16:22:36 +0200

    feat: add some tests

--- core/actions/_lib/validateActionSchema.ts
+++ core/actions/_lib/validateActionSchema.ts
@@ -1,15 +1,45 @@
-import type { Action, CommunitiesId } from "db/public";
+import type z from "zod";
 
-import { getActionConfigDefaults } from "~/lib/server/actions";
+import type { Action } from "db/public";
+
+import { getActionByName } from "../api";
+import { schemaWithJsonFields } from "./schemaWithJsonFields";
 
 /**
  * parses the data with the action config schema, including defaults
  * and allows for json fields
  */
-export const validateActionSchema = async (
-	action: Action,
-	communityId: CommunitiesId,
+export const parseActionSchema = (
+	actionName: Action,
+	actionConfig: Record<string, any>,
+	defaultConfig: Record<string, any>,
 	data: Record<string, any>
 ) => {
-	const actionDefaults = await getActionConfigDefaults(communityId, action).executeTakeFirst();
+	const action = getActionByName(actionName);
+
+	const schema = schemaWithJsonFields(action.config.schema as z.ZodObject<any>);
+
+	const schemaWithPartialDefaults = schema.partial(
+		Object.keys(defaultConfig ?? {}).reduce(
+			(acc, key) => {
+				acc[key] = true;
+				return acc;
+			},
+			{} as Record<string, true>
+		)
+	);
+
+	const parsedData = schemaWithPartialDefaults.safeParse({
+		...defaultConfig,
+		...actionConfig,
+		...data,
+	});
+
+	if (!parsedData.success) {
+		return {
+			error: parsedData.error,
+		};
+	}
+
+	return parsedData.data;
 };
