39213e656cc1018a9806bd09e0d7faf8a3caae86
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2025-10-22 11:09:36 -0400

    feat: refactor email action

--- packages/ui/src/editors/LexicalEditor.tsx
+++ packages/ui/src/editors/LexicalEditor.tsx
@@ -1,4 +1,5 @@
 import type { EditorState } from "lexical";
+import type { ControllerRenderProps } from "react-hook-form";
 
 import * as React from "react";
 import { CodeNode } from "@lexical/code";
@@ -22,11 +23,7 @@ import { HeadingNode, QuoteNode } from "@lexical/rich-text";
 
 import { cn } from "utils";
 
-import type { AutoFormInputComponentProps } from "../auto-form";
-import AutoFormDescription from "../auto-form/common/description";
-import AutoFormLabel from "../auto-form/common/label";
-import AutoFormTooltip from "../auto-form/common/tooltip";
-import { FormControl, FormItem, FormMessage } from "../form";
+import { FormControl } from "../form";
 import { useTokenContext } from "../tokens";
 import { SingleLinePlugin } from "./SingleLinePlugin";
 import { TokenNode } from "./TokenNode";
@@ -60,17 +57,13 @@ const makeSyntheticChangeEvent = (value: string) => {
 	};
 };
 
-export const LexicalEditor = (
-	props: AutoFormInputComponentProps & {
-		withMarkdown?: boolean;
-		/** If the size of the input should just be a single line. Will also prevent line breaks */
-		singleLine?: boolean;
-	}
-) => {
-	const { showLabel: _showLabel, ...fieldPropsWithoutShowLabel } = props.fieldProps;
-	const showLabel = _showLabel === undefined ? true : _showLabel;
-	const { descriptionPlacement = "top" } = props;
-	const initialValue = React.useMemo(() => props.field.value ?? "", []);
+export type LexicalEditorProps = ControllerRenderProps<any, string> & {
+	withMarkdown?: boolean;
+	singleLine?: boolean;
+};
+
+export const LexicalEditor = (props: LexicalEditorProps) => {
+	const initialValue = React.useMemo(() => props.value ?? "", []);
 	const initialConfig = React.useMemo(() => {
 		return {
 			namespace: "LexicalEditor",
@@ -86,61 +79,40 @@ export const LexicalEditor = (
 		(editorState: EditorState) => {
 			editorState.read(() => {
 				const markdown = $convertToMarkdownString(TRANSFORMERS);
-				fieldPropsWithoutShowLabel.onChange(makeSyntheticChangeEvent(markdown));
+				props.onChange(makeSyntheticChangeEvent(markdown));
 			});
 		},
-		[fieldPropsWithoutShowLabel.onChange]
+		[props.onChange]
 	);
 
 	const labelId = React.useId();
 
 	return (
-		<div className="flex flex-row items-center space-x-2">
-			<FormItem className="flex w-full flex-col justify-start">
-				{showLabel && (
-					<>
-						<AutoFormLabel
-							id={labelId}
-							label={props.label}
-							isRequired={props.isRequired}
+		<LexicalComposer initialConfig={initialConfig}>
+			<RichTextPlugin
+				contentEditable={
+					<FormControl>
+						<ContentEditable
+							ariaLabelledBy={labelId}
+							className={cn(
+								"editor",
+								"prose prose-sm",
+								props.singleLine ? "min-h-5" : "min-h-[200px]",
+								// Copied from ui/src/input.tsx
+								"w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+							)}
 						/>
-						{props.description && descriptionPlacement === "top" && (
-							<AutoFormDescription description={props.description} />
-						)}
-					</>
-				)}
-				<LexicalComposer initialConfig={initialConfig}>
-					<RichTextPlugin
-						contentEditable={
-							<FormControl>
-								<ContentEditable
-									ariaLabelledBy={labelId}
-									className={cn(
-										"editor",
-										"prose prose-sm",
-										props.singleLine ? "min-h-5" : "min-h-[200px]",
-										// Copied from ui/src/input.tsx
-										"w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
-									)}
-								/>
-							</FormControl>
-						}
-						placeholder={null}
-						ErrorBoundary={LexicalErrorBoundary}
-					/>
-					<OnChangePlugin onChange={onChange} />
-					<HistoryPlugin />
-					<AutoFocusPlugin />
-					{props.singleLine && <SingleLinePlugin />}
-					{props.withMarkdown && <MarkdownShortcutPlugin transformers={TRANSFORMERS} />}
-					<TokenPlugin tokens={Object.keys(tokens[props.field.name] ?? {})} />
-				</LexicalComposer>
-				<AutoFormTooltip fieldConfigItem={props.fieldConfigItem} />
-				{props.description && descriptionPlacement === "bottom" && (
-					<AutoFormDescription description={props.description} />
-				)}
-				<FormMessage />
-			</FormItem>
-		</div>
+					</FormControl>
+				}
+				placeholder={null}
+				ErrorBoundary={LexicalErrorBoundary}
+			/>
+			<OnChangePlugin onChange={onChange} />
+			<HistoryPlugin />
+			<AutoFocusPlugin />
+			{props.singleLine && <SingleLinePlugin />}
+			{props.withMarkdown && <MarkdownShortcutPlugin transformers={TRANSFORMERS} />}
+			<TokenPlugin tokens={Object.keys(tokens[props.name] ?? {})} />
+		</LexicalComposer>
 	);
 };
