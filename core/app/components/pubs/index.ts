92c6bf36dfa51078f87bf8436023115861561ae4
Thomas F. K. Jorna <hello@tefkah.com>
2025-10-31 00:52:15 +0100

    fix: format

--- core/app/components/pubs/index.ts
+++
@@ -1,7 +0,0 @@
-export { PubCardClient } from "./PubCard/PubCardClient";
-export type { PubCardClientProps } from "./PubCard/PubCardClient";
-export { PubSearchSelect } from "./PubSearchSelect";
-export type { PubSearchSelectProps } from "./PubSearchSelect";
-export { FormPubSearchSelect } from "./FormPubSearchSelect";
-export type { FormPubSearchSelectProps } from "./FormPubSearchSelect";
-
