364928714fdd2b5bbde7aecb6930ee26cb564668
Thomas F. K. Jorna <hello@tefkah.com>
2025-11-27 00:17:45 +0100

    fix: upgrade astro tailwind

--- site-builder/astro.config.mjs
+++ site-builder/astro.config.mjs
@@ -4,36 +4,40 @@ import tailwind from "@astrojs/tailwind"
 // import pdf from "astro-pdf";
 import { defineConfig } from "astro/config"
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		ssr: {
-			external: ["db"],
-		},
-	},
-	integrations: [
-		react(),
-		tailwind({
-			applyBaseStyles: true,
-			configFile: "./tailwind.config.mjs",
-		}),
+    vite: {
+      ssr: {
+          external: ["db"],
+      },
 
-		// pdf({
-		// 	baseOptions: {
-		// 		path: "/pdf[pathname].pdf",
-		// 		navTimeout: 60000,
-		// 		maxRetries: 3,
+      plugins: [tailwindcss()],
+    },
+    integrations: [
+        react(),
+        tailwind({
+            applyBaseStyles: true,
+            configFile: "./tailwind.config.mjs",
+        }),
 
-		// 	},
-		// 	maxConcurrent: 10,
+        // pdf({
+        // 	baseOptions: {
+        // 		path: "/pdf[pathname].pdf",
+        // 		navTimeout: 60000,
+        // 		maxRetries: 3,
 
-		// 	pages: (pathname) => {
-		// 		if (pathname.includes("/pub") && !pathname.includes("/release")) {
-		// 			return true;
-		// 		}
-		// 		return false;
-		// 	},
-		// }),
-	],
-	prefetch: true,
+        // 	},
+        // 	maxConcurrent: 10,
+
+        // 	pages: (pathname) => {
+        // 		if (pathname.includes("/pub") && !pathname.includes("/release")) {
+        // 			return true;
+        // 		}
+        // 		return false;
+        // 	},
+        // }),
+    ],
+    prefetch: true,
 })
