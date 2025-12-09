78577543ada897de87874a6c6a0a35ae8a8f5b8f
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2025-12-09 17:00:46 -0500

    feat: fix slider merge

--- packages/ui/src/slider.tsx
+++ packages/ui/src/slider.tsx
@@ -5,24 +5,6 @@ import * as SliderPrimitive from "@radix-ui/react-slider"
 
 import { cn } from "utils"
 
-<<<<<<< HEAD
-const Slider = React.forwardRef<
-	React.ComponentRef<typeof SliderPrimitive.Root>,
-	React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
->(({ className, ...props }, ref) => (
-	<SliderPrimitive.Root
-		ref={ref}
-		className={cn("relative flex w-full touch-none select-none items-center", className)}
-		{...props}
-	>
-		<SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-secondary">
-			<SliderPrimitive.Range className="absolute h-full bg-primary" />
-		</SliderPrimitive.Track>
-		<SliderPrimitive.Thumb className="block h-5 w-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50" />
-	</SliderPrimitive.Root>
-))
-Slider.displayName = SliderPrimitive.Root.displayName
-=======
 function Slider({
 	className,
 	defaultValue,
@@ -35,7 +17,7 @@ function Slider({
 		() =>
 			Array.isArray(value) ? value : Array.isArray(defaultValue) ? defaultValue : [min, max],
 		[value, defaultValue, min, max]
-	);
+	)
 
 	return (
 		<SliderPrimitive.Root
@@ -67,12 +49,11 @@ function Slider({
 				<SliderPrimitive.Thumb
 					data-slot="slider-thumb"
 					key={index}
-					className="focus-visible:outline-hidden block size-4 shrink-0 rounded-full border border-primary bg-white shadow-sm ring-ring/50 transition-[color,box-shadow] hover:ring-4 focus-visible:ring-4 disabled:pointer-events-none disabled:opacity-50"
+					className="block size-4 shrink-0 rounded-full border border-primary bg-white shadow-sm ring-ring/50 transition-[color,box-shadow] hover:ring-4 focus-visible:outline-hidden focus-visible:ring-4 disabled:pointer-events-none disabled:opacity-50"
 				/>
 			))}
 		</SliderPrimitive.Root>
-	);
+	)
 }
->>>>>>> cf000fe8a (feat: basic dynamic form generation)
 
 export { Slider }
