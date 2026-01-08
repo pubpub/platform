b65b0bc91b8d95711f06de3b45da3f53edbd797f
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2026-01-08 14:52:49 -0500

    feat: coar notify mock preprint repo

---
+++ core/playwright/test-fixtures.ts
@@ -0,0 +1,18 @@
+import { test as base } from "@playwright/test";
+import { MockPreprintRepo } from "./fixtures/mock-preprint-repo";
+
+export type TestFixtures = {
+	mockPreprintRepo: MockPreprintRepo;
+};
+
+export const test = base.extend<TestFixtures>({
+	mockPreprintRepo: async ({}, use) => {
+		const repo = new MockPreprintRepo();
+		await repo.start();
+		await use(repo);
+		await repo.stop();
+	},
+});
+
+export { expect } from "@playwright/test";
+
