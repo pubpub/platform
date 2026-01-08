e301566d77617a319d25dae532657bfd64139482
Thomas F. K. Jorna <hello@tefkah.com>
2026-01-08 16:23:49 +0100

    fix: actually make pubform editeable on mobile

--- core/app/components/pubs/PubEditor/PageTitleWithStatus.tsx
+++ core/app/components/pubs/PubEditor/PageTitleWithStatus.tsx
@@ -14,18 +14,20 @@ export const PubPageStatus = ({
 	forms: SimpleForm[]
 	defaultFormSlug?: string
 }) => {
-	const status = useSaveStatus({ defaultMessage: "Form will save when you click save" })
+	const status = useSaveStatus({ defaultMessage: "Save form to keep changes" })
 	return (
 		<div className="flex items-center gap-2 font-normal text-muted-foreground text-sm">
-			<span data-testid="save-status-text">{status}</span>
+			<span data-testid="save-status-text" className="whitespace-nowrap text-xs md:text-sm">
+				{status}
+			</span>
 
 			<div className="mx-1 h-4 border-gray-300 border-r"></div>
 			<FormSwitcher
 				forms={forms}
 				defaultFormSlug={defaultFormSlug}
-				className="m-0 h-6 bg-none p-0 text-sm shadow-none focus-visible:ring-2 focus-visible:ring-offset-1"
+				className="m-0 h-6 bg-none p-0 text-xs shadow-none focus-visible:ring-2 focus-visible:ring-offset-1 md:text-sm"
 			>
-				<Pencil size={14} />
+				<Pencil size={14} className="size-3 md:size-4" />
 			</FormSwitcher>
 		</div>
 	)
